17641c69d87000261d7c5d9c258c302a81740247
Philip Vendil <philip.vendil@primekey.se>
2008-05-01 16:01:24 +0000

    DSS-28
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@197 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/bin/signserver.sh
+++ signserver/bin/signserver.sh
@@ -11,7 +11,7 @@ JAVACMD=`which java`
 if [ ! -n "$JAVA_HOME" ]; then
     if [ ! -n "$JAVACMD" ]
     then
-        echo "You must set JAVA_HOME before running the EJBCA cli."
+        echo "You must set JAVA_HOME before running the SignServer cli."
         exit 1
     fi
 else
@@ -25,70 +25,90 @@ class_name=org.signserver.cli.signserver
 #shift
 
 # J2EE server classpath
-if [ ! -n "$APPSRV_HOME" ]; then
-    if [ -n "$JBOSS_HOME" ]; then
-        APPSRV_HOME=$JBOSS_HOME
-    elif [ -n "$WEBLOGIC_HOME" ]; then
-        APPSRV_HOME=$WEBLOGIC_HOME
-    fi
-fi
+#if [ ! -n "$APPSRV_HOME" ]; then
+#    if [ -n "$JBOSS_HOME" ]; then
+#        APPSRV_HOME=$JBOSS_HOME
+#    elif [ -n "$WEBLOGIC_HOME" ]; then
+#        APPSRV_HOME=$WEBLOGIC_HOME
+#    fi
+#fi
 
-if [ -n "$APPSRV_HOME" ]; then
-    J2EE_DIR="${APPSRV_HOME}"/client
-    if [ -r "$APPSRV_HOME"/server/lib/weblogic.jar ]; then
-        echo "Using Weblogic JNDI provider..."
-        J2EE_DIR="${APPSRV_HOME}"/server/lib
-    elif [ -r "$APPSRV_HOME"/lib/appserv-rt.jar ]; then
-        echo Using Glassfish JNDI provider...
-        J2EE_DIR="${APPSRV_HOME}"/lib
-    elif [ -r "$APPSRV_HOME"/j2ee/home/oc4jclient.jar ]; then
-        echo Using Oracle JNDI provider...
-        J2EE_DIR="${APPSRV_HOME}"/j2ee/home
-    elif [ -d "$APPSRV_HOME"/runtimes ]; then
-        echo Using Websphere JNDI provider...
-        J2EE_DIR="${APPSRV_HOME}"/runtimes
-    else 
-        echo "Using JBoss JNDI provider..."
-    fi
+#if [ -n "$APPSRV_HOME" ]; then
+#    J2EE_DIR="${APPSRV_HOME}"/client
+#    if [ -r "$APPSRV_HOME"/server/lib/weblogic.jar ]; then
+#        echo "Using Weblogic JNDI provider..."
+#        J2EE_DIR="${APPSRV_HOME}"/server/lib
+#    elif [ -r "$APPSRV_HOME"/lib/appserv-rt.jar ]; then
+#        echo Using Glassfish JNDI provider...
+#        J2EE_DIR="${APPSRV_HOME}"/lib
+#    elif [ -r "$APPSRV_HOME"/j2ee/home/oc4jclient.jar ]; then
+#        echo Using Oracle JNDI provider...
+#        J2EE_DIR="${APPSRV_HOME}"/j2ee/home
+#    elif [ -d "$APPSRV_HOME"/runtimes ]; then
+#        echo Using Websphere JNDI provider...
+#        J2EE_DIR="${APPSRV_HOME}"/runtimes
+#    else 
+#        echo "Using JBoss JNDI provider..."
+#    fi
+#else
+#    echo "Could not find a valid J2EE server for JNDI provider.."
+#    echo "Specify a APPSRV_HOME environment variable"
+#    exit 1
+#fi
+
+if [ ! -n "${SIGNSERVER_HOME}" ]; then
+  if [ -f /usr/share/signserver/bin/signserver.sh ]; then
+     SIGNSRV_HOME=/usr/share/signserver
+  fi
+  if [ -f /opt/signserver/bin/signserver.sh ]; then
+     SIGNSRV_HOME=/opt/signserver
+  fi
+  if [ -f /usr/local/signserver/bin/signserver.sh ]; then
+     SIGNSRV_HOME=/usr/local/signserver
+  fi
+  if [ -f ./signserver.sh ]; then
+     SIGNSRV_HOME=..
+  fi
+  if [ -f bin/signserver.sh ]; then
+     SIGNSRV_HOME=.
+  fi
 else
-    echo "Could not find a valid J2EE server for JNDI provider.."
-    echo "Specify a APPSRV_HOME environment variable"
-    exit 1
-fi
+  SIGNSRV_HOME=$SIGNSERVER_HOME
+fi 
 
-SIGNSERVER_HOME="`dirname $0`/.."
 # Check that classes exist
-if [ ! -d ${SIGNSERVER_HOME}/tmp/bin/classes ]
+if [ ! -f ${SIGNSRV_HOME}/dist-client/signserver-cli.jar ]
 then    
         echo "You must build SIGNSERVER before using the cli, use 'ant'."
         exit 1
 fi
 
 # library classpath
-CP="$SIGNSERVER_HOME/tmp/bin/classes"
-for i in "${J2EE_DIR}"/*.jar
+CP="$SIGNSRV_HOME/dist-client/signserver-cli.jar"
+for i in "${SIGNSRV_HOME}"/dist-client/lib/*.jar
 do
 	CP="$i":"$CP"
 done
-for i in "${SIGNSERVER_HOME}"/lib/*.jar
+for i in "${SIGNSRV_HOME}"/lib/*.jar
 do
 	CP="$i":"$CP"
 done
-for i in "${SIGNSERVER_HOME}"/lib/1.5/*.jar
+for i in "${SIGNSRV_HOME}"/lib/1.5/*.jar
 do
 	CP="$i":"$CP"
 done
-for i in "${SIGNSERVER_HOME}"/lib/ejb/*.jar
+for i in "${SIGNSRV_HOME}"/lib/ext/ejb/*.jar
 do
 	CP="$i":"$CP"
 done
-for i in "${SIGNSERVER_HOME}"/lib/james/*.jar
+for i in "${SIGNSRV_HOME}"/lib/ext/james/*.jar
 do
 	CP="$i":"$CP"
 done
+CP="${SIGNSRV_HOME}/bin":"$CP"
+#echo $CP
 
-CP=$CP:$EJBCA_HOME/bin
-
+export SIGNSRV_HOME
 
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then

32fc7dead00b2606edd311b129ded0ce2a000128
Philip Vendil <philip.vendil@primekey.se>
2008-05-01 15:19:47 +0000

    DSS-28
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@194 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/bin/signserver.cmd
@@ -0,0 +1,44 @@
+@echo off
+
+rem library classpath
+if "%SIGNSERVER_HOME%" == "" (
+  set SIGNSRV_HOME=..
+  rem It must work to call both as bin\signserver.cmd or from within bin
+  if not exist signserver.cmd set SIGNSRV_HOME=.
+ ) else (
+    set SIGNSRV_HOME="%SIGNSERVER_HOME%"
+) 
+  
+set SIGNSERVER_CP=%SIGNSRV_HOME%\lib\log4j.jar;%SIGNSRV_HOME%\lib\1.5\bcprov-jdk.jar;%SIGNSRV_HOME%\lib\1.5\bcmail-jdk.jar;%SIGNSRV_HOME%\lib\ejbca-util.jar;%SIGNSRV_HOME%\lib\commons-lang-2.0.jar;%SIGNSRV_HOME%\lib\ext\ejb\jboss-ejb3x.jar;%SIGNSRV_HOME%\lib\ext\james\james-2.3.1.jar
+set J2EE_CP=%SIGNSRV_HOME%\dist-client\lib\jbossall-client.jar
+
+
+rem check that we have built the classes
+if not exist %SIGNSRV_HOME%\dist-client\signserver-cli.jar (
+    echo You must build SIGNSERVER before using the cli, use 'ant'.
+    goto end
+)
+
+set CLASSPATH=%J2EE_CP%;%SIGNSERVER_CP%;%SIGNSRV_HOME%\bin;%SIGNSRV_HOME%\dist-client\signserver-cli.jar
+rem echo %CLASSPATH%
+
+rem Fixup arguments, we have to do this since windows normally only 
+rem supports %1-%9 as command line arguments
+shift
+set a=%0
+set b=%1
+set c=%2
+set d=%3
+set e=%4
+set f=%5
+set g=%6
+set h=%7
+set i=%8
+set j=%9
+rem echo %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
+if "%JAVA_HOME%" == "" (
+  java -cp %CLASSPATH%  org.signserver.cli.signserver %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
+) else (
+  "%JAVA_HOME%\bin\java" -cp %CLASSPATH% org.signserver.cli.signserver %a% %b% %c% %d% %e% %f% %g% %h% %i% %j% 
+)
+:end

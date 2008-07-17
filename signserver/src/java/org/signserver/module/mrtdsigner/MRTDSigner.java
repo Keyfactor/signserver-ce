2f6c81259db67b61e4253a705b37e720c5df328a
Philip Vendil <philip.vendil@primekey.se>
2008-07-17 16:44:55 +0000

    DSS-62, DSS-63
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@295 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/src/java/org/signserver/module/mrtdsigner/MRTDSigner.java
@@ -0,0 +1,140 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.signserver.module.mrtdsigner;
+ 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.ejb.EJBException;
+
+import org.signserver.common.CryptoTokenOfflineException;
+import org.signserver.common.ICertReqData;
+import org.signserver.common.ISignRequest;
+import org.signserver.common.ISignerCertReqInfo;
+import org.signserver.common.IllegalRequestException;
+import org.signserver.common.MRTDSignRequest;
+import org.signserver.common.MRTDSignResponse;
+import org.signserver.common.ProcessRequest;
+import org.signserver.common.ProcessResponse;
+import org.signserver.common.RequestContext;
+import org.signserver.server.cryptotokens.ICryptoToken;
+import org.signserver.server.signers.BaseSigner;
+
+
+/**
+ * Class used to sign MRTD Document Objects.
+ * 
+ * @author Philip Vendil
+ * @version $Id: MRTDSigner.java,v 1.9 2007-12-12 14:24:55 herrvendil Exp $
+ */
+
+public class MRTDSigner extends BaseSigner {
+	
+	//private transient Logger log = Logger.getLogger(this.getClass());
+	
+	public MRTDSigner(){
+	}
+	
+	/**
+	 * The main method that signs a number of byte arrays with the algorithm
+	 * specified for MRTD Security Objects.
+	 * 
+	 * 
+	 * @param signRequest must be of the class MRTDSignRequest
+	 * @return returns a MRTDSignResponse with the same number of signatures as requested.
+	 *
+	 */
+	
+	public ProcessResponse processData(ProcessRequest signRequest,
+	                              RequestContext requestContext) throws IllegalRequestException, CryptoTokenOfflineException{
+		
+		
+		ISignRequest sReq = (ISignRequest) signRequest;
+		if(!(signRequest instanceof MRTDSignRequest)){
+			throw new IllegalRequestException("Sign request with id :" + sReq.getRequestID() + " is of the wrong type :" 
+					                               + signRequest.getClass().getName() + " should be MRTDSignRequest ");
+		}
+		
+		MRTDSignRequest req = (MRTDSignRequest) signRequest;
+		
+        ArrayList<byte[]> genSignatures = new ArrayList<byte[]>();
+        
+		if(req.getRequestData() == null){
+			throw new IllegalRequestException("Signature request data cannot be null.");
+		}
+        
+        Iterator<?> iterator = ((ArrayList<?>) req.getRequestData()).iterator();
+        while(iterator.hasNext()){
+        	
+        	byte[] data = null;
+        	try{
+        	   data = (byte[]) iterator.next();	
+        	}catch(Exception e){
+        		throw new IllegalRequestException("Signature request data must be an ArrayList of byte[]");
+        	}
+        	
+        	
+        	Cipher c;
+			try {
+				c = Cipher.getInstance("RSA", getCryptoToken().getProvider(ICryptoToken.PROVIDERUSAGE_SIGN));
+			} catch (NoSuchAlgorithmException e) {
+				throw new EJBException(e);
+			} catch (NoSuchProviderException e) {
+				throw new EJBException(e);
+			} catch (NoSuchPaddingException e) {
+				throw new EJBException(e);
+			}
+
+            try {
+				c.init(Cipher.ENCRYPT_MODE, this.getCryptoToken().getPrivateKey(ICryptoToken.PURPOSE_SIGN));				
+			} catch (InvalidKeyException e) {
+				throw new EJBException(e);
+			}
+
+            byte[] result;
+			try {
+				result = c.doFinal(data);
+			} catch (IllegalBlockSizeException e) {
+				throw new EJBException(e);
+			} catch (BadPaddingException e) {
+				throw new EJBException(e);
+			}
+
+
+            
+            genSignatures.add(result);	
+        }
+        
+		return new MRTDSignResponse(sReq.getRequestID(),genSignatures,getSigningCertificate());
+	} 
+
+
+
+    /**
+     * Not supported yet
+     */
+	public ICertReqData genCertificateRequest(ISignerCertReqInfo info) throws CryptoTokenOfflineException{		
+		return getCryptoToken().genCertificateRequest(info);
+	}
+
+
+	
+}
---
+++ signserver/src/test/org/signserver/module/mrtdsigner/TestMRTDSigner.java
@@ -0,0 +1,178 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.signserver.module.mrtdsigner;
+
+import java.security.cert.Certificate;
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+import javax.crypto.Cipher;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+import junit.framework.TestCase;
+
+import org.signserver.cli.CommonAdminInterface;
+import org.signserver.common.MRTDSignRequest;
+import org.signserver.common.MRTDSignResponse;
+import org.signserver.common.RequestContext;
+import org.signserver.common.SignServerUtil;
+import org.signserver.common.SignerStatus;
+import org.signserver.common.clusterclassloader.MARFileParser;
+import org.signserver.ejb.interfaces.IWorkerSession;
+import org.signserver.testutils.TestUtils;
+import org.signserver.testutils.TestingSecurityManager;
+
+
+public class TestMRTDSigner extends TestCase {
+
+
+	private static IWorkerSession.IRemote sSSession = null;
+	
+	private static String signserverhome;
+	private static int moduleVersion;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		SignServerUtil.installBCProvider();
+		Context context = getInitialContext();		
+		sSSession = (IWorkerSession.IRemote) context.lookup(IWorkerSession.IRemote.JNDI_NAME);
+		TestUtils.redirectToTempOut();
+		TestUtils.redirectToTempErr();
+		TestingSecurityManager.install();
+        signserverhome = System.getenv("SIGNSERVER_HOME");
+        assertNotNull(signserverhome);
+        CommonAdminInterface.BUILDMODE = "SIGNSERVER";
+	}
+	
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+		TestingSecurityManager.remove();
+	}
+	
+	public void test00SetupDatabase() throws Exception{
+		   
+		MARFileParser marFileParser = new MARFileParser(signserverhome +"/dist-server/mrtdsigner.mar");
+		moduleVersion = marFileParser.getVersionFromMARFile();
+		
+		TestUtils.assertSuccessfulExecution(new String[] {"module", "add",
+				signserverhome +"/dist-server/mrtdsigner.mar", "junittest"});		
+	    assertTrue(TestUtils.grepTempOut("Loading module MRTDSIGNER"));
+	    assertTrue(TestUtils.grepTempOut("Module loaded successfully."));
+
+	    sSSession.reloadConfiguration(7890);
+	}
+	
+
+
+	/*
+	 * Test method for 'org.signserver.server.MRTDSigner.signData(ISignRequest)'
+	 */
+
+	public void testSignData() throws Exception{
+		
+
+		  
+	      int reqid = 12;
+	      ArrayList<byte[]> signrequests = new ArrayList<byte[]>();
+	      
+	      byte[] signreq1 = "Hello World".getBytes();
+	      byte[] signreq2 = "Hello World2".getBytes();
+	      signrequests.add(signreq1);
+	      signrequests.add(signreq2);
+		 		  
+		  MRTDSignResponse res =  (MRTDSignResponse) sSSession.process(7890, new MRTDSignRequest(reqid,signrequests), new RequestContext()); 		  
+		  assertTrue(res!=null);
+          assertTrue(reqid == res.getRequestID());	      
+	      Certificate signercert = res.getSignerCertificate();	      
+	      assertNotNull(signercert);
+	      
+	      Cipher c = Cipher.getInstance("RSA", "BC");
+          c.init(Cipher.DECRYPT_MODE, signercert);
+
+          byte[] signres1 = c.doFinal((byte[]) ((ArrayList<?>) res.getProcessedData()).get(0));
+
+          if (!arrayEquals(signreq1, signres1))
+          {
+              assertTrue("First MRTD doesn't match with request",false);
+          }
+
+          byte[] signres2 = c.doFinal((byte[]) ((ArrayList<?>) res.getProcessedData()).get(1));
+
+          if (!arrayEquals(signreq2, signres2))
+          {
+              assertTrue("Second MRTD doesn't match with request",false);
+          }	 
+		  
+
+		  
+
+	}
+
+	private boolean arrayEquals(byte[] signreq2, byte[] signres2) {
+		boolean retval = true;
+		
+		if(signreq2.length != signres2.length){
+			return false;
+		}
+		
+		for(int i=0;i<signreq2.length;i++){
+			if(signreq2[i] != signres2[i]){
+				return false;
+			}
+		}
+		return retval;
+	}
+
+	/*
+	 * Test method for 'org.signserver.server.MRTDSigner.getStatus()'
+	 */
+	public void testGetStatus() throws Exception{
+	  SignerStatus stat = (SignerStatus) sSSession.getStatus(7890);
+	  assertTrue(stat.getTokenStatus() == SignerStatus.STATUS_ACTIVE);		
+      
+	}
+	
+	public void test99TearDownDatabase() throws Exception{
+		TestUtils.assertSuccessfulExecution(new String[] {"removeworker",
+		"7890"});
+		
+		TestUtils.assertSuccessfulExecution(new String[] {"module", "remove","MRTDSIGNER", "" + moduleVersion});		
+		assertTrue(TestUtils.grepTempOut("Removal of module successful."));
+	    sSSession.reloadConfiguration(7890);
+	}
+	 
+ 
+    
+    /**
+     * Get the initial naming context
+     */
+    protected Context getInitialContext() throws Exception {
+    	Hashtable<String, String> props = new Hashtable<String, String>();
+    	props.put(
+    		Context.INITIAL_CONTEXT_FACTORY,
+    		"org.jnp.interfaces.NamingContextFactory");
+    	props.put(
+    		Context.URL_PKG_PREFIXES,
+    		"org.jboss.naming:org.jnp.interfaces");
+    	props.put(Context.PROVIDER_URL, "jnp://localhost:1099");
+    	Context ctx = new InitialContext(props);
+    	return ctx;
+    }
+	
+}

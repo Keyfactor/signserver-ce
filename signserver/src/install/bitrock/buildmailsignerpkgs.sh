e9aa1cae7c07075aec87a590696997b936622633
Philip Vendil <philip.vendil@primekey.se>
2008-09-09 10:05:54 +0000

    First version of mailsigner windows package bitrock definition
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@368 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/src/install/bitrock/buildmailsignerpkgs.sh
+++ signserver/src/install/bitrock/buildmailsignerpkgs.sh
@@ -11,5 +11,5 @@ ant pkgdist
 cd src/install/bitrock
 builder build mailsignernode-unix.xml linux
 builder build mailsignermgmt-unix.xml linux
-#builder build mailsignernode-windows.xml windows
+builder build mailsignernode-windows.xml windows
 builder build mailsignermgmt-windows.xml windows

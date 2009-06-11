2eba0be81c38c770f510ab2c78d2740727b3b535
Markus Kilås <markus.kilas@primekey.com>
2009-06-11 09:46:36 +0000

    DSS-40: Added more tests for the CRL Validator.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@513 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/src/test/org/signserver/validationservice/server/TestCRLValidator.java
+++ signserver/src/test/org/signserver/validationservice/server/TestCRLValidator.java
@@ -18,6 +18,7 @@ import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.net.URL;
 import java.security.KeyPair;
+import java.security.PrivateKey;
 import java.security.cert.X509CRL;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
@@ -31,6 +32,7 @@ import javax.naming.InitialContext;
 import junit.framework.TestCase;
 
 import org.bouncycastle.asn1.x509.CRLDistPoint;
+import org.bouncycastle.jce.X509KeyUsage;
 import org.ejbca.core.model.ca.crl.RevokedCertInfo;
 import org.ejbca.util.keystore.KeyTools;
 import org.signserver.cli.CommonAdminInterface;
@@ -78,7 +80,19 @@ public class TestCRLValidator extends TestCase {
 	
 	/** EndEntity4 signed by RootCA2 */
 	private static X509Certificate certEndEntity4;
+	
+	/** EndEntity5 signed by RootCA1, expired */
+	private static X509Certificate certEndEntity5Expired;
 
+	/** EndEntity6 signed by RootCA1, not yet valid */
+	private static X509Certificate certEndEntity6NotYetValid;
+	
+	/** EndEntity7 signed by RootCA1, signed by wrong issuer */
+	private static X509Certificate certEndEntity7WrongIssuer;
+	
+	/** EndEntity8 signed by RootCA1, with key usage electronic signature */
+	private static X509Certificate certEndEntity8KeyUsageSig;
+	
 	/** CRL for RootCA2 */
 	private static X509CRL crlRootCA2;
 
@@ -92,7 +106,9 @@ public class TestCRLValidator extends TestCase {
 		gCSession = (IGlobalConfigurationSession.IRemote) context.lookup(IGlobalConfigurationSession.IRemote.JNDI_NAME);
 		sSSession = (IWorkerSession.IRemote) context.lookup(IWorkerSession.IRemote.JNDI_NAME);
 
-		signServerHome = new File(System.getenv("SIGNSERVER_HOME"));
+		String envSignServerHome = System.getenv("SIGNSERVER_HOME");
+		assertNotNull("Environment variable SIGNSERVER_HOME must be set!", envSignServerHome);
+		signServerHome = new File(envSignServerHome);
 		assertTrue(signServerHome.exists());
 
 		CommonAdminInterface.BUILDMODE = "SIGNSERVER";
@@ -117,6 +133,23 @@ public class TestCRLValidator extends TestCase {
 		KeyPair keysEndEntity2 = KeyTools.genKeys("1024", "RSA");
 		certEndEntity2 = ValidationTestUtils.genCert("CN=EndEntity2", "CN=RootCA1", keysRootCA1.getPrivate(), keysEndEntity2.getPublic(), 
 				new Date(0), new Date(System.currentTimeMillis() + 1000000), false, 0, crlDistPointCA1WithUrl);
+		
+		KeyPair keysEndEntity5 = KeyTools.genKeys("1024", "RSA");
+		certEndEntity5Expired = ValidationTestUtils.genCert("CN=EndEntity5", "CN=RootCA1", keysRootCA1.getPrivate(), keysEndEntity5.getPublic(), 
+				new Date(0), new Date(System.currentTimeMillis() - 2000000), false, 0, crlDistPointCA1WithUrl);
+		
+		KeyPair keysEndEntity6 = KeyTools.genKeys("1024", "RSA");
+		certEndEntity6NotYetValid = ValidationTestUtils.genCert("CN=EndEntity6", "CN=RootCA1", keysRootCA1.getPrivate(), keysEndEntity6.getPublic(), 
+				new Date(System.currentTimeMillis() + 1000000), new Date(System.currentTimeMillis() + 3000000), false, 0, crlDistPointCA1WithUrl);
+		
+		KeyPair keysEndEntity7 = KeyTools.genKeys("1024", "RSA");
+		PrivateKey notKeyOfRootCA1 = keysEndEntity1.getPrivate();
+		certEndEntity7WrongIssuer = ValidationTestUtils.genCert("CN=EndEntity7", "CN=RootCA1", notKeyOfRootCA1, keysEndEntity7.getPublic(), 
+				new Date(0), new Date(System.currentTimeMillis() + 1000000), false, 0, crlDistPointCA1WithUrl);
+		
+		KeyPair keysEndEntity8 = KeyTools.genKeys("1024", "RSA");
+		certEndEntity8KeyUsageSig = ValidationTestUtils.genCert("CN=EndEntity8", "CN=RootCA1", keysRootCA1.getPrivate(), keysEndEntity8.getPublic(), 
+				new Date(0), new Date(System.currentTimeMillis() + 1000000), false, X509KeyUsage.digitalSignature | X509KeyUsage.nonRepudiation, crlDistPointCA1WithUrl);
 
 		ArrayList<RevokedCertInfo> revoked = new ArrayList<RevokedCertInfo>();
 		revoked.add(new RevokedCertInfo("fingerprint", certEndEntity2.getSerialNumber(), new Date(),
@@ -271,14 +304,129 @@ public class TestCRLValidator extends TestCase {
 		assertEquals("CN=EndEntity4", val.getCertificate().getSubject());
 	}
 	
-	// TODO: Test for no CRLs
+	/**
+	 * Tests the certificate for EndEntity5 signed by RootCA1.
+	 * The certificate is expired.
+	 */
+	public void test05Expired() throws Exception {
+		ValidateRequest req = new ValidateRequest(ICertificateManager.genICertificate(certEndEntity5Expired), ValidationServiceConstants.CERTPURPOSE_NO_PURPOSE);
+		ValidateResponse res = (ValidateResponse) sSSession.process(15, req, new RequestContext());
+
+		Validation val = res.getValidation();
+		assertTrue(val != null);
+		assertFalse("certificate should be expired", val.getStatus().equals(Validation.Status.VALID));
+		assertTrue(val.getStatusMessage() != null);
+		List<ICertificate> cAChain = val.getCAChain();
+		assertTrue(cAChain != null);
+		assertTrue(cAChain.get(0).getSubject().equals("CN=RootCA1"));
+		assertEquals("CN=EndEntity5", val.getCertificate().getSubject());
+	}
 	
-	// TODO: Test for expired
+	/**
+	 * Tests the certificate for EndEntity6 signed by RootCA1.
+	 * The certificate is not yet valid.
+	 */
+	public void test06NotYetValid() throws Exception {
+		ValidateRequest req = new ValidateRequest(ICertificateManager.genICertificate(certEndEntity6NotYetValid), ValidationServiceConstants.CERTPURPOSE_NO_PURPOSE);
+		ValidateResponse res = (ValidateResponse) sSSession.process(15, req, new RequestContext());
+
+		Validation val = res.getValidation();
+		assertTrue(val != null);
+		assertFalse("certificate should not be valid yet", val.getStatus().equals(Validation.Status.VALID));
+		assertTrue(val.getStatusMessage() != null);
+		List<ICertificate> cAChain = val.getCAChain();
+		assertTrue(cAChain != null);
+		assertTrue(cAChain.get(0).getSubject().equals("CN=RootCA1"));
+		assertEquals("CN=EndEntity6", val.getCertificate().getSubject());
+	}
 	
-	// TODO: Test for not yet valid
+	/**
+	 * Tests the certificate for EndEntity7 signed by RootCA1.
+	 * The certificate is not valid as it is not signed width the right issuers key
+	 */
+	public void test07WrongIssuer() throws Exception {
+		ValidateRequest req = new ValidateRequest(ICertificateManager.genICertificate(certEndEntity7WrongIssuer), ValidationServiceConstants.CERTPURPOSE_NO_PURPOSE);
+		ValidateResponse res = (ValidateResponse) sSSession.process(15, req, new RequestContext());
+
+		Validation val = res.getValidation();
+		assertTrue(val != null);
+		assertFalse("certificate should not be valid", val.getStatus().equals(Validation.Status.VALID));
+		assertTrue(val.getStatusMessage() != null);
+		List<ICertificate> cAChain = val.getCAChain();
+		assertTrue(cAChain != null);
+		assertTrue(cAChain.get(0).getSubject().equals("CN=RootCA1"));
+		assertEquals("CN=EndEntity7", val.getCertificate().getSubject());
+	}
+	
+	/**
+	 * Tests the certificate for EndEntity1 signed by RootCA1.
+	 * The certificate is not valid as it is not signed width the right issuers key
+	 */
+	public void test08KeyUsageSignature() throws Exception {
+		
+		// First: test one certificate that has CERTPURPOSE_ELECTRONIC_SIGNATURE and see that it works
+		{
+			ValidateRequest req = new ValidateRequest(ICertificateManager.genICertificate(certEndEntity8KeyUsageSig), ValidationServiceConstants.CERTPURPOSE_ELECTRONIC_SIGNATURE);
+			ValidateResponse res = (ValidateResponse) sSSession.process(15, req, new RequestContext());
+	
+			Validation val = res.getValidation();
+			assertTrue(val != null);
+			assertTrue("certificate should be valid for electronic signature", val.getStatus().equals(Validation.Status.VALID));
+			assertTrue(val.getStatusMessage() != null);
+			List<ICertificate> cAChain = val.getCAChain();
+			assertTrue(cAChain != null);
+			assertTrue(cAChain.get(0).getSubject().equals("CN=RootCA1"));
+			assertEquals("CN=EndEntity8", val.getCertificate().getSubject());
+		}
+		
+		// Second: test one certificate without CERTPURPOSE_ELECTRONIC_SIGNATURE and see that it fails
+		{
+			ValidateRequest req = new ValidateRequest(ICertificateManager.genICertificate(certEndEntity1), ValidationServiceConstants.CERTPURPOSE_ELECTRONIC_SIGNATURE);
+			ValidateResponse res = (ValidateResponse) sSSession.process(15, req, new RequestContext());
+
+			Validation val = res.getValidation();
+			assertTrue(val != null);
+			assertFalse("certificate should fail is it does not have keyusage sig", val.getStatus().equals(Validation.Status.VALID));
+			assertTrue(val.getStatusMessage() != null);
+			List<ICertificate> cAChain = val.getCAChain();
+			assertTrue(cAChain != null);
+			assertTrue(cAChain.get(0).getSubject().equals("CN=RootCA1"));
+			assertEquals("CN=EndEntity1", val.getCertificate().getSubject());
+		}
+		
+	}
+
+	/**
+	 * Tests the certificate for EndEntity3 signed by RootCA2.
+	 * It has a distribution point with an issuer name.
+	 * The CRLPATHS property is removed which should make the validation fail.
+	 * Note: If this test fails the CRLPATHS property is not set, which might cause other tests to also fail.
+	 */
+	public void test09NoCRLPath() throws Exception {
+		
+		String crlPaths = sSSession.getCurrentWorkerConfig(15).getProperty("VAL1.ISSUER2.CRLPATHS");
+		
+		sSSession.setWorkerProperty(15, "VAL1.ISSUER2.CRLPATHS", "");
+		sSSession.reloadConfiguration(15);
+		
+		ValidateRequest req = new ValidateRequest(ICertificateManager.genICertificate(certEndEntity3), ValidationServiceConstants.CERTPURPOSE_NO_PURPOSE);
+		ValidateResponse res = (ValidateResponse) sSSession.process(15, req, new RequestContext());
+
+		Validation val = res.getValidation();
+		assertNotNull(val);
+		assertFalse("Not valid as no CRL should be found", Validation.Status.VALID.equals(val.getStatus()));
+		assertNotNull(val.getStatusMessage());
+		List<ICertificate> cAChain = val.getCAChain();
+		assertNotNull(cAChain);
+		assertTrue(cAChain.get(0).getSubject().equals("CN=RootCA2"));
+		assertEquals("CN=EndEntity3", val.getCertificate().getSubject());
+		
+		sSSession.setWorkerProperty(15, "VAL1.ISSUER2.CRLPATHS", crlPaths);
+		sSSession.reloadConfiguration(15);
+	}
 	
 	// TODO: Add more tests for the CRLValidator here
-
+	
 	public void test99RemoveDatabase() throws Exception {
 
 		gCSession.removeProperty(GlobalConfiguration.SCOPE_GLOBAL, "WORKER15.CLASSPATH");

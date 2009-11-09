d250ee7eda310e74bee4ac15588c208f460653d5
Ezizmuhammed Muhamedkuliyev <aziz@primekey.se>
2009-11-09 19:58:23 +0000

    patch written for digitally signing ooxml documents for openxml4j project (and maintained here ) is in trunk of openxml4j project now, so no need to maintain this project here any more.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@714 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- openxml4j_signaturepatched/src/org/openxml4j/exceptions/InvalidPackageURI.java
+++
@@ -1,9 +0,0 @@
-package org.openxml4j.exceptions;
-
-@SuppressWarnings("serial")
-public class InvalidPackageURI extends OpenXML4JException{
-
-	public InvalidPackageURI(String message){
-		super(message);
-	}
-}

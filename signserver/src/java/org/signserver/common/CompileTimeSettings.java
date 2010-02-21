d6e70d1525e05a50cbb6c94472e49209289e5c64
Markus Kilås <markus.kilas@primekey.com>
2010-02-21 21:59:36 +0000

    DSS-156: Class holding the settings.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@863 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/src/java/org/signserver/common/CompileTimeSettings.java
@@ -0,0 +1,114 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.common;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Settings loaded from built-in properties-file signservercompile.properties
+ * put together during compilation.
+ *
+ * @author Markus Kilås
+ * @version $Id$
+ */
+public class CompileTimeSettings {
+
+    public static final String APPNAME
+            = "appname";
+    public static final String APPNAME_CAP
+            = "appname_capital";
+    public static final String RMIREGISTRYPORT
+            = "rmiregistryport";
+    public static final String RMISERVERPORT
+            = "rmiserverport";
+    public static final String MAILSIGNERPORT
+            = "mailsignerport";
+    public static final String BUILDMODE
+            = "buildmode";
+    public static final String DATASOURCE_JNDINAMEPREFIX
+            = "datasource.jndi-name-prefix";
+    public static final String DATASOURCE_JNDINAME
+            = "datasource.jndi-name";
+    public static final String HEALTHECK_AUTHORZEDIPS
+            = "healthcheck.authorizedips";
+    public static final String HEALTHECK_MINIMUMFREEMEMORY
+            = "healthcheck.minimumfreememory";
+    public static final String HEALTHECK_CHECKDBSTRING
+            = "healthcheck.checkdbstring";
+    public static final String SIGNSERVER_USECLUSTERCLASSLOADER
+            = "signserver.useclusterclassloader";
+    public static final String SIGNSERVER_USECLASSVERSIONS
+            = "signserver.useclassversions";
+    public static final String SIGNSERVER_REQUIRESIGNATURE
+            = "signserver.requiresignature";
+    public static final String SIGNSERVER_PATHTOTRUSTSTORE
+            = "signserver.pathtotruststore";
+    public static final String SIGNSERVER_TRUSTSTOREPWD
+            = "signserver.truststorepwd";
+    public static final String SIGNSERVERCOMMANDFACTORY
+            = "SignServerCommandFactory";
+    public static final String SIGNSERVER_CONFIGFILE
+            = "signserver.configfile";
+    public static final String SIGNSERVER_VERSION
+            = "signserver.version";
+
+    /** Default values for the compile-time properties. */
+    private static final Properties DEFAULT_PROPERTIES = new Properties();
+
+    private static CompileTimeSettings instance;
+
+    /** Properties put together at compile-time. */
+    private Properties properties = new Properties(DEFAULT_PROPERTIES);
+
+    static {
+        // Setup default properties
+        DEFAULT_PROPERTIES.put(BUILDMODE, "SIGNSERVER");
+        DEFAULT_PROPERTIES.put(SIGNSERVER_USECLUSTERCLASSLOADER,
+                Boolean.toString(true));
+        DEFAULT_PROPERTIES.put(SIGNSERVER_USECLASSVERSIONS,
+                Boolean.toString(true));
+        DEFAULT_PROPERTIES.put(SIGNSERVER_REQUIRESIGNATURE,
+                Boolean.toString(false));
+    }
+
+    
+    private CompileTimeSettings() {
+        // Load built-in compile-time properties
+        try {
+            final InputStream in = GlobalConfiguration.class
+                    .getResourceAsStream("signservercompile.properties");
+            if (in == null) {
+                throw new FileNotFoundException("signservercompile.properties");
+            }
+            properties.load(in);
+        } catch (IOException ex) {
+            throw new RuntimeException(
+                    "Unable to load built-in signservercompile.properties", ex);
+        }
+    }
+
+    public static CompileTimeSettings getInstance() {
+        if (instance == null) {
+            instance = new CompileTimeSettings();
+        }
+        return instance;
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+}

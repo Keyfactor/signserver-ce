6d2107d0db20429c6ceac33773160b469b5353bf
Markus Kilås <markus.kilas@primekey.com>
2010-04-27 13:24:48 +0000

    DSS-242: Logging of IP addresses
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@998 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/src/java/org/signserver/server/IWorkerLogger.java
+++ signserver/src/java/org/signserver/server/IWorkerLogger.java
@@ -42,6 +42,7 @@ public interface IWorkerLogger {
     String LOG_REQUEST_FULLURL = "REQUEST_FULLURL";
     String LOG_REQUEST_LENGTH = "REQUEST_LENGTH";
     String LOG_FILENAME = "FILENAME";
+    String LOG_XFORWARDEDFOR = "XFORWARDEDFOR";
 
     String LOG_PURCHASED = "PURCHASED";
 

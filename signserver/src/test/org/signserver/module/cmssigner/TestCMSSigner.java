c87c0f01257c69f5a48e5bd0cdbfd0a451e4187c
Markus Kilås <markus.kilas@primekey.com>
2010-06-11 13:48:27 +0000

    DSS-247: Basic CMS signer
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@1068 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/src/test/org/signserver/module/cmssigner/TestCMSSigner.java
@@ -0,0 +1,184 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
+package org.signserver.module.cmssigner;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.security.cert.CertStore;
+import java.security.cert.Certificate;
+import java.util.Collection;
+
+
+import junit.framework.TestCase;
+
+import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.cms.ContentInfo;
+import org.bouncycastle.cms.CMSSignedData;
+import org.bouncycastle.cms.SignerInformation;
+import org.signserver.cli.CommonAdminInterface;
+import org.signserver.common.GenericSignRequest;
+import org.signserver.common.GenericSignResponse;
+import org.signserver.common.RequestContext;
+import org.signserver.common.SignServerUtil;
+import org.signserver.common.clusterclassloader.MARFileParser;
+import org.signserver.ejb.interfaces.IWorkerSession;
+import org.signserver.common.ServiceLocator;
+import org.signserver.testutils.TestUtils;
+import org.signserver.testutils.TestingSecurityManager;
+
+/**
+ * Tests for CMSSigner.
+ *
+ * @author Markus Kilas
+ * @version $Id$
+ */
+public class TestCMSSigner extends TestCase {
+
+    /** Logger for this class. */
+    private static final Logger LOG = Logger.getLogger(TestCMSSigner.class);
+
+    /**
+     * WORKERID used in this test case as defined in
+     * junittest-part-config.properties.
+     */
+    private static final int WORKERID = 5690;
+
+    private static IWorkerSession.IRemote workerSession;
+    private static String signserverhome;
+    private static int moduleVersion;
+	
+	
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        SignServerUtil.installBCProvider();
+        workerSession = ServiceLocator.getInstance().lookupRemote(
+                IWorkerSession.IRemote.class);
+        TestUtils.redirectToTempOut();
+        TestUtils.redirectToTempErr();
+        TestingSecurityManager.install();
+        signserverhome = System.getenv("SIGNSERVER_HOME");
+        assertNotNull("Please set SIGNSERVER_HOME environment variable", signserverhome);
+        CommonAdminInterface.BUILDMODE = "SIGNSERVER";
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        TestingSecurityManager.remove();
+    }	
+	
+    public void test00SetupDatabase() throws Exception {
+
+        final MARFileParser marFileParser = new MARFileParser(signserverhome
+                + "/dist-server/xmlsigner.mar");
+        moduleVersion = marFileParser.getVersionFromMARFile();
+
+        TestUtils.assertSuccessfulExecution(new String[] {
+                "module",
+                "add",
+                signserverhome + "/dist-server/cmssigner.mar",
+                "junittest"
+            });
+        assertTrue("Loading module",
+                TestUtils.grepTempOut("Loading module CMSSIGNER"));
+        assertTrue("Module loaded",
+                TestUtils.grepTempOut("Module loaded successfully."));
+
+        workerSession.reloadConfiguration(WORKERID);
+    }
+
+    /**
+     * Tests that the signer can produce a CMS structure and that it returns
+     * the signer's certficate and that it is included in the structure and
+     * that it can be used to verify the signature and that the signed content
+     * also is included.
+     * @throws Exception In case of error.
+     */
+    public void test01BasicCMSSignRSA() throws Exception {
+        LOG.debug(">test01BasicCMSSignRSA");
+
+        final int reqid = 37;
+
+        final String testDocument = "Something to sign...123";
+
+        final GenericSignRequest signRequest =
+                new GenericSignRequest(reqid, testDocument.getBytes());
+
+        final GenericSignResponse res =
+                (GenericSignResponse) workerSession.process(WORKERID,
+                    signRequest, new RequestContext());
+        final byte[] data = res.getProcessedData();
+
+        // Answer to right question
+        assertSame("Request ID", reqid, res.getRequestID());
+
+        // Output for manual inspection
+        final FileOutputStream fos = new FileOutputStream(
+                new File(signserverhome + File.separator
+                + "tmp" + File.separator + "signedcms_rsa.p7s"));
+        fos.write((byte[]) data);
+        fos.close();
+
+        // Check certificate returned
+        final Certificate signercert = res.getSignerCertificate();
+        assertNotNull("Signer certificate", signercert);
+
+        // Check that the signed data contains the document (i.e. not detached)
+        final CMSSignedData signedData = new CMSSignedData(data);
+        final ContentInfo ci = signedData.getContentInfo();
+        final byte[] content = (byte[]) signedData.getSignedContent()
+                .getContent();
+        assertEquals("Signed document", testDocument, new String(content));
+
+        // Get signers
+        final Collection signers = signedData.getSignerInfos().getSigners();
+        final SignerInformation signer
+                = (SignerInformation) signers.iterator().next();
+
+        // Verify using the signer's certificate
+        assertTrue("Verification using signer certificate",
+                signer.verify(signercert.getPublicKey(), "BC"));
+
+        // Check that the signer's certificate is included
+        CertStore certs = signedData.getCertificatesAndCRLs("Collection", "BC");
+        Collection<? extends Certificate> signerCerts
+                = certs.getCertificates(signer.getSID());
+        assertEquals("One certificate included", 1, signerCerts.size());
+        assertEquals(signercert, signerCerts.iterator().next());
+
+        LOG.debug("<test01BasicCMSSignRSA");
+    }
+
+    /**
+     * Remove the workers created etc.
+     * @throws Exception in case of error
+     */
+    public void test99TearDownDatabase() throws Exception {
+        TestUtils.assertSuccessfulExecution(new String[] {
+            "removeworker",
+            String.valueOf(WORKERID)
+        });
+
+        TestUtils.assertSuccessfulExecution(new String[] {
+            "module",
+            "remove",
+            "CMSSIGNER",
+            String.valueOf(moduleVersion)
+        });
+        assertTrue("module remove",
+                TestUtils.grepTempOut("Removal of module successful."));
+        workerSession.reloadConfiguration(WORKERID);
+    }
+}

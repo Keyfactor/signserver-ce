a6513ef2779729f395747cc695549c54b00584a9
Markus Kilås <markus.kilas@primekey.com>
2010-10-05 14:26:24 +0000

    DSS-189: Pattern based WorkerLogger.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@1212 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/SignServer-ejb/src/java/org/signserver/server/log/PatternWorkerLogger.java
@@ -0,0 +1,76 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.server.log;
+
+import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Pattern;
+import org.apache.log4j.Logger;
+import org.ejbca.util.IPatternLogger;
+import org.ejbca.util.PatternLogger;
+
+/**
+ * Completely configurable WorkerLogger.
+ *
+ * @version $Id$
+ */
+public class PatternWorkerLogger implements IWorkerLogger {
+
+    private Pattern pattern;
+    private String orderString;
+    private static final Logger ACCOUNTLOG =
+            Logger.getLogger(IWorkerLogger.class);
+    private String logDateFormat;
+    private String timeZone;
+
+    private static final String DEFAULT_LOGPATTERN =
+            "\\$\\{(.+?)\\}";
+
+    private static final String DEFAULT_LOGORDER =
+            "AUDIT; DefaultTimeStampLogger; "
+                + "LOG_ID: ${LOG_ID}; "
+                + "CLIENT_IP: ${CLIENT_IP}; "
+                + "REQUEST_FULLURL: ${REQUEST_FULLURL}; "
+                + "RequestTime: ${LOG_TIME}; "
+                + "ResponseTime: ${REPLY_TIME}; "
+                + "EXCEPTION: ${EXCEPTION}";
+
+    private static final String DEFAULT_LOGDATEFORMAT =
+            "yyyy-MM-dd:HH:mm:ss:z";
+
+    private static final String DEFAULT_LOGTIMEZONE = "GMT";
+
+    public PatternWorkerLogger() {
+    }
+
+    public void init(Properties properties) {
+        this.pattern = Pattern.compile(properties.getProperty("LOGPATTERN",
+                DEFAULT_LOGPATTERN));
+        this.orderString = properties.getProperty("LOGORDER", DEFAULT_LOGORDER);
+        this.logDateFormat = properties.getProperty("LOGDATEFORMAT", DEFAULT_LOGDATEFORMAT);
+        this.timeZone = properties.getProperty("LOGTIMEZONE", DEFAULT_LOGTIMEZONE);
+    }
+
+    public void log(Map<String, String> entries) throws WorkerLoggerException {
+        final IPatternLogger pl = new PatternLogger(this.pattern.matcher(
+                this.orderString), this.orderString, this.ACCOUNTLOG,
+                this.logDateFormat, this.timeZone);
+
+        // TODO: Do a new version of pattern logger instead of this copying
+        for (Map.Entry<String, String> entry : entries.entrySet()) {
+            pl.paramPut(entry.getKey(), entry.getValue());
+        }
+        pl.writeln();
+        pl.flush();
+    }
+}

f10d65b43a944f2936adf2f227091dd6b13ef3e4
Markus Kilås <markus.kilas@primekey.com>
2010-12-09 21:33:09 +0000

    DSS-125: Removed moved tests.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@1387 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/src/test/org/signserver/server/clusterclassloader/testcode/SomeClass.java
+++
@@ -1,9 +0,0 @@
-package org.signserver.server.clusterclassloader.testcode;
-
-public class SomeClass {
-	
-	public SomeClass(){
-		System.out.println("SomeClass initialized : " + this.getClass().getName());
-	}
-
-}

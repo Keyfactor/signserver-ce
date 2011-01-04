56769aa10d75c50decbef06363cf1837696a08cc
Markus Kilås <markus.kilas@primekey.com>
2011-01-04 15:25:10 +0000

    DSS-202: Fixed classpath in start script for Client CLI.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@1591 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/bin/client.sh
+++ signserver/bin/client.sh
@@ -19,6 +19,25 @@ else
 fi
 
 
+class_name=org.signserver.client.cli.Main
+MAIN_JAR=SignServer-Client-CLI.jar
+
+# discard $1 from the command line args
+#shift
+
+# J2EE server classpath
+if [ ! -n "$APPSRV_HOME" ]; then
+    if [ -n "$JBOSS_HOME" ]; then
+        APPSRV_HOME=$JBOSS_HOME
+    elif [ -n "$WEBLOGIC_HOME" ]; then
+        APPSRV_HOME=$WEBLOGIC_HOME
+    fi
+fi
+
+if [ ! -n "$APPSRV_HOME" ]; then
+    echo "To connect to a local SignServer please set APPSRV_HOME first."
+fi
+
 if [ ! -n "${SIGNSERVER_HOME}" ]; then
   if [ -f /etc/signserver/signservermgmt.env ]; then
      . /etc/signserver/signservermgmt.env
@@ -43,18 +62,35 @@ if [ ! -n "${SIGNSERVER_HOME}" ]; then
   fi
 else
   SIGNSRV_HOME=$SIGNSERVER_HOME
-fi
+fi 
 
 # Check that classes exist
-if [ ! -f ${SIGNSRV_HOME}/modules/SignServer-Client-CLI/dist/SignServer-Client-CLI.jar ]
+if [ ! -f ${SIGNSRV_HOME}/modules/SignServer-Client-CLI/dist/${MAIN_JAR} ]
 then
-	echo "You must build SignServer before using the CLI, use 'ant'."
+	echo "You must build SignServer Client CLI first."
         exit 1
 fi
+
+#CP="${SIGNSRV_HOME}/bin/jndi.properties":"$CP"
+
+if [ -r "$APPSRV_HOME"/lib/appserv-rt.jar ]; then
+    echo Using GlassFish JNDI provider...
+    CP=$CP:"$APPSRV_HOME"/lib/appserv-rt.jar
+else
+    echo "Assuming JBoss JNDI provider..."
+    CP=$CP:"$APPSRV_HOME/client/jbossall-client.jar"
+fi
+
+#CP="$CP":"$SIGNSRV_HOME/modules/SignServer-Client-CLI/dist/${MAIN_JAR}"
+
+CP="$CP:lib/log4j.jar:lib/ext/commons-cli-1.0.jar:lib/1.6/bcprov-jdk.jar:lib/cert-cvc.jar:lib/commons-lang-2.4.jar:lib/ejbca-util.jar:lib/1.6/bctsp-jdk.jar:lib/1.6/bcmail-jdk.jar:dist-client/timestampclient/timeStampClient.jar:modules/SignServer-Common/dist/SignServer-Common.jar:modules/SignServer-Client-SigningAndValidationAPI/dist/SignServer-Client-SigningAndValidationAPI.jar:modules/SignServer-Client-SignServerWS/dist/SignServer-Client-SignServerWS.jar:modules/SignServer-ejb-SignServerWS/dist/SignServer-ejb-SignServerWS.jar:modules/SignServer-Client-ValidationCLI/dist/SignServer-Client-ValidationCLI.jar:modules/SignServer-Client-CLI/dist/SignServer-Client-CLI.jar"
+
+
+export SIGNSRV_HOME
 
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
   CP=`cygpath --path --windows "$CP"`
 fi
 
-exec "$JAVACMD" -jar "${SIGNSRV_HOME}/modules/SignServer-Client-CLI/dist/SignServer-Client-CLI.jar" $@
+exec "$JAVACMD" -cp $CP $class_name "$@"

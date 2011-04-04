49c02c606a3e50f1b187213712dd33d3045030c3
Markus Kilås <markus.kilas@primekey.com>
2011-04-04 13:36:42 +0000

    DSS-353: Cleaned up verification of certificates
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@1660 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-MRTDSODSigner/src/java/org/signserver/module/mrtdsodsigner/MRTDSODSigner.java
+++ signserver/modules/SignServer-Module-MRTDSODSigner/src/java/org/signserver/module/mrtdsodsigner/MRTDSODSigner.java
@@ -12,8 +12,6 @@
  *************************************************************************/
 package org.signserver.module.mrtdsodsigner;
 
-
-
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
@@ -59,8 +57,9 @@ import org.signserver.server.signers.BaseSigner;
  *  <li>DODATAGROUPHASHING = True if this signer first should hash to values. Otherwise
  * the values are assumed to be hashes</li>
  * </ul>
- * 
- * @author Markus Kilas
+ *
+ * @author Tomas Gustavsson
+ * @author Markus Kilås
  * @version $Id$
  */
 public class MRTDSODSigner extends BaseSigner {
@@ -97,22 +96,22 @@ public class MRTDSODSigner extends BaseSigner {
 
     private static final String DEFAULT_UNICODEVERSION = "040000";
 
-    private static Object syncObj = new Object();
+    private static final Object syncObj = new Object();
     
     public ProcessResponse processData(ProcessRequest signRequest, RequestContext requestContext) throws IllegalRequestException, CryptoTokenOfflineException, SignServerException {
         if (log.isTraceEnabled()) {
             log.trace(">processData");
         }
         ProcessResponse ret = null;
-        ISignRequest sReq = (ISignRequest) signRequest;
+        final ISignRequest sReq = (ISignRequest) signRequest;
 
         // Check that the request contains a valid SODSignRequest object.
         if (!(signRequest instanceof SODSignRequest)) {
             throw new IllegalRequestException("Recieved request wasn't an expected SODSignRequest.");
         }
-        SODSignRequest sodRequest = (SODSignRequest) signRequest;
+        final SODSignRequest sodRequest = (SODSignRequest) signRequest;
 
-        ICryptoToken token = getCryptoToken();
+        final ICryptoToken token = getCryptoToken();
         // Trying to do a workaround for issue when the PKCS#11 session becomes invalid
         // If autoactivate is on, we can deactivate and re-activate the token.
         synchronized (syncObj) {
@@ -139,9 +138,9 @@ public class MRTDSODSigner extends BaseSigner {
         		}
         	}
         }
-        X509Certificate cert = (X509Certificate) getSigningCertificate();
-        PrivateKey privKey = token.getPrivateKey(ICryptoToken.PURPOSE_SIGN);
-        String provider = token.getProvider(ICryptoToken.PURPOSE_SIGN);
+        final X509Certificate cert = (X509Certificate) getSigningCertificate();
+        final PrivateKey privKey = token.getPrivateKey(ICryptoToken.PURPOSE_SIGN);
+        final String provider = token.getProvider(ICryptoToken.PURPOSE_SIGN);
 
         if (cert == null) {
             throw new CryptoTokenOfflineException("No signing certificate");
@@ -151,77 +150,77 @@ public class MRTDSODSigner extends BaseSigner {
         	log.debug("Using signer certificate with subjectDN '"+CertTools.getSubjectDN(cert)+"', issuerDN '"+CertTools.getIssuerDN(cert)+", serNo "+CertTools.getSerialNumberAsString(cert));
         }
         // Construct SOD
-        SODFile sod;
+        final SODFile sod;
         try {
-        	// Create the SODFile using the data group hashes that was sent to us in the request.
-        	String digestAlgorithm = config.getProperty(PROPERTY_DIGESTALGORITHM, DEFAULT_DIGESTALGORITHM);
-        	String digestEncryptionAlgorithm = config.getProperty(PROPERTY_SIGNATUREALGORITHM, DEFAULT_SIGNATUREALGORITHM);
-        	if (log.isDebugEnabled()) {
-        		log.debug("Using algorithms "+digestAlgorithm+", "+digestEncryptionAlgorithm);
-        	}
-        	String doHashing = config.getProperty(PROPERTY_DODATAGROUPHASHING, DEFAULT_DODATAGROUPHASHING);
-        	Map<Integer, byte[]> dgvalues = sodRequest.getDataGroupHashes();
-        	Map<Integer, byte[]> dghashes = dgvalues;
-        	if (StringUtils.equalsIgnoreCase(doHashing, "true")) {
-        		if (log.isDebugEnabled()) {
-                	log.debug("Converting data group values to hashes using algorithm "+digestAlgorithm);        			
-        		}
-        		// If true here the "data group hashes" are not really hashes but values that we must hash.
-            	// The input is already decoded (if needed) and nice, so we just need to hash it
-        		dghashes = new HashMap<Integer, byte[]>(16);
-        		for (Integer dgId : dgvalues.keySet()) {
-        			byte[] value = dgvalues.get(dgId);
-            		if (log.isDebugEnabled()) {
-            			log.debug("Hashing data group "+dgId+", value is of length: "+value.length);
-            		}
-        			if ( (value != null) && (value.length > 0) ) {
-                		MessageDigest digest = MessageDigest.getInstance(digestAlgorithm);
-        				byte[] result = digest.digest(value);
-                		if (log.isDebugEnabled()) {
-                			log.debug("Resulting hash is of length: "+result.length);
-                		}
-                    	dghashes.put(dgId, result);        				
-        			}
-				}
-        	}
-
-                // Version values from configuration
-                String ldsVersion = config.getProperty(PROPERTY_LDSVERSION,
-                        DEFAULT_LDSVERSION);
-                String unicodeVersion
-                        = config.getProperty(PROPERTY_UNICODEVERSION);
-
-                // Version values in request overrides configuration
-                final String ldsVersionRequest
-                        = sodRequest.getLdsVersion();
-                if (ldsVersionRequest != null) {
-                    ldsVersion = ldsVersionRequest;
-                }
-                final String unicodeVersionRequest
-                        = sodRequest.getUnicodeVersion();
-                if (unicodeVersionRequest != null) {
-                    unicodeVersion = unicodeVersionRequest;
-                }
-
-                // Check version
-                if ("0107".equals(ldsVersion)) {
-                    // LDS V1.7 does not supported the version fields
-                    ldsVersion = null;
-                    unicodeVersion = null;
-                } else if ("0108".equals(ldsVersion)) {
-                    // LDS V1.8 requires a unicode version
-                    if (unicodeVersion == null) {
-                        throw new IllegalRequestException(
-                        "Unicode version must be specified in LDS version 1.8");
-                    }
-                } else {
-                    throw new IllegalRequestException(
-                            "Unsupported LDS version: " + ldsVersion);
-                }
+            // Create the SODFile using the data group hashes that was sent to us in the request.
+            final String digestAlgorithm = config.getProperty(PROPERTY_DIGESTALGORITHM, DEFAULT_DIGESTALGORITHM);
+            final String digestEncryptionAlgorithm = config.getProperty(PROPERTY_SIGNATUREALGORITHM, DEFAULT_SIGNATUREALGORITHM);
+            if (log.isDebugEnabled()) {
+                    log.debug("Using algorithms "+digestAlgorithm+", "+digestEncryptionAlgorithm);
+            }
+            final String doHashing = config.getProperty(PROPERTY_DODATAGROUPHASHING, DEFAULT_DODATAGROUPHASHING);
+            final Map<Integer, byte[]> dgvalues = sodRequest.getDataGroupHashes();
+            Map<Integer, byte[]> dghashes = dgvalues;
+            if (StringUtils.equalsIgnoreCase(doHashing, "true")) {
                 if (log.isDebugEnabled()) {
-                    log.debug("LDS version: " + ldsVersion
-                            + ", unicodeVerison: " + unicodeVersion);
+                    log.debug("Converting data group values to hashes using algorithm "+digestAlgorithm);
                 }
+                // If true here the "data group hashes" are not really hashes but values that we must hash.
+                // The input is already decoded (if needed) and nice, so we just need to hash it
+                dghashes = new HashMap<Integer, byte[]>(16);
+                for (Integer dgId : dgvalues.keySet()) {
+                    byte[] value = dgvalues.get(dgId);
+                    if (log.isDebugEnabled()) {
+                        log.debug("Hashing data group "+dgId+", value is of length: "+value.length);
+                    }
+                    if ( (value != null) && (value.length > 0) ) {
+                        MessageDigest digest = MessageDigest.getInstance(digestAlgorithm);
+                                byte[] result = digest.digest(value);
+                        if (log.isDebugEnabled()) {
+                            log.debug("Resulting hash is of length: "+result.length);
+                        }
+                        dghashes.put(dgId, result);
+                    }
+                }
+            }
+
+            // Version values from configuration
+            String ldsVersion = config.getProperty(PROPERTY_LDSVERSION,
+                    DEFAULT_LDSVERSION);
+            String unicodeVersion
+                    = config.getProperty(PROPERTY_UNICODEVERSION);
+
+            // Version values in request overrides configuration
+            final String ldsVersionRequest
+                    = sodRequest.getLdsVersion();
+            if (ldsVersionRequest != null) {
+                ldsVersion = ldsVersionRequest;
+            }
+            final String unicodeVersionRequest
+                    = sodRequest.getUnicodeVersion();
+            if (unicodeVersionRequest != null) {
+                unicodeVersion = unicodeVersionRequest;
+            }
+
+            // Check version
+            if ("0107".equals(ldsVersion)) {
+                // LDS V1.7 does not supported the version fields
+                ldsVersion = null;
+                unicodeVersion = null;
+            } else if ("0108".equals(ldsVersion)) {
+                // LDS V1.8 requires a unicode version
+                if (unicodeVersion == null) {
+                    throw new IllegalRequestException(
+                    "Unicode version must be specified in LDS version 1.8");
+                }
+            } else {
+                throw new IllegalRequestException(
+                        "Unsupported LDS version: " + ldsVersion);
+            }
+            if (log.isDebugEnabled()) {
+                log.debug("LDS version: " + ldsVersion
+                        + ", unicodeVerison: " + unicodeVersion);
+            }
 
             final SODFile constructedSod
                     = new SODFile(digestAlgorithm, digestEncryptionAlgorithm,
@@ -241,57 +240,20 @@ public class MRTDSODSigner extends BaseSigner {
 
         // Verify the Signature before returning
         try {
-        	if (log.isDebugEnabled()) {
-        		log.debug("Verifying SOD signed by DS with issuer: "+sod.toString());
-        	}
-			boolean verify = sod.checkDocSignature(cert);
-			if (!verify) {
-				log.error("Failed to verify the SOD we signed ourselves.");
-				log.error("Cert: "+cert);
-				log.error("SOD: "+sod);
-				throw new SignServerException("Failed to verify the SOD we signed ourselves.");
-			} else {
-                            // Get chain and signer certificate
-                            final Collection<Certificate> chain = getSigningCertificateChain();
-                            final X509Certificate sodCert
-                                    = sod.getDocSigningCertificate();
-                            try {
-                                // We need a Bouncy Castle certificate
-                                final CertificateFactory factory
-                                        = CertificateFactory.getInstance("X.509", "BC");
-                                final X509Certificate signerCert = (X509Certificate)factory.generateCertificate(
-                                        new ByteArrayInputStream(sodCert.getEncoded()));
+            verifySignatureAndChain(sod, getSigningCertificateChain());
 
-                                // Find the issuer certificate and use it for verification
-                                final X509Certificate issuerCert = (chain == null ? null : findIssuerCert(chain, signerCert));
-                                if (issuerCert == null) {
-                                    log.error("Failed to verify certificate chain");
-                                    log.error("Cert: " + cert);
-                                    log.error("SOD Cert: " + signerCert);
-                                    log.error("Chain: " + chain);
-                                    throw new GeneralSecurityException("Issuer of cert not in chain");
-                                }
-                                signerCert.verify(issuerCert.getPublicKey());
-
-                                if (log.isDebugEnabled()) {
-                                        log.debug("SOD verified correctly, returning SOD.");
-                                }
-                                // Return response
-                                byte[] signedbytes = sod.getEncoded();
-                                String fp = CertTools.getFingerprintAsString(signedbytes);
-                                ret = new SODSignResponse(sReq.getRequestID(), signedbytes, cert, fp, new ArchiveData(signedbytes));
-                            } catch (GeneralSecurityException ex) {
-                                log.error("Error verifying certificate in the SOD we signed ourselves. ", ex);
-                                throw new SignServerException("SOD verification failure", ex);
-                            }
-			}
-		} catch (GeneralSecurityException e) {
-			log.error("Error verifying the SOD we signed ourselves. ", e);
-                        throw new SignServerException("SOD verification failure", e);
-		} catch (IOException e) {
-                    log.error("Error verifying the SOD we signed ourselves. ", e);
-                    throw new SignServerException("SOD verification failure", e);
-                }
+            if (log.isDebugEnabled()) {
+                log.debug("SOD verified correctly, returning SOD.");
+            }
+            // Return response
+            final byte[] signedbytes = sod.getEncoded();
+            String fp = CertTools.getFingerprintAsString(signedbytes);
+            ret = new SODSignResponse(sReq.getRequestID(), signedbytes, cert,
+                    fp, new ArchiveData(signedbytes));
+        } catch (GeneralSecurityException e) {
+                log.error("Error verifying the SOD we signed ourselves. ", e);
+                throw new SignServerException("SOD verification failure", e);
+        }
 
         if (log.isTraceEnabled()) {
             log.trace("<processData");
@@ -302,20 +264,81 @@ public class MRTDSODSigner extends BaseSigner {
     private X509Certificate findIssuerCert(Collection<Certificate> chain, X509Certificate sodCert) {
         X509Certificate result = null;
         final X509Name issuer = new X509Name(sodCert.getIssuerX500Principal().getName());
-        log.debug("Looking for " + issuer);
+        if (log.isDebugEnabled()) {
+            final StringBuilder buff = new StringBuilder();
+            buff.append("Looking for ");
+            buff.append(issuer);
+            log.debug(buff.toString());
+        }
         for (Certificate cert : chain) {
             if (cert instanceof X509Certificate) {
                 final X509Certificate x509 = (X509Certificate) cert;
                 final X509Name subject = new X509Name(x509.getSubjectX500Principal().getName());
                 if (issuer.equals(subject)) {
                     result = (X509Certificate) cert;
-                    log.debug("Found issuer");
+                    if (log.isDebugEnabled()) {
+                        log.debug("Found issuer");
+                    }
                     break;
                 } else {
-                    log.debug(issuer + "!=" + subject);
+                    if (log.isDebugEnabled()) {
+                        final StringBuilder buff = new StringBuilder();
+                        buff.append(issuer);
+                        buff.append("!=");
+                        buff.append(subject);
+                        log.debug(buff.toString());
+                    }
                 }
             }
         }
         return result;
+    }
+
+    private void verifySignatureAndChain(final SODFile sod,
+            final Collection<Certificate> chain) throws GeneralSecurityException {
+        try {
+            if (log.isDebugEnabled()) {
+                final StringBuilder buff = new StringBuilder();
+                buff.append("Verifying SOD signed by DS with issuer: ");
+                buff.append(sod.toString());
+                log.debug(buff.toString());
+            }
+
+            // Get Signer certificate from SOD
+            final X509Certificate sodCert = sod.getDocSigningCertificate();
+
+            // We need a Bouncy Castle certificate so reconstruct it
+            final CertificateFactory factory
+                    = CertificateFactory.getInstance("X.509", "BC");
+            final X509Certificate signerCert
+                    = (X509Certificate) factory.generateCertificate(
+                        new ByteArrayInputStream(sodCert.getEncoded()));
+
+            // Verify the SOD signature using certificate from SOD
+            final boolean consistent = sod.checkDocSignature(signerCert);
+            if (!consistent) {
+                log.error("Failed to verify the SOD we signed ourselves.");
+                log.error("Cert: " + signerCert);
+                log.error("SOD: " + sod);
+                throw new GeneralSecurityException("Signature not consistent");
+            }
+
+            // Find the issuer certificate from the configurated chain
+            final X509Certificate issuerCert = (chain == null ? null : findIssuerCert(chain, signerCert));
+            if (issuerCert == null) {
+                log.error("Failed to verify certificate chain");
+                log.error("Cert: " + signerCert);
+                log.error("SOD Cert: " + signerCert);
+                log.error("Chain: " + chain);
+                throw new GeneralSecurityException("Issuer of cert not in chain");
+            }
+
+            // Verify the signer certificate using the issuer from the chain
+            signerCert.verify(issuerCert.getPublicKey());
+        } catch (IOException e) {
+            log.error("Getting signer certificate from SOD failed", e);
+            throw new GeneralSecurityException(
+                    "Getting signer certificate from SOD failed", e);
+        }
     }
 }

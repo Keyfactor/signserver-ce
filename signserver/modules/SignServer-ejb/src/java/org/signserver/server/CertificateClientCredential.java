6c489576b98d01dcb02e9d03f8c71e22238fb133
Johan Eklund <johan.eklund@primekey.com>
2011-05-03 14:40:17 +0000

    NoTicket. Add LGPL header and SVN version keyword.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@1685 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-ejb/src/java/org/signserver/server/CertificateClientCredential.java
+++ signserver/modules/SignServer-ejb/src/java/org/signserver/server/CertificateClientCredential.java
@@ -1,5 +1,22 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+
 package org.signserver.server;
 
+/**
+ * Representation of a X509 client certificate for authentication use.
+ * @version $Id$
+ */
 public class CertificateClientCredential implements IClientCredential {
 
     private String serialNumber;

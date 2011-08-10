80708db76621ce67d551298850d03cac5873fa88
Markus Kilås <markus.kilas@primekey.com>
2011-08-10 10:25:09 +0000

    DSS-415: Source code formatting and trivial changes.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@1826 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-ejb/src/java/org/signserver/server/signers/DummySigner.java
+++ signserver/modules/SignServer-ejb/src/java/org/signserver/server/signers/DummySigner.java
@@ -10,14 +10,13 @@
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
  *************************************************************************/
-
 package org.signserver.server.signers;
 
 import org.apache.log4j.Logger;
 import org.signserver.common.CryptoTokenAuthenticationFailureException;
 import org.signserver.common.CryptoTokenOfflineException;
+import org.signserver.common.GenericServletResponse;
 import org.signserver.common.GenericSignRequest;
-import org.signserver.common.GenericSignResponse;
 import org.signserver.common.IllegalRequestException;
 import org.signserver.common.ProcessRequest;
 import org.signserver.common.ProcessResponse;
@@ -29,99 +28,98 @@ import org.signserver.common.WorkerStatus;
 
 /**
  * Dummy Signer used for test and demonstration purposes.
- * 
- * 
- * @author Philip Vendil 17 dec 2007
  *
+ * @author Philip Vendil 17 dec 2007
  * @version $Id$
  */
-
 public class DummySigner extends BaseSigner {
-	private transient Logger log = Logger.getLogger(this.getClass());
 
-	/**
-	 * Time to wait before sending back the response.
-	 */
-	public static final String WAITTIME = "WAITTIME";
-	
-	/**
-	 * Default wait time i milliseconds.
-	 */
-	public static final String DEFAULT_WAITTIME = "1000";
-	
-	private boolean active = true;
-	
-	/**
-	 * Method that does nothing, more than returning the data sent after
-	 * the configured milliseconds. Also simulates CryptTokenOfflineException
-	 * if the token is off-line.
-	 * 
-	 *  Expects GenericSignRequests
-	 * 
-	 * @see org.signserver.server.IProcessable#processData(org.signserver.common.ProcessRequest, org.signserver.common.RequestContext)
-	 */
-	public ProcessResponse processData(ProcessRequest signRequest,
-			RequestContext requestContext) throws IllegalRequestException,
-			CryptoTokenOfflineException, SignServerException {
+    /** Logger for this class. */
+    private static final Logger LOG = Logger.getLogger(DummySigner.class);
+    
+    /**
+     * Time to wait before sending back the response.
+     */
+    public static final String WAITTIME = "WAITTIME";
 
-		if(!active){
-			throw new CryptoTokenOfflineException("Error crypto token is offline.");
-		}
-		
-		try {
-			Thread.sleep(getWaitTime());
-		} catch (InterruptedException e) {
-			log.info(e.getMessage());
-		}
-		
-		GenericSignRequest req = (GenericSignRequest) signRequest;
-		
-		return new GenericSignResponse(req.getRequestID(),req.getRequestData(),null,null,null);
-	}
+    /**
+     * Default wait time i milliseconds.
+     */
+    public static final String DEFAULT_WAITTIME = "1000";
+    
+    private boolean active = true;
+    
+    private Long waitTime;
 
-	private Long waitTime = null;
-	private long getWaitTime() {
-		if(waitTime == null){
-			String waitTimeString = config.getProperties().getProperty(WAITTIME,DEFAULT_WAITTIME);
-			try{
-               waitTime = Long.parseLong(waitTimeString);
-			}catch(NumberFormatException e){
-				log.error("Property " + WAITTIME + " missconfigured, should only contain integers");
-			}
-		}
-		return waitTime;
-	}
+    /**
+     * Method that does nothing, more than returning the data sent after
+     * the configured milliseconds. Also simulates CryptTokenOfflineException
+     * if the token is off-line.
+     * 
+     *  Expects GenericSignRequests
+     * 
+     * @see org.signserver.server.IProcessable#processData(org.signserver.common.ProcessRequest, org.signserver.common.RequestContext)
+     */
+    public ProcessResponse processData(ProcessRequest signRequest,
+            RequestContext requestContext) throws IllegalRequestException,
+            CryptoTokenOfflineException, SignServerException {
 
-	/**
-	 * Dummy implementation that doesn't check the auth code
-	 * @see org.signserver.server.BaseProcessable#activateSigner(java.lang.String)
-	 */
-	@Override
-	public void activateSigner(String authenticationCode)
-			throws CryptoTokenAuthenticationFailureException,
-			CryptoTokenOfflineException {
-		this.active = true;
-	}
+        if (!active) {
+            throw new CryptoTokenOfflineException("Error crypto token is offline.");
+        }
 
-	/* (non-Javadoc)
-	 * @see org.signserver.server.BaseProcessable#deactivateSigner()
-	 */
-	@Override
-	public boolean deactivateSigner() throws CryptoTokenOfflineException {
-		this.active = false;
-		return true;
-	}
+        try {
+            Thread.sleep(getWaitTime());
+        } catch (InterruptedException e) {
+            LOG.info(e.getMessage());
+        }
 
-	/**
-	 * @see org.signserver.server.signers.BaseSigner#getStatus()
-	 */
-	@Override
-	public WorkerStatus getStatus() {
-		if(active){
-		  return  new SignerStatus(workerId,  SignerStatus.STATUS_ACTIVE, new ProcessableConfig( config), null);
-		}else{
-			return  new SignerStatus(workerId, SignerStatus.STATUS_OFFLINE, new ProcessableConfig( config), null);
-		}
-	}
+        GenericSignRequest req = (GenericSignRequest) signRequest;
 
+        return new GenericServletResponse(req.getRequestID(), req.getRequestData(), null, null, null, "text/plain");
+    }
+
+    private long getWaitTime() {
+        if (waitTime == null) {
+            String waitTimeString = config.getProperties().getProperty(WAITTIME, DEFAULT_WAITTIME);
+            try {
+                waitTime = Long.parseLong(waitTimeString);
+            } catch (NumberFormatException e) {
+                LOG.error("Property " + WAITTIME + " missconfigured, should only contain integers");
+            }
+        }
+        return waitTime;
+    }
+
+    /**
+     * Dummy implementation that doesn't check the auth code
+     * @see org.signserver.server.BaseProcessable#activateSigner(java.lang.String)
+     */
+    @Override
+    public void activateSigner(String authenticationCode)
+            throws CryptoTokenAuthenticationFailureException,
+            CryptoTokenOfflineException {
+        this.active = true;
+    }
+
+    /* (non-Javadoc)
+     * @see org.signserver.server.BaseProcessable#deactivateSigner()
+     */
+    @Override
+    public boolean deactivateSigner() throws CryptoTokenOfflineException {
+        this.active = false;
+        return true;
+    }
+
+    /**
+     * @see org.signserver.server.signers.BaseSigner#getStatus()
+     */
+    @Override
+    public WorkerStatus getStatus() {
+        if (active) {
+            return new SignerStatus(workerId, SignerStatus.STATUS_ACTIVE, new ProcessableConfig(config), null);
+        } else {
+            return new SignerStatus(workerId, SignerStatus.STATUS_OFFLINE, new ProcessableConfig(config), null);
+        }
+    }
 }

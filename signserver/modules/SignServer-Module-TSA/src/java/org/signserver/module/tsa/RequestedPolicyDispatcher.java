87287deeb62b8d510d86c2096851fb9d77452e80
Markus Kilås <markus.kilas@primekey.com>
2012-02-13 09:48:09 +0000

    DSS-213: Dispatching based on requested TSA policy
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@2108 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Module-TSA/src/java/org/signserver/module/tsa/RequestedPolicyDispatcher.java
@@ -0,0 +1,238 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.module.tsa;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import javax.naming.NamingException;
+import javax.persistence.EntityManager;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.cmp.PKIFailureInfo;
+import org.bouncycastle.asn1.cmp.PKIStatus;
+import org.bouncycastle.tsp.TSPException;
+import org.bouncycastle.tsp.TimeStampRequest;
+import org.bouncycastle.tsp.TimeStampResponse;
+import org.signserver.common.*;
+import org.signserver.ejb.interfaces.IWorkerSession;
+import org.signserver.ejb.interfaces.IWorkerSession.IRemote;
+import org.signserver.server.ITimeStampSignerLookup;
+import org.signserver.server.WorkerContext;
+import org.signserver.server.dispatchers.BaseDispatcher;
+import org.signserver.server.log.IWorkerLogger;
+import org.signserver.server.tsa.org.bouncycastle.tsp.TimeStampResponseGenerator;
+
+/**
+ * Dispatching requests to a Time Stamp Unit based on the requested profile.
+ *
+ * Properties:<br/>
+ * DEFAULTWORKER - Worker name or ID to dispatch to in case no policy was requested.
+ * USEDEFAULTIFMISMATCH - If true dispatches to DEFAULTWORKER in case no mapping existed for the requested policy OID (default: false)
+ * MAPPINGS - Mapping from requested policy OID to a worker name.
+ * The property is of the form:
+ * POLICYOID1:WORKERNAMEORID1; POLICYOID2:WORKERNAMEORID2; POLICYOID3:WORKERNAMEORID2;
+ *
+ * @author Markus Kilås
+ * @version $Id$
+ */
+public class RequestedPolicyDispatcher extends BaseDispatcher {
+
+    /** Log4j instance for this class. */
+    private static final Logger LOG = Logger.getLogger(
+            RequestedPolicyDispatcher.class);
+
+    /** Workersession. */
+    private IWorkerSession.IRemote workerSession;
+    
+    private static final String MAPPINGS = "MAPPINGS";
+    private static final String DEFAULTWORKER = "DEFAULTWORKER";
+    private static final String USEDEFAULTIFMISMATCH = "USEDEFAULTIFMISMATCH";
+
+
+    private final Random random = new Random();
+    
+        private static final String REQUEST_CONTENT_TYPE
+            = "application/timestamp-query";
+    private static final String RESPONSE_CONTENT_TYPE
+            = "application/timestamp-reply";
+    
+    private Map<String, String> workerMapping = new HashMap<String, String>();
+    
+    private String defaultWorker;
+    private boolean useDefaultIfMismatch;
+    
+    @Override
+    public void init(final int workerId, final WorkerConfig config,
+            final WorkerContext workerContext, final EntityManager workerEM) {
+        try {
+            super.init(workerId, config, workerContext, workerEM);
+
+            String policyWorkerMapping = config.getProperty(MAPPINGS);
+            if (policyWorkerMapping == null) {
+                LOG.error("Property " + MAPPINGS + " missing!");
+            } else {
+                workerMapping = parseMapping(policyWorkerMapping);
+            }
+            
+            defaultWorker = config.getProperty(DEFAULTWORKER);
+            
+            useDefaultIfMismatch = Boolean.parseBoolean(config.getProperty(USEDEFAULTIFMISMATCH, "false"));
+            
+            if (LOG.isDebugEnabled()) {
+                LOG.debug(new StringBuilder()
+                        .append("workerMapping: ").append(workerMapping).append("\n")
+                        .append("defaultWorker: ").append(defaultWorker).append("\n")
+                        .append("useDefaultIfMismatch: ").append(useDefaultIfMismatch).toString());
+            }
+            
+            workerSession = ServiceLocator.getInstance().lookupRemote(
+                        IWorkerSession.IRemote.class);
+        } catch (NamingException ex) {
+            LOG.error("Unable to lookup worker session", ex);
+        }
+    }
+
+    @Override
+    public ProcessResponse processData(final ProcessRequest signRequest,
+            final RequestContext context) throws IllegalRequestException,
+            CryptoTokenOfflineException, SignServerException {
+        final GenericSignResponse result;
+        
+        // Log values
+        final Map<String, String> logMap = (Map<String, String>) context.get(RequestContext.LOGMAP);
+        
+        // Check context
+        final RequestContext nextContext = context;
+        if (context.get(this.getClass().getName()) != null) {
+            throw new SignServerException("Dispatcher called more then one time for the same request. Aborting.");
+        } else {
+            context.put(this.getClass().getName(), "called");
+        }
+
+        // Check that the request contains a valid TimeStampRequest object.
+        if (!(signRequest instanceof GenericSignRequest)) {
+            throw new IllegalRequestException("Recieved request wasn't a expected GenericSignRequest.");
+        }
+        final ISignRequest sReq = (ISignRequest) signRequest;
+        
+        // Get TimeStampRequest
+        final TimeStampRequest timeStampRequest;
+        if (sReq.getRequestData() instanceof TimeStampRequest) {
+            timeStampRequest = (TimeStampRequest) sReq.getRequestData();
+        } else if (sReq.getRequestData() instanceof byte[]) {
+            try {
+                timeStampRequest =  new TimeStampRequest((byte[]) sReq.getRequestData());
+            } catch (IOException ex) {
+                throw new IllegalRequestException("Could not parse TimeStampRequest", ex);
+            }
+        } else {
+            throw new IllegalRequestException("Expected a TimeStampRequest");
+        }
+        
+        try {
+            // Add to context
+            if (timeStampRequest.getReqPolicy() != null) {
+                nextContext.put(ITimeStampSignerLookup.TSA_REQUESTEDPOLICYOID, timeStampRequest.getReqPolicy());
+            }
+            
+            // Find to which worker the request should be dispatched
+            final String toWorker = lookupWorkerToDispatchTo(timeStampRequest, context);
+            if (toWorker == null) {
+                final TimeStampResponseGenerator gen = new TimeStampResponseGenerator(null, null);
+                final TimeStampResponse resp = gen.generateFailResponse(PKIStatus.REJECTION, PKIFailureInfo.unacceptedPolicy, "request contains unknown policy.");
+
+                // Auditlog
+                logMap.put(IWorkerLogger.LOG_CLIENT_AUTHORIZED, "false");
+                logMap.put(IWorkerLogger.LOG_EXCEPTION, "requested policy not supported");
+
+                result = new GenericServletResponse(sReq.getRequestID(), resp.getEncoded(), null, null, null, RESPONSE_CONTENT_TYPE);
+            } else {
+                int toWorkerId = 0;
+                try {
+                    toWorkerId = Integer.parseInt(toWorker);
+                } catch (NumberFormatException ignored) {}
+                if (toWorkerId < 1) {
+                    toWorkerId = getWorkerSession().getWorkerId(toWorker);
+                }
+                
+                HttpServletRequest httpRequest = null;
+                if (sReq instanceof GenericServletRequest) {
+                    httpRequest = ((GenericServletRequest) sReq).getHttpServletRequest();   
+                }
+                ProcessRequest newRequest = new GenericServletRequest(sReq.getRequestID(), (byte[]) sReq.getRequestData(), httpRequest);
+                
+                result = (GenericSignResponse) getWorkerSession().process(toWorkerId, newRequest, context);
+            }
+        } catch (IOException e) {
+            logMap.put(ITimeStampLogger.LOG_TSA_EXCEPTION, e.getMessage());
+            throw new SignServerException("Response message could not be constructed", e);
+        } catch (TSPException e) {
+            throw new SignServerException("Response message could not be constructed", e);
+        }
+        return result;
+    }
+
+    public IRemote getWorkerSession() {
+        return workerSession;
+    }
+    
+    private Map<String, String> parseMapping(String mapping) {
+        
+        if (mapping == null) {
+            return Collections.emptyMap();
+        }
+        final String[] entries = mapping.split(";");
+        final Map<String, String> result = new HashMap<String, String>();
+        for (String entry : entries) {
+            final String[] keyvalue = entry.trim().split(":");
+            if (keyvalue.length == 2) {
+                result.put(keyvalue[0].trim(), keyvalue[1].trim());
+            }
+        }
+        if (LOG.isDebugEnabled()) {
+            final StringBuilder str = new StringBuilder();
+            str.append("Authorization mapping: ");
+            str.append("\n");
+            for (Map.Entry<String, String> entry : result.entrySet()) {
+                str.append("\"");
+                str.append(entry.getKey());
+                str.append("\"");
+                str.append(" --> ");
+                str.append("\"");
+                str.append(entry.getValue());
+                str.append("\"");
+                str.append("\n");
+            }
+            LOG.debug(str.toString());
+        }
+        return result;
+    }
+
+    protected String lookupWorkerToDispatchTo(TimeStampRequest timeStampRequest, RequestContext requestContext) {
+        String result;
+        if (timeStampRequest.getReqPolicy() == null) {
+            result = defaultWorker;
+        } else {
+            result = workerMapping.get(timeStampRequest.getReqPolicy());
+            if (result == null && useDefaultIfMismatch) {
+                result = defaultWorker;
+            }
+        }
+        return result;
+    }
+
+    
+}
---
+++ signserver/modules/SignServer-Test-System/test/org/signserver/module/tsa/RequestedPolicyDispatcherTest.java
@@ -0,0 +1,313 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.module.tsa;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import java.security.cert.Certificate;
+import java.util.Random;
+import org.bouncycastle.asn1.cmp.PKIFailureInfo;
+import org.bouncycastle.asn1.cmp.PKIStatus;
+import org.bouncycastle.tsp.*;
+import org.signserver.common.*;
+import org.signserver.testutils.ModulesTestCase;
+import org.signserver.testutils.TestingSecurityManager;
+
+/**
+ * Tests for RequestedProfileDistpatcher.
+ *
+ * @version $Id$
+ */
+public class RequestedPolicyDispatcherTest extends ModulesTestCase {
+
+    /** Worker ID for test dispatcher. */
+    private static final int DISPATCHER0 = 8910;
+    private static final int DISPATCHER9 = 8909;
+    
+    /** Worker ID for test worker. */
+    private static final int WORKER1 = 8911;
+
+    /** Worker ID for test worker. */
+    private static final int WORKER2 = 8912;
+
+    /** Worker ID for test worker. */
+    private static final int WORKER3 = 8913;
+
+    private static final String WORKER1_PROFILE = "1.2.13.1";
+    private static final String WORKER1_ALTERNATIVE_PROFILE = "1.2.13.9";
+    private static final String WORKER2_PROFILE = "1.2.13.2";
+    private static final String WORKER3_PROFILE = "1.2.13.3";
+    private static final String UNSUPPORTED_PROFILE = "1.2.13.55";
+    
+    private Random random = new Random(4711);
+
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        SignServerUtil.installBCProvider();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+        TestingSecurityManager.remove();
+    }
+
+    /**
+     * Setup signers and dispatchers definied in ts-setup1.properties.
+     */
+    public void test00SetupDatabase() throws Exception {
+        setProperties(getClass().getResourceAsStream("ts-setup1.properties"));
+        workerSession.reloadConfiguration(DISPATCHER0);
+        workerSession.reloadConfiguration(DISPATCHER9);
+        workerSession.reloadConfiguration(WORKER1);
+        workerSession.reloadConfiguration(WORKER2);
+        workerSession.reloadConfiguration(WORKER3);
+    }
+
+    /**
+     * Some basic tests around generating timestamps.
+     * @throws Exception in case of error
+     */
+    public void test01BasicTimeStamp() throws Exception {
+        assertSuccessfulTimestamp(WORKER1);
+        assertSuccessfulTimestamp(WORKER2);
+        assertSuccessfulTimestamp(WORKER3);
+    }
+    
+    /**
+     * Tests that the signers only accepts requests with their profile.
+     */
+    public void test02AcceptedProfiles() throws Exception {
+        TimeStampRequestGenerator gen = new TimeStampRequestGenerator();
+        TimeStampRequest req;
+        TimeStampResponse res;
+        
+        // Test that worker1 accepts its profile but not the other
+        gen.setReqPolicy(WORKER1_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA1, new byte[20], createNounce());
+        res = requestTimeStamp(WORKER1, req);
+        assertEquals("token granted", PKIStatus.GRANTED, res.getStatus());
+        assertEquals("right profile", WORKER1_PROFILE, res.getTimeStampToken().getTimeStampInfo().getPolicy());
+        assertValid(req, res);
+        
+        gen.setReqPolicy(WORKER2_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA1, new byte[20], createNounce());
+        res = requestTimeStamp(WORKER1, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+        assertValid(req, res);
+        
+        gen.setReqPolicy(WORKER3_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA1, new byte[20], createNounce());
+        res = requestTimeStamp(WORKER1, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+        assertValid(req, res);
+        
+        // Test that worker2 accepts its profile but not the other
+        gen.setReqPolicy(WORKER2_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA1, new byte[20], createNounce());
+        res = requestTimeStamp(WORKER2, req);
+        assertEquals("token granted", PKIStatus.GRANTED, res.getStatus());
+        assertEquals("right profile", WORKER2_PROFILE, res.getTimeStampToken().getTimeStampInfo().getPolicy());
+        assertValid(req, res);
+        
+        gen.setReqPolicy(WORKER1_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA1, new byte[20], createNounce());
+        res = requestTimeStamp(WORKER2, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+        
+        gen.setReqPolicy(WORKER3_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA1, new byte[20], createNounce());
+        res = requestTimeStamp(WORKER2, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+        
+        // Test that worker3 accepts its profile but not the other
+        gen.setReqPolicy(WORKER3_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA1, new byte[20], createNounce());
+        res = requestTimeStamp(WORKER3, req);
+        assertEquals("token granted", PKIStatus.GRANTED, res.getStatus());
+        assertEquals("right profile", WORKER3_PROFILE, res.getTimeStampToken().getTimeStampInfo().getPolicy());
+        assertValid(req, res);
+        
+        gen.setReqPolicy(WORKER1_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA1, new byte[20], createNounce());
+        res = requestTimeStamp(WORKER3, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+        
+        gen.setReqPolicy(WORKER2_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA1, new byte[20], createNounce());
+        res = requestTimeStamp(WORKER3, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+    }
+    
+    /**
+     * Tests that requests going through the dispatcher gets the right profiles.
+     */
+    public void test03AcceptedProfilesThroughDispatcher() throws Exception {
+        TimeStampRequestGenerator gen = new TimeStampRequestGenerator();
+        TimeStampRequest req;
+        TimeStampResponse res;
+        
+        // Test that a request with WORKER1_PROFILE is accepted
+        gen.setReqPolicy(WORKER1_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA256, new byte[32], createNounce());
+        res = requestTimeStamp(DISPATCHER0, req);
+        assertEquals("token granted", PKIStatus.GRANTED, res.getStatus());
+        assertEquals("right profile", WORKER1_PROFILE, res.getTimeStampToken().getTimeStampInfo().getPolicy());
+        assertValid(req, res);
+        
+        // Test that a request with WORKER2_PROFILE is accepted
+        gen.setReqPolicy(WORKER2_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA256, new byte[32], createNounce());
+        res = requestTimeStamp(DISPATCHER0, req);
+        assertEquals("token granted", PKIStatus.GRANTED, res.getStatus());
+        assertEquals("right profile", WORKER2_PROFILE, res.getTimeStampToken().getTimeStampInfo().getPolicy());
+        assertValid(req, res);
+        
+        // Test that a request with WORKER3_PROFILE is accepted
+        gen.setReqPolicy(WORKER3_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA256, new byte[32], createNounce());
+        res = requestTimeStamp(DISPATCHER0, req);
+        assertEquals("token granted", PKIStatus.GRANTED, res.getStatus());
+        assertEquals("right profile", WORKER3_PROFILE, res.getTimeStampToken().getTimeStampInfo().getPolicy());
+        assertValid(req, res);
+        
+        // Test that an unknown profile is not accepted (USEDEFAULTIFMISMATCH=false)
+        gen.setReqPolicy(UNSUPPORTED_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA256, new byte[32], createNounce());
+        res = requestTimeStamp(DISPATCHER0, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+        
+        // Test that an unknown profile is not accepted (USEDEFAULTIFMISMATCH=true but profile not known by the default worker)
+        gen.setReqPolicy(UNSUPPORTED_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA256, new byte[32], createNounce());
+        res = requestTimeStamp(DISPATCHER9, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+     }
+     
+    /**
+     * Tests that requests which does not request a certain profile gets dispatched 
+     * to the default worker.
+     */
+    public void test04DefaultWorker() throws Exception {
+        TimeStampRequestGenerator gen = new TimeStampRequestGenerator();
+        TimeStampRequest req;
+        TimeStampResponse res;
+        
+        // Test that a request with no reqPolicy goes to WORKER1_PROFILE
+        req = gen.generate(TSPAlgorithms.SHA256, new byte[32], createNounce());
+        res = requestTimeStamp(DISPATCHER0, req);
+        assertEquals("token granted", PKIStatus.GRANTED, res.getStatus());
+        assertEquals("right profile", WORKER1_PROFILE, res.getTimeStampToken().getTimeStampInfo().getPolicy());
+        assertValid(req, res);
+    }
+    
+    /**
+     * Tests the USEDEFAULTIFMISMATCH option which dispatches requests to the 
+     * default worker if no mapping matched.
+     */
+    public void test05UseDefaultIfMisMatch() throws Exception {
+        TimeStampRequestGenerator gen = new TimeStampRequestGenerator();
+        TimeStampRequest req;
+        TimeStampResponse res;
+        
+        // Test that an profile not known by DISPATCHER0 but by a TSUnit1 is not accepted (USEDEFAULTIFMISMATCH=false)
+        gen.setReqPolicy(WORKER1_ALTERNATIVE_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA256, new byte[32], createNounce());
+        res = requestTimeStamp(DISPATCHER0, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+        
+        // Test that an profile not known by DISPATCHER9 but by a TSUnit1 is accepted (USEDEFAULTIFMISMATCH=true)
+        gen.setReqPolicy(WORKER1_ALTERNATIVE_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA256, new byte[32], createNounce());
+        res = requestTimeStamp(DISPATCHER9, req);
+        assertEquals("token granted", PKIStatus.GRANTED, res.getStatus());
+        assertEquals("right profile", WORKER1_ALTERNATIVE_PROFILE, res.getTimeStampToken().getTimeStampInfo().getPolicy());
+        
+        // Test that an profile not known by DISPATCHER9 and not by a TSUnit1 is rejected even though USEDEFAULTIFMISMATCH=true
+        gen.setReqPolicy(UNSUPPORTED_PROFILE);
+        req = gen.generate(TSPAlgorithms.SHA256, new byte[32], createNounce());
+        res = requestTimeStamp(DISPATCHER9, req);
+        assertEquals("token rejection", PKIStatus.REJECTION, res.getStatus());
+        assertEquals(new PKIFailureInfo(PKIFailureInfo.unacceptedPolicy), res.getFailInfo());
+    }
+    
+    /**
+     * Clean up.
+     */
+    public void test99TearDownDatabase() throws Exception {
+        removeWorker(DISPATCHER0);
+        removeWorker(DISPATCHER9);
+        removeWorker(WORKER1);
+        removeWorker(WORKER2);
+        removeWorker(WORKER3);
+    }
+    
+    private void assertValid(TimeStampRequest req, TimeStampResponse res) {
+        try {
+            res.validate(req);
+        } catch (TSPException ex) {
+            fail(ex.getMessage());
+        }
+    }
+
+    private void assertSuccessfulTimestamp(int worker) throws Exception {
+        final int reqid = random.nextInt();
+        final BigInteger nounce = createNounce();
+
+        TimeStampRequestGenerator timeStampRequestGenerator = new TimeStampRequestGenerator();
+        TimeStampRequest timeStampRequest = timeStampRequestGenerator.generate(TSPAlgorithms.SHA1, new byte[20], nounce);
+        byte[] requestBytes = timeStampRequest.getEncoded();
+
+        GenericSignRequest signRequest = new GenericSignRequest(reqid, requestBytes);
+
+        final GenericSignResponse res = (GenericSignResponse) workerSession.process(worker, signRequest, new RequestContext());
+
+        assertEquals("Request ID", reqid, res.getRequestID());
+
+        Certificate signercert = res.getSignerCertificate();
+        assertNotNull("contains certificate", signercert);
+
+        final TimeStampResponse timeStampResponse = new TimeStampResponse((byte[]) res.getProcessedData());
+        timeStampResponse.validate(timeStampRequest);
+
+        assertEquals("Token granted", PKIStatus.GRANTED, timeStampResponse.getStatus());
+        assertNotNull("Got timestamp token", timeStampResponse.getTimeStampToken());
+    }
+    
+    private TimeStampResponse requestTimeStamp(int worker, TimeStampRequest request) throws IOException, IllegalRequestException, CryptoTokenOfflineException, TSPException, SignServerException {
+        final int reqid = random.nextInt();
+        byte[] requestBytes = request.getEncoded();
+
+        GenericSignRequest signRequest = new GenericSignRequest(reqid, requestBytes);
+        final GenericSignResponse res = (GenericSignResponse) workerSession.process(worker, signRequest, new RequestContext());
+        return new TimeStampResponse((byte[]) res.getProcessedData());
+    }
+    
+    private BigInteger createNounce() {
+        byte[] bytes = new byte[8];
+        random.nextBytes(bytes);
+        return new BigInteger(bytes);
+    }
+
+}

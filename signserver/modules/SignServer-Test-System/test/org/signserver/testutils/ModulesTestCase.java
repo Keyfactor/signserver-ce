fbb3c9d974b5509e36c4b26a591679ed317e7480
Markus Kilås <markus.kilas@primekey.com>
2012-02-23 18:55:18 +0000

    DSS-208: Added worker for querying and setting status properties
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/branches/Branch_3_2@2152 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-System/test/org/signserver/testutils/ModulesTestCase.java
+++ signserver/modules/SignServer-Test-System/test/org/signserver/testutils/ModulesTestCase.java
@@ -32,6 +32,7 @@ import org.signserver.common.ServiceLocator;
 import org.signserver.common.WorkerConfig;
 import org.signserver.ejb.interfaces.IGlobalConfigurationSession;
 import org.signserver.ejb.interfaces.IWorkerSession;
+import org.signserver.statusrepo.IStatusRepositorySession;
 
 /**
  * Base class for test cases.
@@ -55,6 +56,7 @@ public class ModulesTestCase extends TestCase {
 
     protected IWorkerSession workerSession;
     protected IGlobalConfigurationSession globalSession;
+    protected IStatusRepositorySession statusSession;
 
     private static File signServerHome;
 
@@ -106,6 +108,17 @@ public class ModulesTestCase extends TestCase {
         return globalSession;
     }
 
+    protected IStatusRepositorySession getStatusSession() {
+        if (statusSession == null) {
+            try {
+               statusSession = ServiceLocator.getInstance().lookupRemote(
+                    IStatusRepositorySession.IRemote.class);
+            } catch (NamingException ex) {
+                throw new RuntimeException("Could not lookup IStatusSession: " + ex.getMessage());
+            }
+        }
+        return statusSession;
+    }
 
     protected void addDummySigner1() throws CertificateException {
         addSoftDummySigner(getSignerIdDummy1(), getSignerNameDummy1());

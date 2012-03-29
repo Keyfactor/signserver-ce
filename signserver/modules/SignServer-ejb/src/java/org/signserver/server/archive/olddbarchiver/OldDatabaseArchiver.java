9cd774c4a897951f429628a7112b30ac34b56f3d
Markus Kilås <markus.kilas@primekey.com>
2012-03-29 08:57:43 +0000

    DSS-409: Option to use base64 encoding instead of the large and slow XML encoding for archiving
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@2288 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-ejb/src/java/org/signserver/server/archive/olddbarchiver/OldDatabaseArchiver.java
+++ signserver/modules/SignServer-ejb/src/java/org/signserver/server/archive/olddbarchiver/OldDatabaseArchiver.java
@@ -13,6 +13,8 @@
 package org.signserver.server.archive.olddbarchiver;
 
 import java.security.cert.X509Certificate;
+import org.apache.log4j.Logger;
+import org.bouncycastle.util.encoders.Base64;
 import org.signserver.common.ArchiveDataVO;
 import org.signserver.common.RequestContext;
 import org.signserver.common.WorkerConfig;
@@ -20,11 +22,19 @@ import org.signserver.server.SignServerContext;
 import org.signserver.server.archive.Archivable;
 import org.signserver.server.archive.ArchiveException;
 import org.signserver.server.archive.Archiver;
+import org.signserver.server.archive.ArchiverInitException;
 import org.signserver.server.archive.olddbarchiver.entities.ArchiveDataService;
 
 /**
  * Archiver only accepting responses and currently only supports Archivables of
  * class ArchiveDataArchivable. 
+ * 
+ *  Worker properties:
+ *  ARCHIVERx.RESPONSEFORMAT - Format for storing the response in the database. 
+ *  Can be "XML" for storage in an upgradeble XML encoded Base64PutHashMap (default) 
+ *  or "BASE64" for only storing the request data bytes in base64 encoding.
+ * 
+ *  where x is the (zero-based) index in the ARCHIVERS property list.
  * 
  * Developers:
  * This class could be improved to support any Archivable if the
@@ -35,12 +45,34 @@ import org.signserver.server.archive.olddbarchiver.entities.ArchiveDataService;
  * @version $Id$
  */
 public class OldDatabaseArchiver implements Archiver {
+    
+    /** Logger for this class. */
+    private static final Logger LOG = Logger.getLogger(OldDatabaseArchiver.class);
 
+    public static final String RESPONSEFORMAT = "RESPONSEFORMAT";
+    public static final String XML = "XML";
+    public static final String BASE64 = "BASE64";
+    
     private ArchiveDataService dataService;
+    
+    private boolean base64Encoding;
 
     @Override
-    public void init(int listIndex, WorkerConfig config, SignServerContext context) {
+    public void init(int listIndex, WorkerConfig config, SignServerContext context) throws ArchiverInitException {
+        dataService = null;
+        String responseFormatProperty = "ARCHIVER" + listIndex + "." + RESPONSEFORMAT;
+        String responseFormat = config.getProperty(responseFormatProperty, XML);
+        if (BASE64.equalsIgnoreCase(responseFormat)) {
+            base64Encoding = true;
+        } else if (XML.equalsIgnoreCase(responseFormat)) {
+            base64Encoding = false;
+        } else {
+            throw new ArchiverInitException("Unknown value for property " + responseFormatProperty + ". Should be \"" + XML + "\" or \"" + BASE64 + "\"");
+        }
         dataService = new ArchiveDataService(context.getEntityManager());
+        if (LOG.isDebugEnabled()) {
+            LOG.debug("Archiver" + listIndex + " will use base64 encoding: " + base64Encoding);
+        }
     }
 
     @Override
@@ -49,17 +81,32 @@ public class OldDatabaseArchiver implements Archiver {
         final boolean archived;
         if (Archivable.TYPE_RESPONSE.equals(archivable.getType())
                 && archivable instanceof ArchiveDataArchivable) {
+            if (dataService == null) {
+                throw new ArchiveException("Could not archive as archiver was not successfully initialized");
+            }
             final ArchiveDataArchivable ada = (ArchiveDataArchivable) archivable;
             final Integer workerId = (Integer) requestContext.get(RequestContext.WORKER_ID);
             final X509Certificate certificate = (X509Certificate) requestContext.get(RequestContext.CLIENT_CERTIFICATE);
             final String remoteIp = (String) requestContext.get(RequestContext.REMOTE_IP);
 
-            dataService.create(ArchiveDataVO.TYPE_RESPONSE_XMLENCODED,
+            final String uniqueId;
+            if (base64Encoding) {
+                uniqueId = dataService.create(ArchiveDataVO.TYPE_RESPONSE_BASE64ENCODED,
+                            workerId,
+                            ada.getArchiveId(),
+                            certificate,
+                            remoteIp,
+                            new String(Base64.encode(ada.getContentEncoded())));
+            } else {
+                uniqueId = dataService.create(ArchiveDataVO.TYPE_RESPONSE_XMLENCODED,
                             workerId,
                             ada.getArchiveId(),
                             certificate,
                             remoteIp,
                             ada.getArchiveData());
+            }
+            // TODO: Log uniqueId
+            
             archived = true;
         } else {
             archived = false;

fc52ec934b2c2999d7de4ea0db986831bb9d78ac
Marcus Lundblad <marcus.lundblad@primekey.com>
2012-05-11 06:33:23 +0000

    DSS-466: Use %* instead of enumerating a fixed number of arguments also to the remaining Windows command scripts.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@2361 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/bin/admingui.cmd
+++ signserver/bin/admingui.cmd
@@ -42,24 +42,11 @@ if exist %APPSRV_HOME%\client\jbossall-client.jar  (
 set CLASSPATH=%MAIN_CLASSPATH%;%JEE_CLASSPATH%
 rem echo %CLASSPATH%
 
-rem Fixup arguments, we have to do this since windows normally only 
-rem supports %1-%9 as command line arguments
-shift
-set a=%0
-set b=%1
-set c=%2
-set d=%3
-set e=%4
-set f=%5
-set g=%6
-set h=%7
-set i=%8
-set j=%9
-rem echo %a% %b% %c% %d% %e% %f% %g% %h% %i% %j%
+
 if "%JAVA_HOME%" == "" (
-  java -cp %CLASSPATH% -splash:%SIGNSRV_HOME%\res\admingui-splash.png %class_name% %a% %b% %c% %d% %e% %f% %g% %h% %i% %j% -connectfile %SIGNSRV_HOME%/conf/admingui.properties -defaultconnectfile %SIGNSRV_HOME%/conf/admingui_default.properties
+  java -cp %CLASSPATH% -splash:%SIGNSRV_HOME%\res\admingui-splash.png %class_name% %* -connectfile %SIGNSRV_HOME%/conf/admingui.properties -defaultconnectfile %SIGNSRV_HOME%/conf/admingui_default.properties
 ) else (
-  "%JAVA_HOME%\bin\java" -cp %CLASSPATH% -splash:%SIGNSRV_HOME%\res\admingui-splash.png %class_name% %a% %b% %c% %d% %e% %f% %g% %h% %i% %j% -connectfile %SIGNSRV_HOME%/conf/admingui.properties -defaultconnectfile %SIGNSRV_HOME%/conf/admingui_default.properties
+  "%JAVA_HOME%\bin\java" -cp %CLASSPATH% -splash:%SIGNSRV_HOME%\res\admingui-splash.png %class_name% %* -connectfile %SIGNSRV_HOME%/conf/admingui.properties -defaultconnectfile %SIGNSRV_HOME%/conf/admingui_default.properties
 
 )
 :end
--- signserver/bin/ant.cmd
+++ signserver/bin/ant.cmd
@@ -1,2 +1,2 @@
 @echo off
-ant -f build.xml -q %1 %2 %3 %4 %5 %6 %7 %8 %9 | FindStr /v "Trying to override old definition of task http://www.netbeans.org/"
+ant -f build.xml -q %* | FindStr /v "Trying to override old definition of task http://www.netbeans.org/"

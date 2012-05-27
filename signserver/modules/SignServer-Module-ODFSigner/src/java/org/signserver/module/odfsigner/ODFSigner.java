f8f3718b392871e79bbfb58fcfc975f463150410
Markus Kilås <markus.kilas@primekey.com>
2012-05-27 10:42:46 +0000

    DSS-492: Changed to use our built odfdom. Packages names was slightly changed in the last patch.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@2393 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-ODFSigner/src/java/org/signserver/module/odfsigner/ODFSigner.java
+++ signserver/modules/SignServer-Module-ODFSigner/src/java/org/signserver/module/odfsigner/ODFSigner.java
@@ -19,8 +19,8 @@ import java.security.cert.X509Certificate;
 
 import org.bouncycastle.util.encoders.Hex;
 import org.ejbca.util.CertTools;
-import org.odftoolkit.odfdom.crypto.dsig.DocumentSignatureManager;
-import org.odftoolkit.odfdom.crypto.dsig.SignatureCreationMode;
+import org.odftoolkit.odfdom.pkg.signature.DocumentSignatureManager;
+import org.odftoolkit.odfdom.pkg.signature.SignatureCreationMode;
 import org.odftoolkit.odfdom.doc.OdfDocument;
 import org.signserver.common.ArchiveData;
 import org.signserver.common.CryptoTokenOfflineException;

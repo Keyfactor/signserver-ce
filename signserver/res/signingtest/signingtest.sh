b0a9d85eab7d9225f544fba635cda79b4ecc9aea
Marcus Lundblad <marcus.lundblad@primekey.com>
2012-06-20 08:01:25 +0000

    DSS-502: Add test script and sample files for signing PDF and XML.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@2415 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/res/signingtest/signingtest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# test that SIGNSERVER_HOME is set
+if [ ! -n "${SIGNSERVER_HOME}" ]; then
+  SIGNSERVER_HOME=..
+fi
+
+SIGNCLIENT=$SIGNSERVER_HOME/bin/signclient
+
+# test PDF signer
+$SIGNCLIENT signdocument -workername TestPDFSigner -infile test.pdf -outfile test-out.pdf 
+
+# test XML signer
+$SIGNCLIENT signdocument -workername TestXMLSigner -infile test.xml -outfile test-out.xml
+
+
+  
+

b2bc32031f0d11ce883aae4adff4d75819355c73
Marcus Lundblad <marcus.lundblad@primekey.com>
2012-06-28 06:34:20 +0000

    DSS-502: Make the script runnable from $SIGNSERVER_HOME (and everywhere) so that the documentation will actually work...
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@2435 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/res/signingtest/signingtest.sh
+++ signserver/res/signingtest/signingtest.sh
@@ -11,38 +11,39 @@ if [ ! -n "${SIGNSERVER_HOME}" ]; then
 fi
 
 SIGNCLIENT=$SIGNSERVER_HOME/bin/signclient
+SCRIPTDIR=$SIGNSERVER_HOME/res/signingtest
 
 # test PDF signer
 echo "Testing PDF signer..."
-$SIGNCLIENT signdocument -workername TestPDFSigner -infile input/test.pdf -outfile output/test-out.pdf 
+$SIGNCLIENT signdocument -workername TestPDFSigner -infile $SCRIPTDIR/input/test.pdf -outfile $SCRIPTDIR/output/test-out.pdf 
 echo "Testing PDF signer done"
 
 # test XML signer
 echo "Testing XML signer..."
-$SIGNCLIENT signdocument -workername TestXMLSigner -infile input/test.xml -outfile output/test-out.xml
+$SIGNCLIENT signdocument -workername TestXMLSigner -infile $SCRIPTDIR/input/test.xml -outfile $SCRIPTDIR/output/test-out.xml
 echo "Testing XML signer done"
 
 # test ODF signer
 echo "Testing ODF signer..."
-$SIGNCLIENT signdocument -workername TestODFSigner -infile input/test.odf -outfile output/test-out.odf
+$SIGNCLIENT signdocument -workername TestODFSigner -infile $SCRIPTDIR/input/test.odf -outfile $SCRIPTDIR/output/test-out.odf
 echo "Testing ODF signer done"
 
 # test OOXML signer
 echo "Testing OOXML signer..."
-$SIGNCLIENT signdocument -workername TestOOXMLSigner -infile input/test.docx -outfile output/test-out.docx  
+$SIGNCLIENT signdocument -workername TestOOXMLSigner -infile $SCRIPTDIR/input/test.docx -outfile $SCRIPTDIR/output/test-out.docx  
 echo "Testing OOXML signer done"
 
 # test MRTDSODSigner
 echo "Testing MRTDSODSigner..."
-$SIGNCLIENT signdatagroups -workername TestMRTDSODSigner -data "1=value1\&2=value2\&3=value3" > output/test-out.sod
+$SIGNCLIENT signdatagroups -workername TestMRTDSODSigner -data "1=value1\&2=value2\&3=value3" > $SCRIPTDIR/output/test-out.sod
 echo "Testing MRTDSODSigner done"
 
 # test CMSSigner
 echo "Testing CMS Signer..."
-$SIGNCLIENT signdocument -workername TestCMSSigner -infile input/test.xml -outfile output/test-signed.p7s 
+$SIGNCLIENT signdocument -workername TestCMSSigner -infile $SCRIPTDIR/input/test.xml -outfile $SCRIPTDIR/output/test-signed.p7s 
 echo "Testing CMS Signer done"
 
 # test TSA
 echo "Testing TSA..."
-$SIGNCLIENT timestamp -instr "Foobar" -outrep output/test-tsa.resp -url http://localhost:8080/signserver/tsa\?workerName=TestTimeStampSigner
+$SIGNCLIENT timestamp -instr "Foobar" -outrep $SCRIPTDIR/output/test-tsa.resp -url http://localhost:8080/signserver/tsa\?workerName=TestTimeStampSigner
 echo "Testing TSA done"

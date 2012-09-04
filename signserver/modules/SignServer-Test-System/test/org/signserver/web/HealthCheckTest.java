b7d795cfce86bb1c9c69c74dcf48943aa21c899d
Marcus Lundblad <marcus.lundblad@primekey.com>
2012-09-04 11:47:27 +0000

    DSS-529: Add healthcheck test case with timestamp signer with a time source not "insynk".
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@2593 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-System/test/org/signserver/web/HealthCheckTest.java
+++ signserver/modules/SignServer-Test-System/test/org/signserver/web/HealthCheckTest.java
@@ -12,8 +12,13 @@
  *************************************************************************/
 package org.signserver.web;
 
+import java.io.File;
 import java.util.Collections;
 import java.util.Map;
+
+import org.signserver.common.ServiceLocator;
+import org.signserver.statusrepo.IStatusRepositorySession;
+import org.signserver.statusrepo.common.StatusName;
 
 /**
  * Tests the Health check.
@@ -25,18 +30,36 @@ public class HealthCheckTest extends WebTestCase {
 
     private static final Map<String, String> NO_FIELDS = Collections.emptyMap();
     
+    /** Worker ID for test TSA worker. */
+    private static final int TSA_WORKER = 8904;
+    
+    /** The status repository session. */
+    private static IStatusRepositorySession.IRemote repository;
+    
     @Override
     protected String getServletURL() {
         return "http://localhost:8080/signserver/healthcheck/signserverhealth";
     }
+    
+    
 
-    /**
+    @Override
+	protected void setUp() throws Exception {
+        repository = ServiceLocator.getInstance().lookupRemote(
+                IStatusRepositorySession.IRemote.class);
+	}
+
+
+
+	/**
      * Sets up a dummy signer.
      * @throws Exception in case of error
      */
     public void test00SetupDatabase() throws Exception {
         addDummySigner1();
 //        addCMSSigner1();
+        setProperties(new File(getSignServerHome(), "modules/SignServer-Module-TSA/src/conf/junittest-part-config.properties"));
+        workerSession.reloadConfiguration(TSA_WORKER);
     }
 
     /**
@@ -63,6 +86,18 @@ public class HealthCheckTest extends WebTestCase {
         String body = new String(sendAndReadyBody(NO_FIELDS));
         assertFalse("Not ALLOK: " + body, body.contains("ALLOK"));
     }
+    
+    /**
+     * Tests that a time stamp signer with a timesource not insync results in a healthcheck error
+     */
+    public void test03TimeSourceNotInsync() throws Exception {
+    	// Test without insync
+        repository.update(StatusName.TIMESOURCE0_INSYNC.name(), "");
+        
+        assertStatusReturned(NO_FIELDS, 500);
+        String body = new String(sendAndReadyBody(NO_FIELDS));
+        assertFalse("Not ALLOK: " + body, body.contains("ALLOK"));
+    }
 
     /**
      * Remove the workers created etc.
@@ -71,5 +106,6 @@ public class HealthCheckTest extends WebTestCase {
     public void test99TearDownDatabase() throws Exception {
         removeWorker(getSignerIdDummy1());
 //        removeWorker(getSignerIdCMSSigner1());
+        removeWorker(TSA_WORKER);
     }
 }

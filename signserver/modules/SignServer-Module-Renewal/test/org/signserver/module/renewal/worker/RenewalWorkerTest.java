11934a156700e3fe763848c34070ef39a4921450
Markus Kilås <markus.kilas@primekey.com>
2012-10-10 15:39:53 +0000

    DSS-558: Added test for the DN in the request
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/branches/Branch_3_2@2827 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-Renewal/test/org/signserver/module/renewal/worker/RenewalWorkerTest.java
+++ signserver/modules/SignServer-Module-Renewal/test/org/signserver/module/renewal/worker/RenewalWorkerTest.java
@@ -130,6 +130,9 @@ public class RenewalWorkerTest extends AbstractTestCase {
         assertTrue("should have requested certificate",
                 mockEjbcaWs.isPkcs10RequestCalled());
 
+        // Check that the right DN is included
+        assertEquals("Requested DN", "CN=" + SIGNER_6102_ENDENTITY, mockEjbcaWs.getLastPKCS10().getRequestDN());
+        
         // Should have certificate and chain
         final X509Certificate cert = (X509Certificate) getWorkerSession()
                 .getSignerCertificate(SIGNERID_6102);

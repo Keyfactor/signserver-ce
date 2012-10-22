2ca336d2ec52c21dbc6e247ba1d1a99ce8afcec1
Marcus Lundblad <marcus.lundblad@primekey.com>
2012-10-22 11:33:13 +0000

    DSS-516: Added dummy timesource implementation returing 0 time (Date(0)) for testing purposes.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@2860 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-ejb/src/java/org/signserver/server/ZeroTimeSource.java
@@ -0,0 +1,28 @@
+package org.signserver.server;
+
+import java.util.Date;
+import java.util.Properties;
+
+/**
+ * Simple class implementing the ITimeSource interface
+ * always returns time 0
+ * This is mainly intended to use for testing to get a
+ * predictable non-null time value
+ * 
+ * @author Marcus Lundblad
+ * @version $Id$
+ *
+ */
+public class ZeroTimeSource implements ITimeSource {
+
+	@Override
+	public void init(Properties props) {
+		// no properties defined
+	}
+
+	@Override
+	public Date getGenTime() {
+		return new Date(0);
+	}
+
+}

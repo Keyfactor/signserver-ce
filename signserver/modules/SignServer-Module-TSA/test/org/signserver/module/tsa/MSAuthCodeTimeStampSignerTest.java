fb55b089344c9c8314692202b086277f13ba3a70
Markus Kilås <markus.kilas@primekey.com>
2012-11-13 10:18:21 +0000

    NoTicket: Added missing source code header
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@2975 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-TSA/test/org/signserver/module/tsa/MSAuthCodeTimeStampSignerTest.java
+++ signserver/modules/SignServer-Module-TSA/test/org/signserver/module/tsa/MSAuthCodeTimeStampSignerTest.java
@@ -1,12 +1,19 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.signserver.module.tsa;
 
 import java.util.Date;
 import junit.framework.TestCase;
-
 import org.apache.log4j.Logger;
 import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;

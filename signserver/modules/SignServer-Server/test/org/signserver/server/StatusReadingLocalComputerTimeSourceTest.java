ca584c25a5dc9c58aae276cc6e12ea86987e588d
Marcus Lundblad <marcus.lundblad@primekey.com>
2012-11-29 13:14:10 +0000

    DSS-572: Add some more tests for the leapsecond detection algorithm.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3059 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/test/org/signserver/server/StatusReadingLocalComputerTimeSourceTest.java
+++ signserver/modules/SignServer-Server/test/org/signserver/server/StatusReadingLocalComputerTimeSourceTest.java
@@ -15,17 +15,60 @@ import junit.framework.TestCase;
 public class StatusReadingLocalComputerTimeSourceTest extends TestCase {
 
     
-    /**
-     * Test that the last second of December is detected as a potential leapsecond event
-     */
-    public void test01PotentialLeapSecondDecember() throws Exception {
+    private void assertPotentialLeapsecond(int year, int month, int day, int hour, int min, int sec) {
         Date date;
         Calendar cal = Calendar.getInstance();
         
-        cal.set(2012, 11, 31, 23, 59, 59);
+        cal.set(year, month - 1, day, hour, min, sec);
         date = cal.getTime();
         
-        assertTrue("Should detect possible leapsecond", StatusReadingLocalComputerTimeSource.isPotentialLeapsecond(date));
+        assertTrue("Should detect possible leapsecond: " + date.toString(), StatusReadingLocalComputerTimeSource.isPotentialLeapsecond(date));
     }
     
+    private void assertNotPotentialLeapsecond(int year, int month, int day, int hour, int min, int sec) {
+        Date date;
+        Calendar cal = Calendar.getInstance();
+        
+        cal.set(year, month - 1, day, hour, min, sec);
+        date = cal.getTime();
+        
+        assertFalse("Should not detect possible leapsecond: " + date.toString(), StatusReadingLocalComputerTimeSource.isPotentialLeapsecond(date));
+    }
+    
+    /**
+     * Test that the last second of December is detected as a potential leapsecond event.
+     * 
+     * @throws Exception
+     */
+    public void test01PotentialLeapSecondDecember() throws Exception {
+        assertPotentialLeapsecond(2012, 12, 31, 23, 59, 59);
+    }
+    
+    /**
+     * Test that the last second of June is detected as a potential leapsecond event.
+     * 
+     * @throws Exception
+     */
+    public void test02PotentialLeapSecondJune() throws Exception {
+        assertPotentialLeapsecond(2013, 6, 30, 23, 59, 59);
+    }
+    
+    /**
+     * Test that the first second of January is detected as a potential leapsecond event.
+     * (an interval around the actual second should be detected).
+     * 
+     * @throws Exception
+     */
+    public void test03PotentialLeapSecondJanuary() throws Exception {
+        assertPotentialLeapsecond(2013, 1, 1, 0, 0, 0);
+    }
+    
+    /**
+     * Test that another arbitrary date is not detected as a potential leapsecond event.
+     * 
+     * @throws Exception
+     */
+    public void test04NotPotentialLeapSecondOther() throws Exception {
+        assertNotPotentialLeapsecond(2013, 4, 7, 12, 47, 11);
+    }
 }

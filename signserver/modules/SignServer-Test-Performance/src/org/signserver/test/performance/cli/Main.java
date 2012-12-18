4712c1f7925bd5ff6896afbc64f1692a5fdb9973
Marcus Lundblad <marcus.lundblad@primekey.com>
2012-12-18 13:52:54 +0000

    DSS-574: Skeleton of CLI stress test tool.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3105 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Test-Performance/src/org/signserver/test/performance/cli/Main.java
@@ -0,0 +1,95 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.test.performance.cli;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintWriter;
+import java.rmi.RemoteException;
+import java.util.Arrays;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.log4j.Logger;
+import org.signserver.common.InvalidWorkerIdException;
+
+/**
+ * Performance test tool
+ * @author Marcus Lundblad
+ * @version $Id$
+ *
+ */
+public class Main {
+    /** Logger for this class */
+    private static Logger LOG = Logger.getLogger(Main.class);
+
+    private static final String TEST_SUITE = "testsuite";
+    private static final String TIME_LIMIT = "timelimit";
+    private static final Options OPTIONS;
+    
+    private static final String NL = System.getProperty("line.separator");
+    private static final String COMMAND = "stresstest";
+    
+    private enum TestSuites {
+        TimeStamp1,
+    }
+
+    static {
+        OPTIONS = new Options();
+        OPTIONS.addOption(TEST_SUITE, true, "Test suite to run. Any of " + Arrays.asList(TestSuites.values()) + ".");
+        OPTIONS.addOption(TIME_LIMIT, true, "Optional. Only run for the specified time (in milliseconds).");
+    }
+
+    private static void printUsage() {
+        StringBuilder footer = new StringBuilder();
+        footer.append(NL)
+                .append("Sample usages:").append(NL)
+                .append("a) ").append(COMMAND).append(" -testsuite signWhileUpdatingConfig").append(NL);
+                
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        final HelpFormatter formatter = new HelpFormatter();
+        PrintWriter pw = new PrintWriter(bout);
+        formatter.printHelp(pw, HelpFormatter.DEFAULT_WIDTH, COMMAND + " <options>", "Random testing tool", OPTIONS, HelpFormatter.DEFAULT_LEFT_PAD, HelpFormatter.DEFAULT_DESC_PAD, footer.toString());
+        pw.close();
+        LOG.info(bout.toString());
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws RemoteException, InvalidWorkerIdException {
+        try {
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("(Debug logging is enabled)");
+            }
+            
+            final CommandLine commandLine = new GnuParser().parse(OPTIONS, args);
+
+            // Test suite
+            final TestSuites ts;
+            if (commandLine.hasOption(TEST_SUITE)) {
+                ts = TestSuites.valueOf(commandLine.getOptionValue(TEST_SUITE));
+            } else {
+                throw new ParseException("Missing option: -" + TEST_SUITE);
+            }
+            
+            
+        } catch (ParseException ex) {
+            LOG.error("Parse error: " + ex.getMessage());
+            printUsage();
+            System.exit(-2);
+        }
+    }
+}

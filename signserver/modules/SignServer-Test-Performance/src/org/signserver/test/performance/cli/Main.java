7e225e59ebe053c8e3e2686b27c5efd8f0711d75
Marcus Lundblad <marcus.lundblad@primekey.com>
2012-12-19 12:32:26 +0000

    DSS-574: Update usage text.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3115 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-Performance/src/org/signserver/test/performance/cli/Main.java
+++ signserver/modules/SignServer-Test-Performance/src/org/signserver/test/performance/cli/Main.java
@@ -16,6 +16,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
 import java.rmi.RemoteException;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.GnuParser;
@@ -24,6 +26,8 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.log4j.Logger;
 import org.signserver.common.InvalidWorkerIdException;
+import org.signserver.test.performance.FailureCallback;
+import org.signserver.test.performance.WorkerThread;
 
 /**
  * Performance test tool
@@ -42,6 +46,8 @@ public class Main {
     private static final String NL = System.getProperty("line.separator");
     private static final String COMMAND = "stresstest";
     
+    private static int exitCode;
+    
     private enum TestSuites {
         TimeStamp1,
     }
@@ -56,7 +62,7 @@ public class Main {
         StringBuilder footer = new StringBuilder();
         footer.append(NL)
                 .append("Sample usages:").append(NL)
-                .append("a) ").append(COMMAND).append(" -testsuite signWhileUpdatingConfig").append(NL);
+                .append("a) ").append(COMMAND).append(" -testsuite TimeStamp1").append(NL);
                 
         ByteArrayOutputStream bout = new ByteArrayOutputStream();
         final HelpFormatter formatter = new HelpFormatter();
@@ -85,11 +91,103 @@ public class Main {
                 throw new ParseException("Missing option: -" + TEST_SUITE);
             }
             
+            // Time limit
+            final long limitedTime;
+            if (commandLine.hasOption(TIME_LIMIT)) {
+                limitedTime = Long.parseLong(commandLine.getOptionValue(TIME_LIMIT));
+            } else {
+                limitedTime = -1;
+            }
+  
+            final LinkedList<WorkerThread> threads = new LinkedList<WorkerThread>();
+            final FailureCallback callback = new FailureCallback() {
+
+                @Override
+                public void failed(WorkerThread thread, String message) {
+                    // Stop
+                    for (WorkerThread w : threads) {
+                        w.stopIt();
+                    }
+                    
+                    // Wait until all stoped
+                    try {
+                        for (WorkerThread w : threads) {
+                            w.join(1000);
+                        }
+                    } catch (InterruptedException ex) {
+                        LOG.error("Interrupted: " + ex.getMessage());
+                    }
+                    
+                    // Print message
+                    LOG.error(thread + ": " + message);
+                    exitCode = -1;
+                }
+            };
+            Thread.UncaughtExceptionHandler handler = new Thread.UncaughtExceptionHandler() {
+
+                @Override
+                public void uncaughtException(Thread t, Throwable e) {
+                    LOG.error("Uncaught exception from t", e);
+                    callback.failed((WorkerThread) t, "Uncaught exception: " + e.getMessage());
+                }
+            };
+
+            try {
+                switch (ts) {
+                case TimeStamp1:
+                    runTimeStamp1(threads);
+                    break;
+                default:
+                    throw new Exception("Unsupported test suite");
+                }
+                
+                // Wait 1 second to start
+                Thread.sleep(1000);
             
+                // Start all threads
+                for (WorkerThread w : threads) {
+                    w.setUncaughtExceptionHandler(handler);
+                    w.start();
+                }
+
+                // If time limited
+                if (limitedTime > 0) {
+                    try {
+                        Thread.sleep(limitedTime);
+                    } catch (InterruptedException ex) {
+                        LOG.error("Interrupted: " + ex.getMessage());
+                    }
+                    // Stop all threads
+                    for (WorkerThread w : threads) {
+                        w.stopIt();
+                    }
+                }
+            
+
+                // Wait until all stopped
+                try {
+                    for (WorkerThread w : threads) {
+                        w.join();
+                        LOG.info(w + ": Operations performed: " + w.getOperationsPerformed());
+                    }
+                } catch (InterruptedException ex) {
+                    LOG.error("Interrupted: " + ex.getMessage());
+                }
+            
+            } catch (Exception ex) {
+                LOG.error("Failed: " + ex.getMessage(), ex);
+                exitCode = -1;
+            }
+
+            System.exit(exitCode);
         } catch (ParseException ex) {
             LOG.error("Parse error: " + ex.getMessage());
             printUsage();
             System.exit(-2);
         }
+    }
+    
+    private static void runTimeStamp1(final List<WorkerThread> threads) throws Exception {
+        
     }
 }

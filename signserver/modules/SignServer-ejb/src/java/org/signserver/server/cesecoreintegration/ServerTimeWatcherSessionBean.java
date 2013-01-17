85d94a348b1d92e71b68a7e2b708d99d74d3a8b1
Markus Kilås <markus.kilas@primekey.com>
2013-01-17 15:27:29 +0000

    DSS-584: Excluding an replacing some CESeCore classes
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3199 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-ejb/src/java/org/signserver/server/cesecoreintegration/ServerTimeWatcherSessionBean.java
@@ -0,0 +1,41 @@
+/*************************************************************************
+ *                                                                       *
+ *  EJBCA: The OpenSource Certificate Authority                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.server.cesecoreintegration;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+
+import org.cesecore.time.TrustedTime;
+import org.cesecore.time.TrustedTimeWatcherSessionLocal;
+import org.cesecore.time.providers.TrustedTimeProviderException;
+
+/**
+ * This is the trusted time watcher implementation.
+ * 
+ *
+ * @version $Id$
+ */
+@Stateless
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class ServerTimeWatcherSessionBean implements TrustedTimeWatcherSessionLocal {
+
+	@Override
+	public TrustedTime getTrustedTime(final boolean force) throws TrustedTimeProviderException {
+		final TrustedTime tt = new TrustedTime();
+		tt.setSync(false);
+		return tt;
+	}
+
+
+}

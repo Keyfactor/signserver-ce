da9b241b7f4e2d55349ae6923b45aa48b8e27026
Markus Kilås <markus.kilas@primekey.com>
2013-02-08 13:30:47 +0000

    DSS-598: Basic test for the database CLI
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3305 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-DatabaseCLI/test/org/signserver/db/cli/DatabaseCLITest.java
@@ -0,0 +1,89 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.db.cli;
+
+import javax.persistence.PersistenceException;
+import junit.framework.TestCase;
+import org.apache.log4j.Logger;
+import org.signserver.testutils.CLITestHelper;
+
+/**
+ * Tests for the database CLI.
+ *
+ * @author Markus Kilås
+ * @version $Id$
+ */
+public class DatabaseCLITest extends TestCase {
+    
+    /** Logger for this class. */
+    private static final Logger LOG = Logger.getLogger(DatabaseCLITest.class);
+    
+    private static final String JDBC_ERROR = "This test requires the JDBC drivers to be present on the classpath. Put the database connector as lib/ext/jdbc/jdbc.jar.";
+    
+    private CLITestHelper cli = getDatabaseCLI();
+    
+    public DatabaseCLITest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public CLITestHelper getDatabaseCLI() {
+        if (cli == null) {
+            cli = new CLITestHelper(Main.class);
+        }
+        return cli;
+    }
+    
+    /**
+     * Tests that non-existing command gives an error return code.
+     */
+    public void testNonExistingCommand() throws Exception {
+        LOG.info("testNonExistingCommand");
+        final int actual = cli.execute(new String[] {"_any_non_existing_command_123_"});
+        assertEquals("return code", Main.RETURN_INVALID_ARGUMENTS, actual);
+    }
+    
+    /**
+     * Tests that non-existing command argument gives an error return code.
+     */
+    public void testVerifyLogUnexpectedArgument() throws Exception {
+        LOG.info("testVerifyLogUnexpectedArgument");
+        final int actual = cli.execute(new String[] {"audit", "verifylog", "_invalid_argument_123_"});
+        assertEquals("return code", Main.RETURN_INVALID_ARGUMENTS, actual);
+    }
+    
+    /**
+     * Tests that the audit verifylog command completes successful.
+     */
+    public void testVerifyLog() throws Exception {
+        LOG.info("testVerifyLog");
+        try {
+            final int actual = cli.execute(new String[] {"audit", "verifylog"});
+            assertEquals("return code", Main.RETURN_SUCCESS, actual);
+        } catch (PersistenceException ex) {
+            throw new Exception(JDBC_ERROR, ex);
+        }
+    }
+    
+    // TODO add test methods here. The name must begin with 'test'. For example:
+    // public void testHello() {}
+}

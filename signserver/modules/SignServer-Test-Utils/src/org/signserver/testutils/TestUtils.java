08efcf4ae0526e38495c82dbcae0401de1e2b249
Markus Kilås <markus.kilas@primekey.com>
2013-05-03 21:19:57 +0000

    DSS-306: Setting default socket factory implicitly instead of using System.setProperties() to configure truststore as it does not seem to work with JDK 7 / GlassFish 3.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3469 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-Utils/src/org/signserver/testutils/TestUtils.java
+++ signserver/modules/SignServer-Test-Utils/src/org/signserver/testutils/TestUtils.java
@@ -16,8 +16,23 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
 import java.util.Properties;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509KeyManager;
 import org.apache.log4j.Logger;
+import org.signserver.client.cli.defaultimpl.AliasKeyManager;
 
 /**
  * Class containing utility methods used to simplify testing.
@@ -32,13 +47,56 @@ public class TestUtils {
     
     private Properties buildConfig;
     
-    public void setupSSLTruststore() {
-        System.setProperty("javax.net.ssl.trustStore", 
-                getTruststoreFile().getAbsolutePath());
-        System.setProperty("javax.net.ssl.trustStorePassword",
-                getTrustStorePassword());
+    public void setupSSLTruststore() throws KeyStoreException, FileNotFoundException, IOException, NoSuchAlgorithmException, CertificateException, KeyManagementException, UnrecoverableKeyException {
+        // This does not work on JDK 7 / GlassFish 3
+//        System.setProperty("javax.net.ssl.trustStore", trustStore);
+//        System.setProperty("javax.net.ssl.trustStorePassword",
+//                getTrustStorePassword());
         //System.setProperty("javax.net.ssl.keyStore", "../../p12/testadmin.jks");
         //System.setProperty("javax.net.ssl.keyStorePassword", "foo123");
+        
+        // Instead set the socket factory
+        KeyStore truststore = loadKeyStore(getTruststoreFile(), getTrustStorePassword());
+        setDefaultSocketFactory(truststore, null, null, null);
+    }
+    
+    private static KeyStore loadKeyStore(final File truststoreFile,
+            final String truststorePassword) throws KeyStoreException, FileNotFoundException, IOException, NoSuchAlgorithmException, CertificateException {
+        KeyStore keystore = KeyStore.getInstance("JKS");
+        keystore.load(new FileInputStream(truststoreFile), truststorePassword.toCharArray());
+        return keystore;
+    }
+
+    private static void setDefaultSocketFactory(final KeyStore truststore,
+            final KeyStore keystore, String keyAlias, char[] keystorePassword) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, UnrecoverableKeyException {
+
+        final TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+        tmf.init(truststore);
+
+        final KeyManager[] keyManagers;
+        if (keystore == null) {
+            keyManagers = null;
+        } else {
+            if (keyAlias == null) {
+                keyAlias = keystore.aliases().nextElement();
+            }
+            final KeyManagerFactory kKeyManagerFactory
+                    = KeyManagerFactory.getInstance("SunX509");
+            kKeyManagerFactory.init(keystore, keystorePassword);
+            keyManagers = kKeyManagerFactory.getKeyManagers();
+            for (int i = 0; i < keyManagers.length; i++) {
+                if (keyManagers[i] instanceof X509KeyManager) {
+                    keyManagers[i] = new AliasKeyManager(
+                            (X509KeyManager) keyManagers[i], keyAlias);
+                }
+            }
+        }
+
+        final SSLContext context = SSLContext.getInstance("TLS");
+        context.init(keyManagers, tmf.getTrustManagers(), new SecureRandom());
+
+        SSLSocketFactory factory = context.getSocketFactory();
+        HttpsURLConnection.setDefaultSSLSocketFactory(factory);
     }
     
     private Properties getBuildConfig() {

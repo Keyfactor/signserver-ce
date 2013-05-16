a5a11dffb0e7d0c3f3de0eb313f0503cdf5ecfc2
Marcus Lundblad <marcus.lundblad@primekey.com>
2013-05-16 13:34:26 +0000

    DSS-626: Add tests with IPv6 addresses.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3482 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-System/test/org/signserver/server/RemoteAddressAuthorizerTest.java
+++ signserver/modules/SignServer-Test-System/test/org/signserver/server/RemoteAddressAuthorizerTest.java
@@ -181,7 +181,85 @@ public class RemoteAddressAuthorizerTest extends ModulesTestCase {
 
         assertEquals("HTTP response code", 200, responseCode);
     }
-
+    
+    /**
+     * Test that allowing literal localhost in IPv6 short-form is allowed when using short-form.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void test07RequestIPv6Localhost() throws Exception {
+        final RemoteAddressAuthorizer auth = new RemoteAddressAuthorizer();
+        
+        auth.setAllowFromProperty("::1");
+        assertTrue("Allow IPv6 localhost", auth.isAddressAuthorized("::1"));
+    }
+    
+    /**
+     * Test that IPv6 localhost in long-form is accepted when configured to accept the short-form.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void test08RequestIPv6LocalhostLongForm() throws Exception {
+        final RemoteAddressAuthorizer auth = new RemoteAddressAuthorizer();
+        
+        auth.setAllowFromProperty("::1");
+        assertTrue("Allow IPv6 localhost long-form", auth.isAddressAuthorized("0000:0000:0000:0000:0000:0000:0000:0001"));
+    }
+    
+    /**
+     * Test that IPv6 localhost in short-form is accepted when configured to accept the long-form.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void test09RequestIPv6LocalhostShortForm() throws Exception {
+        final RemoteAddressAuthorizer auth = new RemoteAddressAuthorizer();
+        
+        auth.setAllowFromProperty("0000:0000:0000:0000:0000:0000:0000:0001");
+        assertTrue("Allow IPv6 localhost short-form", auth.isAddressAuthorized("::1"));
+    }
+    
+    /**
+     * Test that a non-localhost IPv6 address using some contractions is accepted when in the allow list.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void test10RequestIPv6Accepted() throws Exception {
+        final RemoteAddressAuthorizer auth = new RemoteAddressAuthorizer();
+        
+        auth.setAllowFromProperty("3ffe:1900:4545:3:200:f8ff:fe21:67cf");
+        assertTrue("Allow IPv6 address", auth.isAddressAuthorized("3ffe:1900:4545:3:200:f8ff:fe21:67cf"));
+    }
+    
+    /**
+     * Test that an IPv6 address not in the allow list is not accepted.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void test11RequestIPv6NotAccepted() throws Exception {
+        final RemoteAddressAuthorizer auth = new RemoteAddressAuthorizer();
+        
+        auth.setAllowFromProperty("3ffe:1900:4545:3:200:f8ff:fe21:67cf");
+        assertFalse("Reject other IPv6 address", auth.isAddressAuthorized("3ffe:1900:4545:3:200:f8ff:fe21:77cf"));
+    }
+    
+    /**
+     * Test that accepted IPv6 works when there's also an IPv4 address in the allow list.
+     * 
+     * @throws Exception
+     */
+    @Test
+    public void test12RequestIPv6AcceptedWithIPv4() throws Exception {
+        final RemoteAddressAuthorizer auth = new RemoteAddressAuthorizer();
+        
+        auth.setAllowFromProperty("127.0.0.1, 3ffe:1900:4545:3:200:f8ff:fe21:67cf");
+        assertTrue("Allow IPv6 address", auth.isAddressAuthorized("3ffe:1900:4545:3:200:f8ff:fe21:67cf"));
+    }
+    
     private int process(URL workerUrl) {
         int responseCode = -1;
 

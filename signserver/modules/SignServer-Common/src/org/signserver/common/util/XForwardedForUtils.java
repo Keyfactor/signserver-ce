84594bf83205675e57b6643803000617a50b704a
Marcus Lundblad <marcus.lundblad@primekey.com>
2013-05-23 07:35:51 +0000

    DSS-628: Add a utility method to extract several IP addresses from the X-ForwardedFor header.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3499 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Common/src/org/signserver/common/util/XForwardedForUtils.java
+++ signserver/modules/SignServer-Common/src/org/signserver/common/util/XForwardedForUtils.java
@@ -36,6 +36,28 @@ public class XForwardedForUtils {
      *         or null if the header is missing from the request
      */
     public static String getXForwardedForIP(final RequestContext requestContext) {
+        final String[] ips = getXForwardedForIPs(requestContext, 1);
+        
+        if (ips == null) {
+            return null;
+        } else if (ips.length == 0) {
+            return null;
+        } else {
+            return ips[0];
+        }
+    }
+    
+    /**
+     * Utility method to extract forwarded IP addresses from
+     * the X-Forwarded-For header (if present).
+     * The given number of IP address from the end of the comma-separated list will be returned, or all if the list is shorter.
+     * 
+     * @param requestContext
+     * @param maxAddresses Maximum number of IP addresses to return
+     * @return An array with the IP addresses from the end of X-ForwardedFor header, at most maxAddresses elements, or null if the header is missing.
+     *          If the header is the empty string, a zero-length array is returned.
+     */
+    public static String[] getXForwardedForIPs(final RequestContext requestContext, final int maxAddresses) {
         final String xForwardedFor = (String) requestContext.get(RequestContext.X_FORWARDED_FOR);
         
         if (LOG.isDebugEnabled()) {
@@ -43,22 +65,22 @@ public class XForwardedForUtils {
         }
         
         if (xForwardedFor != null) {
-            // the X-FORWARDED-FOR contains a comma-separated list of IP addresses, take the the last one
-            final String[] ips = xForwardedFor.split(",");
-            final String ip = ips[ips.length - 1].trim();
+            final String ipsString = xForwardedFor.trim();
             
-            if (ip.length() == 0) {
-                // the header was empty
-                return null;
+            if (ipsString.length() == 0) {
+                return new String[0];
             }
             
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Got IP address from X-Forwarded-For: " + ip);
+            final String[] ips = ipsString.split(",");
+            final String[] result = new String[Math.min(maxAddresses, ips.length)];
+            
+            for (int i = 0; i < result.length; i++) {
+                result[i] = ips[ips.length - i - 1].trim();
             }
             
-            return ip;
+            return result;
         }
-        
+       
         return null;
     }
     
--- signserver/modules/SignServer-Common/test/org/signserver/common/util/XForwardedForUtilsTest.java
+++ signserver/modules/SignServer-Common/test/org/signserver/common/util/XForwardedForUtilsTest.java
@@ -83,4 +83,82 @@ public class XForwardedForUtilsTest extends TestCase {
         
         assertEquals("Should return the last IP address in the X-Forwarded-For header", "1.2.3.4", ip);
     }
+    
+    /**
+     * Test that fetching 2 forwarded addresses gets the last two and that exactly two addresses
+     * are returned.
+     * 
+     * @throws Exception
+     */
+    public void test05ReturnTwo() throws Exception {
+        final RequestContext context = new RequestContext();
+        
+        context.put(RequestContext.X_FORWARDED_FOR, "42.42.42.42, 1.2.3.4, 47.47.47.47");
+        
+        final String[] ips = XForwardedForUtils.getXForwardedForIPs(context, 2);
+        
+        assertEquals("Should return 2 IP addresses", 2, ips.length);
+        assertEquals("First element should be last proxied address", "47.47.47.47", ips[0]);
+        assertEquals("Second element should be second last proxied address", "1.2.3.4", ips[1]);
+    }
+    
+    /**
+     * Test that requesting more addresses that are in the request context works as expected,
+     * returning all of them, and don't cause crashes like ArrayIndexOutOfBounds, f.ex.
+     * 
+     * @throws Exception
+     */
+    public void test06ShorterList() throws Exception {
+        final RequestContext context = new RequestContext();
+        
+        context.put(RequestContext.X_FORWARDED_FOR, "42.42.42.42, 1.2.3.4, 47.47.47.47");
+        
+        final String[] ips = XForwardedForUtils.getXForwardedForIPs(context, 5);
+        
+        assertEquals("Should return all addresses", 3, ips.length);
+    }
+    
+    /**
+     * Test that requesting multiple IP addresses with an empty request property
+     * gives an empty array.
+     * 
+     * @throws Exception
+     */
+    public void test07EmptyList() throws Exception {
+        final RequestContext context = new RequestContext();
+        
+        context.put(RequestContext.X_FORWARDED_FOR, "");
+        
+        final String[] ips = XForwardedForUtils.getXForwardedForIPs(context, 5);
+        
+        assertEquals("Should return empty array", 0, ips.length);
+    }
+    
+    /**
+     * Test that requesting multiple IP addresses with no header returns null.
+     * 
+     * @throws Exception
+     */
+    public void test08NoList() throws Exception {
+        final RequestContext context = new RequestContext();
+        final String[] ips = XForwardedForUtils.getXForwardedForIPs(context, 5);
+        
+        assertNull("Should return null", ips);
+    }
+    
+    /**
+     * Test that setting the header to an empty list with some extra whitespace
+     * still gives an empty array of addresses.
+     * 
+     * @throws Exception
+     */
+    public void test09EmptyListExtraWhitespace() throws Exception {
+        final RequestContext context = new RequestContext();
+        
+        context.put(RequestContext.X_FORWARDED_FOR, " ");
+        
+        final String[] ips = XForwardedForUtils.getXForwardedForIPs(context, 5);
+        
+        assertEquals("Should return empty array", 0, ips.length);
+    }
 }

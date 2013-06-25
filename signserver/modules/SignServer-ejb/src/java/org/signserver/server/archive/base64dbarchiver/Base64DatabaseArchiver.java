b42a6514b9df1ff4c17a97b966c1da9d93b88273
Marcus Lundblad <marcus.lundblad@primekey.com>
2013-06-25 07:12:44 +0000

    DSS-646: Remove unused import.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3573 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-ejb/src/java/org/signserver/server/archive/base64dbarchiver/Base64DatabaseArchiver.java
+++ signserver/modules/SignServer-ejb/src/java/org/signserver/server/archive/base64dbarchiver/Base64DatabaseArchiver.java
@@ -16,7 +16,6 @@ import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;

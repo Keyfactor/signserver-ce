210ae7a5aa1fd3123a5de15e04da2df207a851b0
Marcus Lundblad <marcus.lundblad@primekey.com>
2013-06-26 15:53:28 +0000

    DSS-648: Added implementation of signed attribute table generator with the ability to exclude the signingTime attribute.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3590 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Module-TSA/src/java/org/signserver/module/tsa/OptionalSigningTimeSignedAttributeTableGenerator.java
@@ -0,0 +1,52 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.module.tsa;
+
+import java.util.Map;
+
+import org.bouncycastle.asn1.cms.AttributeTable;
+import org.bouncycastle.asn1.cms.CMSAttributes;
+import org.bouncycastle.cms.DefaultSignedAttributeTableGenerator;
+
+/**
+ * Implementation of a signed CMS attibute table generator with
+ * the ability to exclude the signingTime attribute.
+ * 
+ * @author Marcus Lundblad
+ * @version $Id$
+ *
+ */
+public class OptionalSigningTimeSignedAttributeTableGenerator extends
+        DefaultSignedAttributeTableGenerator {
+    
+    private boolean includeSigningTime;
+
+    /**
+     *
+     * @param includeSigningTime Determines whether to include the signingTime attribute
+     */
+    public OptionalSigningTimeSignedAttributeTableGenerator(final boolean includeSigningTime) {
+        this.includeSigningTime = includeSigningTime;
+    }
+
+    @Override
+    public AttributeTable getAttributes(Map parameters) {
+        final AttributeTable attrs = super.getAttributes(parameters);
+        
+        if (!includeSigningTime) {
+            attrs.remove(CMSAttributes.signingTime);
+        }
+        
+        return attrs;
+    }
+}

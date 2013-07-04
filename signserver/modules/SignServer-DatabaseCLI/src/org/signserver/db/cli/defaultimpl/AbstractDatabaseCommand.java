9c3812f5af0a4095231afddce01c73b4107576ce
Markus Kilås <markus.kilas@primekey.com>
2013-07-04 13:17:17 +0000

    DSS-647: Option to start audit log verification from a specified sequence number
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3606 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-DatabaseCLI/src/org/signserver/db/cli/defaultimpl/AbstractDatabaseCommand.java
+++ signserver/modules/SignServer-DatabaseCLI/src/org/signserver/db/cli/defaultimpl/AbstractDatabaseCommand.java
@@ -12,12 +12,7 @@
  *************************************************************************/
 package org.signserver.db.cli.defaultimpl;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Properties;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import org.hibernate.ejb.Ejb3Configuration;
 import org.signserver.cli.spi.AbstractCommand;
 import org.signserver.cli.spi.IllegalCommandArgumentsException;
 
@@ -30,65 +25,17 @@ import org.signserver.cli.spi.IllegalCommandArgumentsException;
  */
 public abstract class AbstractDatabaseCommand extends AbstractCommand {
     
-    private static final String DEFAULT_PU = "cesecore-read";
-    
+    private final DatabaseHelper helper = new DatabaseHelper();
     private EntityManager entityManager;
-    
-    private static final String HSQLDB = "hsqldb";
-    private static final String MYSQL = "mysql";
-    private static final String ORACLE = "oracle";
-    private static final String POSTGRES = "postgres";
-    
-    private static final HashSet<String> types = new HashSet<String>();
-    private static final HashMap<String, String> DIALECTS = new HashMap<String, String>();
-    private static final HashMap<String, String> DRIVERS = new HashMap<String, String>();
-    
-    static {
-        types.add(HSQLDB);
-        types.add(MYSQL);
-        types.add(ORACLE);
-        types.add(POSTGRES);
-        
-        DIALECTS.put(HSQLDB, "org.hibernate.dialect.HSQLDialect");
-        DIALECTS.put(MYSQL, "org.hibernate.dialect.MySQLDialect");
-        DIALECTS.put(ORACLE, "org.hibernate.dialect.Oracle10gDialect");
-        DIALECTS.put(POSTGRES, "org.hibernate.dialect.PostgreSQLDialect");
-        
-        DRIVERS.put(HSQLDB, "org.hsqldb.jdbcDriver");
-        DRIVERS.put(MYSQL, "com.mysql.jdbc.Driver");
-        DRIVERS.put(ORACLE, "oracle.jdbc.driver.OracleDriver");
-        DRIVERS.put(POSTGRES, "org.postgresql.Driver");
-    }
-    
     
     protected EntityManager getEntityManager() throws IllegalCommandArgumentsException {
         if (entityManager == null) {
             final String type = getRequiredProperty("dbcli.database.name");
             if (type == null) {
-                throw new IllegalCommandArgumentsException("Unknown value for dbcli.database.name. Possible values are" + types);
+                throw new IllegalCommandArgumentsException("Unknown value for dbcli.database.name. Possible values are" + helper.getTypes());
             }
             
-            final String dialect = DIALECTS.get(type);
-            final String driverClass = DRIVERS.get(type);
-            final String mappingFile = "META-INF/cesecore-orm-" + type + ".xml";
-            
-            // Properties to override
-            Properties properties = new Properties();
-            properties.put("hibernate.dialect", dialect);
-            // Would have been great if we could do: properties.put("hibernate.PROPERTY-TO-SET-MAPPING-FILE", "META-INF/cesecore-orm-mysql.xml");
-            properties.put("hibernate.connection.url", getRequiredProperty("dbcli.database.url"));
-            properties.put("hibernate.connection.driver_class", driverClass);
-            properties.put("hibernate.connection.username", getRequiredProperty("dbcli.database.username"));
-            properties.put("hibernate.connection.password", getConfiguration().getProperty("dbcli.database.password", ""));
-
-            // Explicitly use Hibernate to get the entity manager factory as we 
-            // must supply an entity mappings-file at runtime
-            final Ejb3Configuration cfg = new Ejb3Configuration()
-                    .addResource(mappingFile)
-                    .configure(DEFAULT_PU, properties);
-            // Would have been nice: final EntityManagerFactory amf = Persistence.createEntityManagerFactory(DEFAULT_PU, properties);
-            final EntityManagerFactory emf = cfg.buildEntityManagerFactory();
-            entityManager = emf.createEntityManager();
+            entityManager = helper.getEntityManager(type, getRequiredProperty("dbcli.database.url"), getRequiredProperty("dbcli.database.username"), getConfiguration().getProperty("dbcli.database.password", ""));
         }
         return entityManager;
     }

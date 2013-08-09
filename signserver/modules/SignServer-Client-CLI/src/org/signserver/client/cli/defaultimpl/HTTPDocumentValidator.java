f46424282e255b448c2bc805f9bb4d0a5cd879a4
Marcus Lundblad <marcus.lundblad@primekey.com>
2013-08-09 12:32:56 +0000

    DSS-147: Implement HTTP validation command.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3653 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Client-CLI/src/org/signserver/client/cli/defaultimpl/HTTPDocumentValidator.java
+++ signserver/modules/SignServer-Client-CLI/src/org/signserver/client/cli/defaultimpl/HTTPDocumentValidator.java
@@ -12,11 +12,15 @@
  *************************************************************************/
 package org.signserver.client.cli.defaultimpl;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URL;
+import java.net.URLConnection;
 
 import org.apache.log4j.Logger;
+import org.bouncycastle.util.encoders.Base64;
 import org.signserver.common.CryptoTokenOfflineException;
 import org.signserver.common.IllegalRequestException;
 import org.signserver.common.SignServerException;
@@ -31,6 +35,11 @@ import org.signserver.common.SignServerException;
 public class HTTPDocumentValidator extends AbstractDocumentValidator {
     /** Logger for this class. */
     private static final Logger LOG = Logger.getLogger(HTTPDocumentValidator.class);
+    
+    public static final String CRLF = "\r\n";
+    private static final String BASICAUTH_AUTHORIZATION = "Authorization";
+    private static final String BASICAUTH_BASIC = "Basic";
+    private static final String BOUNDARY = "------------------signserver";
     
     private URL processServlet;
     private String workerName;
@@ -68,8 +77,92 @@ public class HTTPDocumentValidator extends AbstractDocumentValidator {
                     + " to worker " + workerName);
         }
 
+        InputStream in = null;
         
+        try {
+            final URLConnection conn = processServlet.openConnection();
+            
+            conn.setDoOutput(true);
+            conn.setAllowUserInteraction(false);
 
+            if (username != null && password != null) {
+                conn.setRequestProperty(BASICAUTH_AUTHORIZATION, 
+                        BASICAUTH_BASIC + " "
+                        + new String(Base64.encode((username + ":" + password).getBytes())));
+            }
+            
+            final StringBuilder sb = new StringBuilder();
+            sb.append("--" + BOUNDARY);
+            sb.append(CRLF);
+            
+            if (workerName == null) {
+                sb.append("Content-Disposition: form-data; name=\"workerId\"");
+                sb.append(CRLF);
+                sb.append(CRLF);
+                sb.append(workerId);
+            } else {
+                sb.append("Content-Disposition: form-data; name=\"workerName\"");
+                sb.append(CRLF);
+                sb.append(CRLF);
+                sb.append(workerName);
+            }
+            sb.append(CRLF);
+            
+            sb.append("Content-Type: application/octet-stream");
+            sb.append(CRLF);
+            sb.append("Content-Transfer-Encoding: binary");
+            sb.append(CRLF);
+            sb.append(CRLF);
+
+            conn.addRequestProperty("Content-Type",
+                    "multipart/form-data; boundary=" + BOUNDARY);
+            conn.addRequestProperty("Content-Length", String.valueOf(
+                    sb.toString().length() + BOUNDARY.length() + 8-1));
+            
+            out = conn.getOutputStream();
+            
+            out.write(sb.toString().getBytes());
+            out.write(data);
+            
+            out.write(("\r\n--" + BOUNDARY + "--\r\n").getBytes());
+            out.flush();
+            
+            // Get the response
+            in = conn.getInputStream();
+            final ByteArrayOutputStream os = new ByteArrayOutputStream();
+            int len;
+            final byte[] buf = new byte[1024];
+            while ((len = in.read(buf)) > 0) {
+                os.write(buf, 0, len);
+            }
+            os.close();
+            
+            // read string from response
+            final String response = os.toString();
+            
+            if ("VALID".equals(response)) {
+                out.write(("Valid: " + Boolean.TRUE.booleanValue()).getBytes());
+            } else {
+                out.write(("Valid: " + Boolean.FALSE.booleanValue()).getBytes());
+            }
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        } finally {
+            if (out != null) {
+                try {
+                    out.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        }
     }
 
 }

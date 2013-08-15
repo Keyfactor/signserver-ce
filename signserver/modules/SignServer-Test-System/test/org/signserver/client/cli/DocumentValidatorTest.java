95d06803070c78ebe5fe33a432aa24d67d486b64
Marcus Lundblad <marcus.lundblad@primekey.com>
2013-08-15 12:27:25 +0000

    DSS-147: Added tests using the -protocol option for the validatedocument CLI command.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3693 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-System/test/org/signserver/client/cli/DocumentValidatorTest.java
+++ signserver/modules/SignServer-Test-System/test/org/signserver/client/cli/DocumentValidatorTest.java
@@ -106,22 +106,26 @@ public class DocumentValidatorTest extends ModulesTestCase {
             fail("Should have thrown exception about missing arguments");
         } catch (IllegalCommandArgumentsException expected) {}
     }
-
-    /**
-     * Tests the sample use case a from the documentation.
-     * <pre>
-     * a) signdocument -workername XMLSigner -data "&lt;root/&gt;"
-     * </pre>
-     * @throws Exception
-     */
-    public void test02validateDocumentFromParameter() throws Exception {
+    
+    
+    private void testValidateDocumentFromParameter(final String protocol) throws Exception {
         try {
             String res =
-                    new String(execute("validatedocument",
-                    "-workername", "TestXMLValidator",
-                    "-data", XMLValidatorTestData.TESTXML1,
-                    "-truststore", new File(new File(signserverhome), "p12/truststore.jks").getAbsolutePath(),
-                    "-truststorepwd", getTruststorePassword()));
+                    new String(protocol == null ?
+                            execute("validatedocument",
+                                    "-workername", "TestXMLValidator",
+                                    "-data", XMLValidatorTestData.TESTXML1,
+                                    "-truststore", new File(new File(signserverhome), "p12/truststore.jks").getAbsolutePath(),
+                                    "-truststorepwd", getTruststorePassword()) :
+                            execute("validatedocument",
+                                    "-workername", "TestXMLValidator",
+                                    "-data", XMLValidatorTestData.TESTXML1,
+                                    "-truststore", new File(new File(signserverhome), "p12/truststore.jks").getAbsolutePath(),
+                                    "-truststorepwd", getTruststorePassword(),
+                                    "-protocol", protocol));
+                    
+                        
+            
             assertTrue("contains Valid: true: "
                     + res, res.contains("Valid: true"));
         } catch (IllegalCommandArgumentsException ex) {
@@ -130,14 +134,7 @@ public class DocumentValidatorTest extends ModulesTestCase {
         }
     }
 
-    /**
-     * Tests the sample use case b from the documentation.
-     * <pre>
-     * b) signdocument -workername XMLSigner -infile /tmp/document.xml
-     * </pre>
-     * @throws Exception
-     */
-    public void test02signDocumentFromFile() throws Exception {
+    private void testValidateDocumentFromFile(final String protocol) throws Exception {
         try {
             final File doc = File.createTempFile("test2.xml", null);
             FileOutputStream out = null;
@@ -152,10 +149,16 @@ public class DocumentValidatorTest extends ModulesTestCase {
             }
 
             String res =
-                    new String(execute("validatedocument", "-workername",
-                    "TestXMLValidator", "-infile", doc.getAbsolutePath(),
-                    "-truststore", new File(new File(signserverhome), "p12/truststore.jks").getAbsolutePath(),
-                    "-truststorepwd", getTruststorePassword()));
+                    new String(protocol == null ?
+                            execute("validatedocument", "-workername",
+                                    "TestXMLValidator", "-infile", doc.getAbsolutePath(),
+                                    "-truststore", new File(new File(signserverhome), "p12/truststore.jks").getAbsolutePath(),
+                                    "-truststorepwd", getTruststorePassword()) :
+                            execute("validatedocument", "-workername",
+                                    "TestXMLValidator", "-infile", doc.getAbsolutePath(),
+                                    "-truststore", new File(new File(signserverhome), "p12/truststore.jks").getAbsolutePath(),
+                                    "-truststorepwd", getTruststorePassword(),
+                                    "-protocol", protocol));
             assertTrue("contains Valid: true: "
                     + res, res.contains("Valid: true"));
         } catch (IllegalArgumentException ex) {
@@ -163,7 +166,64 @@ public class DocumentValidatorTest extends ModulesTestCase {
             fail(ex.getMessage());
         }
     }
+    
+    /**
+     * Tests the sample use case a from the documentation.
+     * <pre>
+     * a) validatedocument -workername XMLSigner -data "&lt;root/&gt;" -truststore $SIGNSERVER_HOME/p12/truststore.jks -truststorepwd foo123
+     * </pre>
+     * @throws Exception
+     */
+    public void test02ValidateDocumentFromParameterDefaultProtocol() throws Exception {
+        testValidateDocumentFromParameter(null);
+    }
+    
+    /**
+     * Test with explicitly setting -protocol WEBSERVICES (the default).
+     * 
+     * @throws Exception
+     */
+    public void test03ValidateDocumentFromParameterWebservices() throws Exception {
+        testValidateDocumentFromParameter("WEBSERVICES");
+    }
+    
+    /**
+     * Test with -protocol HTTP.
+     * 
+     * @throws Exception
+     */
+    public void test04ValidateDocumentFromParameterHTTP() throws Exception {
+        testValidateDocumentFromParameter("HTTP");
+    }
 
+    /**
+     * Tests the sample use case b from the documentation.
+     * <pre>
+     * b) signdocument -workername XMLSigner -infile /tmp/document.xml
+     * </pre>
+     * @throws Exception
+     */
+    public void test05ValidateDocumentFromFileDefaultProtocol() throws Exception {
+        testValidateDocumentFromFile(null);
+    }
+
+    /**
+     * Test with -protcol WEBSERVICES and -infile.
+     * 
+     * @throws Exception
+     */
+    public void test06ValidateDocumentFromFileWebservices() throws Exception {
+       testValidateDocumentFromFile("WEBSERVICES"); 
+    }
+    
+    /**
+     * Test with -protocol HTTP and -infile.
+     * 
+     * @throws Exception
+     */
+    public void test07ValidateDocumentFromFileHTTP() throws Exception {
+        testValidateDocumentFromFile("HTTP");
+    }
 
     public void test99TearDownDatabase() throws Exception {
         removeWorker(WORKERID);

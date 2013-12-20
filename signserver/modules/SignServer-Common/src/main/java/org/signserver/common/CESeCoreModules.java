31f59f934a70915d5e371f9607378f5ae55f2bc0
Markus Kilås <markus.kilas@primekey.com>
2013-12-20 13:56:32 +0000

    DSS-606: Fixed Remote EJB JNDI lookups for JBoss 7
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@3899 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Common/src/main/java/org/signserver/common/CESeCoreModules.java
@@ -0,0 +1,25 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.common;
+
+/**
+ * Names of known CESeCore EJB modules.
+ *
+ * @author Markus Kilås
+ * @version $Id$
+ */
+public interface CESeCoreModules {
+
+    /** The main CESeCore ejb-jar. */
+    String CORE = "cesecore-ejb_1.1.2";
+}

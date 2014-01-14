52d1f91ca2eab3a9f114cbc626fc85c790323652
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-01-14 09:42:50 +0000

    DSS-701: Remove unused import.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4006 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-XAdES/src/main/java/org/signserver/module/xades/signer/XAdESSigner.java
+++ signserver/modules/SignServer-Module-XAdES/src/main/java/org/signserver/module/xades/signer/XAdESSigner.java
@@ -19,7 +19,6 @@ import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import javax.persistence.EntityManager;

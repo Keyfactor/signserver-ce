8ae7e6eeafc6938febbb383d726ac54b844de289
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-02-14 11:27:45 +0000

    DSS-283: Add skeleton for unit tests of the properties parser.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4296 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Common/src/test/java/org/signserver/common/util/PropertiesParserTest.java
@@ -0,0 +1,26 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package java.org.signserver.common.util;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit tests for the properties parser.
+ * 
+ * @author Marcus Lundblad
+ * @version $Id$
+ *
+ */
+public class PropertiesParserTest extends TestCase {
+
+}

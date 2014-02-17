2cb422e17049c145a70bc8f06281daa79de73653
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-02-17 06:20:25 +0000

    DSS-283: Added test case for basic parsing.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4300 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Common/src/test/java/org/signserver/common/util/PropertiesParserTest.java
+++ signserver/modules/SignServer-Common/src/test/java/org/signserver/common/util/PropertiesParserTest.java
@@ -10,7 +10,16 @@
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
  *************************************************************************/
-package java.org.signserver.common.util;
+package org.signserver.common.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.Properties;
+
+import org.signserver.common.util.PropertiesParser;
+import org.signserver.common.util.PropertiesParser.GlobalProperty;
+import org.signserver.common.util.PropertiesParser.WorkerProperty;
 
 import junit.framework.TestCase;
 
@@ -22,5 +31,55 @@ import junit.framework.TestCase;
  *
  */
 public class PropertiesParserTest extends TestCase {
+    
+    private static String correctConfig =
+            "GLOB.WORKER42.CLASSPATH = foo.bar.Worker\n" +
+            "GLOB.WORKER42.SIGNERTOKEN.CLASSPATH = foo.bar.Token\n" +
+            "WORKER42.FOOBAR = Some value\n";
+
+    private boolean containsGlobalProperty(final String scope, final String key,
+                                            final String value,
+                                            final Map<GlobalProperty, String> props) {
+        final String foundValue = props.get(new GlobalProperty(scope, key));
+        
+        return foundValue != null && foundValue.equals(value);
+    }
+    
+    private boolean containsWorkerProperty(final String workerIdOrName,
+                                            final String key, final String value,
+                                            final Map<WorkerProperty, String> props) {
+        final String foundValue = props.get(new WorkerProperty(workerIdOrName, key));
+        
+        return foundValue != null && foundValue.equals(value);
+    }
+    
+    public void testParsingCorrect() throws Exception {
+        final Properties prop = new Properties();
+        final PropertiesParser parser = new PropertiesParser();
+        
+        try {
+            prop.load(new ByteArrayInputStream(correctConfig.getBytes()));
+            parser.process(prop);
+            
+            final Map<GlobalProperty, String> globalProps = parser.getSetGlobalProperties();
+            final Map<WorkerProperty, String> workerProps = parser.getSetWorkerProperties();
+            
+            assertEquals("Number of global properties", 2, globalProps.size());
+            assertEquals("Number of worker properties", 1, workerProps.size());
+            
+            assertTrue("Should contain global property",
+                    containsGlobalProperty("GLOB.", "WORKER42.CLASSPATH",
+                            "foo.bar.Worker", globalProps));
+            assertTrue("Should contain global property",
+                    containsGlobalProperty("GLOB.", "WORKER42.SIGNERTOKEN.CLASSPATH",
+                            "foo.bar.Token", globalProps));
+            assertTrue("Should contain worker property",
+                    containsWorkerProperty("42", "FOOBAR", "Some value",
+                            workerProps));
+            
+        } catch (IOException e) {
+            fail("Failed to parse properties");
+        }
+    }
 
 }

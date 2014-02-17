ebb8b1736635e28518307914ca11736b01553438
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-02-17 13:28:26 +0000

    DSS-283: Added skeleton test case for the property applier.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4312 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Common/src/test/java/org/signserver/common/util/PropertiesApplierTest.java
@@ -0,0 +1,117 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package java.org.signserver.common.util;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.signserver.common.AuthorizedClient;
+import org.signserver.common.util.PropertiesApplier;
+import org.signserver.common.util.PropertiesConstants;
+import org.signserver.common.util.PropertiesParser.GlobalProperty;
+import org.signserver.common.util.PropertiesParser.WorkerProperty;
+
+import junit.framework.TestCase;
+
+/**
+ * Tests for the property applier used for loading configuration property files.
+ * 
+ * @author Marcus Lundblad
+ * @version $Id$
+ *
+ */
+public class PropertiesApplierTest extends TestCase {
+
+    private static class MockPropertiesApplier extends PropertiesApplier {
+
+        private Map<GlobalProperty, String> globalProperties = new HashMap<GlobalProperty, String>();
+        private Map<WorkerProperty, String> workerProperties = new HashMap<WorkerProperty, String>();
+        
+        public String getWorkerProperty(final int workerId, final String key) {
+            return workerProperties.get(new WorkerProperty(Integer.toString(workerId), key));
+        }
+        
+        public String getGlobalProperty(final String scope, final String key) {
+            return globalProperties.get(new GlobalProperty(scope, key));
+        }
+        
+        @Override
+        protected void setGlobalProperty(String scope, String key, String value) {
+            globalProperties.put(new GlobalProperty(scope, key), value);
+        }
+
+        @Override
+        protected void removeGlobalProperty(String scope, String key) {
+            globalProperties.remove(new GlobalProperty(scope, key));
+        }
+
+        @Override
+        protected void setWorkerProperty(int workerId, String key, String value) {
+            workerProperties.put(new WorkerProperty(Integer.toString(workerId), key), value);
+        }
+
+        @Override
+        protected void removeWorkerProperty(int workerId, String key) {
+            workerProperties.remove(new WorkerProperty(Integer.toString(workerId), key));
+        }
+
+        @Override
+        protected void uploadSignerCertificate(int workerId, byte[] signerCert) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        protected void uploadSignerCertificateChain(int workerId,
+                List<byte[]> signerCertChain) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        protected void addAuthorizedClient(int workerId, AuthorizedClient ac) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        protected void removeAuthorizedClient(int workerId, AuthorizedClient ac) {
+            // TODO Auto-generated method stub
+            
+        }
+
+        @Override
+        protected int genFreeWorkerId() throws PropertiesApplierException {
+            
+        }
+
+        @Override
+        protected int getWorkerId(String workerName)
+                throws PropertiesApplierException {
+            for (final WorkerProperty prop : workerProperties.keySet()) {
+                if (PropertiesConstants.NAME.equals(prop.getKey())) {
+                    final String value = workerProperties.get(prop);
+                    
+                    if (workerName.equals(value)) {
+                        return Integer.valueOf(prop.getWorkerIdOrName());
+                    }
+                }
+            }
+            
+            throw new PropertiesApplierException("No such worker: " + workerName);
+        }
+        
+    }
+    
+}

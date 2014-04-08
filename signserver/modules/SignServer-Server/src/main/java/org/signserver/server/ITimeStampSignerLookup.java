0792dc8531d63d2bd1616eef6c07c3bfd88c942f
Markus Kilås <markus.kilas@primekey.com>
2014-04-08 13:37:50 +0000

    DSS-214: Removing old classes never put in use
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4562 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/main/java/org/signserver/server/ITimeStampSignerLookup.java
+++
@@ -1,6 +0,0 @@
-package org.signserver.server;
-
-public interface ITimeStampSignerLookup extends IWorkerLookup {
-
-    public static final String TSA_REQUESTEDPOLICYOID = "TSA_REQUESTEDPOLICYOID";
-}

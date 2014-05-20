fd82f49e450b3e0ec6936f8034f06d49718310a6
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-05-20 12:41:41 +0000

    DSS-805: Fix unit test failure.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/branches/Branch_3_5@4734 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/main/java/org/signserver/server/signers/BaseSigner.java
+++ signserver/modules/SignServer-Server/src/main/java/org/signserver/server/signers/BaseSigner.java
@@ -149,7 +149,9 @@ public abstract class BaseSigner extends BaseProcessable implements ISigner {
     @Override
     protected List<String> getFatalErrors() {
         final LinkedList<String> errors = new LinkedList<String>(super.getFatalErrors());
-        errors.addAll(getSignerCertificateFatalErrors());
+        if (!Boolean.parseBoolean(config.getProperty("NOCERTIFICATES", Boolean.FALSE.toString()))) {
+            errors.addAll(getSignerCertificateFatalErrors());
+        }
         errors.addAll(configErrors);
         return errors;
     }

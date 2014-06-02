2b5126d9fc10f66c3d43de0bef2a773873893c57
Markus Kilås <markus.kilas@primekey.com>
2014-06-02 12:16:19 +0000

    DSS-816: Invoking the internal TSA using a special session bean
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/branches/Branch_3_5@4789 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-PDFSigner/src/main/java/org/signserver/module/pdfsigner/InternalTSAClient.java
+++ signserver/modules/SignServer-Module-PDFSigner/src/main/java/org/signserver/module/pdfsigner/InternalTSAClient.java
@@ -1,40 +1,41 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.signserver.module.pdfsigner;
-
-import java.math.BigInteger;
-
-import org.apache.log4j.Logger;
-import org.signserver.common.GenericSignRequest;
-import org.signserver.common.GenericSignResponse;
-import org.signserver.common.ProcessResponse;
-import org.signserver.common.RequestContext;
-import org.signserver.common.SignServerException;
-import org.signserver.ejb.interfaces.IInternalWorkerSession;
-import org.signserver.ejb.interfaces.IWorkerSession;
-import org.signserver.server.UsernamePasswordClientCredential;
 
 import com.lowagie.text.pdf.PdfPKCS7;
 import com.lowagie.text.pdf.TSAClient;
+import org.bouncycastle.asn1.x509.X509ObjectIdentifiers;
+import org.bouncycastle.tsp.TimeStampToken;
+import org.signserver.ejb.interfaces.IInternalWorkerSession;
+import org.signserver.server.tsa.InternalTimeStampTokenFetcher;
 
-import org.bouncycastle.asn1.x509.*;
-import org.bouncycastle.tsp.*;
-
+/**
+ * TSA Client fetching the token internally.
+ *
+ * @author Markus Kilås
+ * @version $Id$
+ */
 public class InternalTSAClient implements TSAClient {
-    private static Logger LOG = Logger.getLogger(InternalTSAClient.class);
-    
-    private IInternalWorkerSession session;
-    private String workerNameOrId;
-    private String username;
-    private String password;
+
+    private final InternalTimeStampTokenFetcher fetcher;
+
     private int tokenSizeEstimated = 7168;
-    
+
     public InternalTSAClient(final IInternalWorkerSession session, final String workerNameOrId,
             final String username, final String password) {
-        this.session = session;
-        this.workerNameOrId = workerNameOrId;
-        this.username = username;
-        this.password = password;
+        this.fetcher = new InternalTimeStampTokenFetcher(session, workerNameOrId, username, password);
     }
-    
+
     @Override
     public int getTokenSizeEstimate() {
         return tokenSizeEstimated;
@@ -43,51 +44,10 @@ public class InternalTSAClient implements TSAClient {
     @Override
     public byte[] getTimeStampToken(PdfPKCS7 caller, byte[] imprint)
             throws Exception {
-        int workerId;
-        try {
-            workerId = Integer.parseInt(workerNameOrId);
-        } catch (NumberFormatException ex) {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug("Not a workerId, maybe workerName: " + workerNameOrId);
-            }
-            workerId = session.getWorkerId(workerNameOrId);
-        }
-        
-        // Setup the time stamp request
-        TimeStampRequestGenerator tsqGenerator = new TimeStampRequestGenerator();
-        tsqGenerator.setCertReq(true);
-
-        BigInteger nonce = BigInteger.valueOf(System.currentTimeMillis());
-        TimeStampRequest request = tsqGenerator.generate(X509ObjectIdentifiers.id_SHA1, imprint, nonce);
-        byte[] requestBytes = request.getEncoded();
-
-        final RequestContext context = new RequestContext();
-        
-        if (username != null && password != null) {
-            context.put(RequestContext.CLIENT_CREDENTIAL,
-                    new UsernamePasswordClientCredential(username, password));
-        }
-        
-        final ProcessResponse resp = session.process(workerId, new GenericSignRequest(hashCode(), requestBytes), context);
-    
-        if (resp instanceof GenericSignResponse) {
-            final byte[] respBytes = ((GenericSignResponse) resp).getProcessedData();
-        
-            TimeStampResponse response = new TimeStampResponse(respBytes);
-            
-            TimeStampToken  tsToken = response.getTimeStampToken();
-            if (tsToken == null) {
-                throw new SignServerException("TSA '" + workerNameOrId + "' failed to return time stamp token: " + response.getStatusString());
-            }
-
-            byte[] encoded = tsToken.getEncoded();
-            tokenSizeEstimated = encoded.length;
-
-            return encoded;
-        } else {
-            throw new SignServerException("Unknown response");
-        }
-        
+        final TimeStampToken token = fetcher.fetchToken(imprint, X509ObjectIdentifiers.id_SHA1);
+        final byte[] encoded = token.getEncoded();
+        tokenSizeEstimated = encoded.length;
+        return encoded;
     }
 
 }
---
+++ signserver/modules/SignServer-Module-XAdES/src/main/java/org/signserver/module/xades/signer/InternalTimeStampTokenProvider.java
@@ -0,0 +1,86 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.module.xades.signer;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.xml.security.algorithms.MessageDigestAlgorithm;
+import org.bouncycastle.asn1.ASN1ObjectIdentifier;
+import org.bouncycastle.tsp.TSPAlgorithms;
+import org.bouncycastle.tsp.TSPException;
+import org.bouncycastle.tsp.TimeStampToken;
+import org.signserver.common.CryptoTokenOfflineException;
+import org.signserver.common.IllegalRequestException;
+import org.signserver.common.SignServerException;
+import org.signserver.ejb.interfaces.IInternalWorkerSession;
+import org.signserver.server.tsa.InternalTimeStampTokenFetcher;
+import xades4j.UnsupportedAlgorithmException;
+import xades4j.providers.MessageDigestEngineProvider;
+import xades4j.providers.TimeStampTokenGenerationException;
+import xades4j.providers.TimeStampTokenProvider;
+
+/**
+ * Time-stamp token provider using the internal worker session.
+ *
+ * @author Markus Kilås
+ * @version $Id$
+ */
+public class InternalTimeStampTokenProvider implements TimeStampTokenProvider {
+
+    /** Mapping from digest URI (xml-sec) to OID. */
+    private static final Map<String, ASN1ObjectIdentifier> digestUriToOidMap;
+    static {
+        digestUriToOidMap = new HashMap<String, ASN1ObjectIdentifier>(6);
+        digestUriToOidMap.put(MessageDigestAlgorithm.ALGO_ID_DIGEST_NOT_RECOMMENDED_MD5, TSPAlgorithms.MD5);
+        digestUriToOidMap.put(MessageDigestAlgorithm.ALGO_ID_DIGEST_RIPEMD160, TSPAlgorithms.RIPEMD160);
+        digestUriToOidMap.put(MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA1, TSPAlgorithms.SHA1);
+        digestUriToOidMap.put(MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA256, TSPAlgorithms.SHA256);
+        digestUriToOidMap.put(MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA384, TSPAlgorithms.SHA384);
+        digestUriToOidMap.put(MessageDigestAlgorithm.ALGO_ID_DIGEST_SHA512, TSPAlgorithms.SHA512);
+    }
+
+    private final MessageDigestEngineProvider messageDigestProvider;
+
+    private final InternalTimeStampTokenFetcher fetcher;
+
+    public InternalTimeStampTokenProvider(final MessageDigestEngineProvider messageDigestProvider,
+            final IInternalWorkerSession session, final String workerNameOrId, final String username, final String password) {
+        this.messageDigestProvider = messageDigestProvider;
+        this.fetcher = new InternalTimeStampTokenFetcher(session, workerNameOrId, username, password);
+    }
+
+    @Override
+    public TimeStampTokenRes getTimeStampToken(final byte[] tsDigestInput, final String digestAlgUri) throws TimeStampTokenGenerationException {
+        try {
+            MessageDigest md = messageDigestProvider.getEngine(digestAlgUri);
+            byte[] imprint = md.digest(tsDigestInput);
+            TimeStampToken token = fetcher.fetchToken(imprint, digestUriToOidMap.get(digestAlgUri));
+            return new TimeStampTokenRes(imprint, token.getTimeStampInfo().getGenTime());
+        } catch (UnsupportedAlgorithmException ex) {
+            throw new TimeStampTokenGenerationException("Digest algorithm not supported", ex);
+        } catch (IllegalRequestException ex) {
+            throw new TimeStampTokenGenerationException("The time-stamp request failed", ex);
+        } catch (CryptoTokenOfflineException ex) {
+            throw new TimeStampTokenGenerationException("The time-stamp request could not be process because of offline TSA", ex);
+        } catch (SignServerException ex) {
+            throw new TimeStampTokenGenerationException("The time-stamp request could not be process because of internal error in the TSA", ex);
+        } catch (TSPException ex) {
+            throw new TimeStampTokenGenerationException("Invalid time stamp response", ex);
+        } catch (IOException ex) {
+            throw new TimeStampTokenGenerationException("Encoding error", ex);
+        }
+    }
+
+}

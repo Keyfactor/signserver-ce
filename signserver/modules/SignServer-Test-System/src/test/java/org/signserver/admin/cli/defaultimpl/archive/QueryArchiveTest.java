2b9f811835292528787b20a582ba43cb3f7859a3
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-08-07 11:51:27 +0000

    DSS-470: Add test for CLI archive query parser.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4951 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Test-System/src/test/java/org/signserver/admin/cli/defaultimpl/archive/QueryArchiveTest.java
@@ -0,0 +1,187 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.admin.cli.defaultimpl.archive;
+
+import java.text.ParseException;
+
+import org.cesecore.util.query.elems.RelationalOperator;
+import org.cesecore.util.query.elems.Term;
+import org.signserver.common.ArchiveMetadata;
+
+import junit.framework.TestCase;
+
+/**
+ * Test the archive query parser.
+ * 
+ * @author Marcus Lundblad
+ * @version $Id$
+ *
+ */
+public class QueryArchiveTest extends TestCase {
+    /**
+     * Test with a valid criteria.
+     * @throws Exception
+     */
+    public void test01ParseCriteria() throws Exception {
+        final String criteria = "archiveid EQ 1";
+        final Term term = QueryArchiveCommand.parseCriteria(criteria);
+        
+        assertEquals("Operation", RelationalOperator.EQ, term.getOperator());
+        assertEquals("Name", ArchiveMetadata.ARCHIVE_ID, term.getName());
+        assertEquals("Value", "1", term.getValue());
+    }
+    
+    /**
+     * Test that a non-existing operator isn't accepted.
+     * @throws Exception
+     */
+    public void test02ParseCriteriaInvalidOperator() throws Exception {
+        final String criteria = "signerid FOO 1";
+        
+        try {
+            final Term term = QueryArchiveCommand.parseCriteria(criteria);
+            fail("Should throw an IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            // expected
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.getClass().getName());
+        }
+    }
+
+    /**
+     * Test that the BETWEEN operator is properly rejected.
+     * @throws Exception
+     */
+    public void test03ParseCriteriaBetween() throws Exception {
+        final String criteria = "signerid BETWEEN 1";
+        
+        try {
+            final Term term = QueryArchiveCommand.parseCriteria(criteria);
+            fail("Should throw an IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            // expected
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.getClass().getName());
+        }
+    }
+    
+    /**
+     * Test that using numerical field yields an Integer value (otherwise hibernate will get upset...)
+     * @throws Exception
+     */
+    public void test04ParseCriteriaNumericValue() throws Exception {
+        final String criteria = "signerid GT 1";
+        final Term term = QueryArchiveCommand.parseCriteria(criteria);
+        
+        assertEquals("Operation", RelationalOperator.GT, term.getOperator());
+        assertEquals("Name", ArchiveMetadata.SIGNER_ID, term.getName());
+        assertEquals("Value", Integer.valueOf(1), term.getValue());
+    }
+    
+    /**
+     * Test the NULL operator.
+     */
+    public void test05ParseCriteriaNull() throws Exception {
+        final String criteria = "requestCertSerialnumber NULL";
+        final Term term = QueryArchiveCommand.parseCriteria(criteria);
+        
+        assertEquals("Operation", RelationalOperator.NULL, term.getOperator());
+        assertEquals("Name", ArchiveMetadata.REQUEST_CERT_SERIAL_NUMBER, term.getName());
+        assertNull("Value", term.getValue());
+    }
+    
+    /**
+     * Test that setting a non-numeric value for a numeric field fails.
+     * @throws Exception
+     */
+    public void test06ParseCriteriaInvalidValue() throws Exception {
+        final String criteria = "signerid EQ foo";
+        
+        try {
+            final Term term = QueryArchiveCommand.parseCriteria(criteria);
+            fail("Should throw a NumberFormatException");
+        } catch (NumberFormatException e) {
+            // expected
+        } catch (Exception e) {
+            fail("Unexpect exception: " + e.getClass().getName());
+        }
+    }
+    
+    public void test07ParseCriteriaWithoutValue() throws Exception {
+        final String criteria = "signerid EQ";
+        
+        try {
+            final Term term = QueryArchiveCommand.parseCriteria(criteria);
+            fail("Should throw an IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            // expected
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.getClass().getName());
+        }
+    }
+    
+    /**
+     * Test that using an invalid field name is rejected.
+     * @throws Exception
+     */
+    public void test08ParseCriteriaInvalidField() throws Exception {
+        final String criteria = "dummyField EQ 0";
+        
+        try {
+            final Term term = QueryArchiveCommand.parseCriteria(criteria);
+            fail("Should throw an IllegalArgumentException");
+        } catch (IllegalArgumentException e) {
+            // expected
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.getClass().getName());
+        }
+    }
+    
+    /**
+     * Test that parsing a criteria with a millisecond time stamp works.
+     * @throws Exception
+     */
+    public void test09DateMilliseconds() throws Exception {
+        final String criteria = "time EQ 1234567890";
+        final Term term = QueryArchiveCommand.parseCriteria(criteria);
+        
+        assertEquals("Operation", RelationalOperator.EQ, term.getOperator());
+        assertEquals("Name", ArchiveMetadata.TIME, term.getName());
+        assertEquals("Value", Long.valueOf(1234567890), term.getValue());
+    }
+    
+    /**
+     * Test that parsing a date criteria using an ISO format date works.
+     * @throws Exception
+     */
+    public void test10DateISO() throws Exception {
+        final String criteria = "time EQ 2013-02-11 14:00:00+0100";
+        final Term term = QueryArchiveCommand.parseCriteria(criteria);
+        
+        assertEquals("Operation", RelationalOperator.EQ, term.getOperator());
+        assertEquals("Name", ArchiveMetadata.TIME, term.getName());
+        assertEquals("Value", Long.valueOf(1360587600000L), term.getValue()); 
+    }
+    
+    public void test11DateInvalid() throws Exception {
+        final String criteria = "time EQ foobar";
+        
+        try {
+            final Term term = QueryArchiveCommand.parseCriteria(criteria);
+        } catch (ParseException e) {
+            // expected
+        } catch (Exception e) {
+            fail("Unexpected exception: " + e.getClass().getName());
+        }
+    }
+}

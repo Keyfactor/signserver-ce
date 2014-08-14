30e6002d29f9268a3adc10ca59ef822f0f6532cf
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-08-14 15:04:57 +0000

    DSS-820: Remove commented-out code. Resurrect prepoluted combobox values for time conditions.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4966 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-AdminGUI/src/main/java/org/signserver/admin/gui/AddConditionDialog.java
+++ signserver/modules/SignServer-AdminGUI/src/main/java/org/signserver/admin/gui/AddConditionDialog.java
@@ -33,34 +33,10 @@ import org.signserver.admin.gui.adminws.gen.RelationalOperator;
  */
 public class AddConditionDialog extends javax.swing.JDialog {
 
-    // TODO: Sort of Duplicated in AuditlogTableModel and other places?
-    /*
-    private static final AuditlogColumn[] COLUMNS =  {
-        new AuditlogColumn(AuditRecordData.FIELD_ADDITIONAL_DETAILS, "Details"),
-        new AuditlogColumn(AuditRecordData.FIELD_AUTHENTICATION_TOKEN, "Admin Subject"),
-        new AuditlogColumn(AuditRecordData.FIELD_CUSTOM_ID, "Admin Issuer"),
-        new AuditlogColumn(AuditRecordData.FIELD_EVENTSTATUS, "Outcome"),
-        new AuditlogColumn(AuditRecordData.FIELD_EVENTTYPE, "Event"),
-        new AuditlogColumn(AuditRecordData.FIELD_MODULE, "Module"),
-        new AuditlogColumn(AuditRecordData.FIELD_NODEID, "Node"),
-        new AuditlogColumn(AuditRecordData.FIELD_SEARCHABLE_DETAIL1, "Admin Serial Number"),
-        new AuditlogColumn(AuditRecordData.FIELD_SEARCHABLE_DETAIL2, "Worker ID"),
-        new AuditlogColumn(AuditRecordData.FIELD_SERVICE, "Service"),
-        new AuditlogColumn(AuditRecordData.FIELD_SEQUENCENUMBER, "Sequence Number"),
-        new AuditlogColumn(AuditRecordData.FIELD_TIMESTAMP, "Time")
-    };
-     */
+    
     
     // TODO: this should be overridable to enable dynamically changing the dialog
     private static final Object[] COLUMNS = AuditlogColumn.values();
-    
-    /** Relational operator used by each column. */
-    /*
-    private static final HashMap<String, QueryOperator[]> OPERATORS = new HashMap<String, QueryOperator[]>();
-     */
-
-    /** Available values by each column. */
-    //private static final HashMap<String, List<String>> VALUES = new HashMap<String, List<String>>();
     
     /** Relational operators useful for text values. */
     private static final QueryOperator[] TEXT_OPERATORS = {
@@ -89,44 +65,7 @@ public class AddConditionDialog extends javax.swing.JDialog {
         QueryOperator.fromEnum(RelationalOperator.NULL)
     };
     
-    /** Available values for event status. */
-    //private static final List<String> STATUS_VALUES; 
-    
-    /** Available values for time. */
-    //private static final List<String> TIME_VALUES;
-    
     private static final SimpleDateFormat SDF = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
-    
-    static {
-        /*
-        OPERATORS.put(AuditRecordData.FIELD_ADDITIONAL_DETAILS, TEXT_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_AUTHENTICATION_TOKEN, TEXT_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_CUSTOM_ID, TEXT_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_EVENTSTATUS, TYPE_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_EVENTTYPE, TEXT_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_MODULE, TEXT_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_NODEID, TEXT_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_SEARCHABLE_DETAIL1, TEXT_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_SEARCHABLE_DETAIL2, TEXT_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_SERVICE, TEXT_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_SEQUENCENUMBER, NUMBER_OPERATORS);
-        OPERATORS.put(AuditRecordData.FIELD_TIMESTAMP, NUMBER_OPERATORS);
-         */
-
-        /*
-        STATUS_VALUES = new ArrayList<String>();
-        for (EventStatus st : EventStatus.values()) {
-            STATUS_VALUES.add(st.name());
-        }
-        VALUES.put(AuditRecordData.FIELD_EVENTSTATUS, STATUS_VALUES);
-        
-        final long time = System.currentTimeMillis();
-        TIME_VALUES = new ArrayList<String>();
-        TIME_VALUES.add(SDF.format(new Date(time)));
-        TIME_VALUES.add(String.valueOf(time));
-        VALUES.put(AuditRecordData.FIELD_TIMESTAMP, TIME_VALUES);
-        */
-    }
     
     private boolean okPressed;
     private QueryColumn column;
@@ -290,6 +229,12 @@ private void columnComboboxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN
         
         if (col.getType() == QueryColumn.Type.TYPE) {
             valueCombobox.setModel(new DefaultComboBoxModel(col.getTypeValues().toArray()));
+        } else if (col.getType() == QueryColumn.Type.TIME) {
+            // prepopulate with time values
+            final long time = System.currentTimeMillis();
+            final String[] timeValues = {SDF.format(new Date(time)), String.valueOf(time)};
+                
+            valueCombobox.setModel(new DefaultComboBoxModel(timeValues));
         } else {
             valueCombobox.setModel(new DefaultComboBoxModel());
         }

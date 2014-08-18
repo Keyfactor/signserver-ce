8833c792a9e1a3fe9d7e63e20e3f90da4731210e
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-08-18 20:21:02 +0000

    DSS-470: Remove duplicated import.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4984 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-AdminCLI/src/main/java/org/signserver/admin/cli/defaultimpl/AbstractWSClientsCommand.java
+++ signserver/modules/SignServer-AdminCLI/src/main/java/org/signserver/admin/cli/defaultimpl/AbstractWSClientsCommand.java
@@ -22,7 +22,6 @@ import org.apache.commons.cli.GnuParser;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.ejbca.util.CertTools;
-import org.ejbca.util.CertTools;
 import org.signserver.admin.cli.defaultimpl.AbstractAdminCommand.ClientEntry;
 import org.signserver.cli.spi.CommandFailureException;
 import org.signserver.cli.spi.IllegalCommandArgumentsException;

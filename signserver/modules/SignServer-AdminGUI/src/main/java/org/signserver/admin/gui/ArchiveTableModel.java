df86ee1306679e05eaf46a2c38ee3a3daa101a97
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-08-18 13:35:52 +0000

    DSS-820: Implement table model for archive search result. First draft.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4978 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-AdminGUI/src/main/java/org/signserver/admin/gui/ArchiveTableModel.java
+++ signserver/modules/SignServer-AdminGUI/src/main/java/org/signserver/admin/gui/ArchiveTableModel.java
@@ -12,9 +12,14 @@
  *************************************************************************/
 package org.signserver.admin.gui;
 
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
 import org.signserver.admin.gui.adminws.gen.ArchiveEntry;
+import org.signserver.common.ArchiveDataVO;
+import org.signserver.server.archive.Archivable;
 
 /**
  * Table model for archive search result.
@@ -24,20 +29,64 @@ import org.signserver.admin.gui.adminws.gen.ArchiveEntry;
  */
 public class ArchiveTableModel extends AbstractTableModel {
 
+    private static final String[] COLUMNS = new String [] {
+                "Archive ID", "Time", "Type", "Signer ID", "Admin Serial Number", "Admin Issuer", "IP Address"
+            };
+    
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ssZ");
+    
+    
+    private List<ArchiveEntry> entries = Collections.emptyList();
+    
     @Override
     public int getRowCount() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return entries.size();
     }
 
     @Override
     public int getColumnCount() {
-                throw new UnsupportedOperationException("Not supported yet.");
-
+        return COLUMNS.length;
     }
 
     @Override
-    public Object getValueAt(int arg0, int arg1) {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        final Object result;
+        final ArchiveEntry entry = entries.get(rowIndex);
+        
+        switch(columnIndex) {
+            case 1:
+                result = entry.getArchiveId();
+                break;
+            case 2:
+                result = sdf.format(entry.getTime());
+                break;
+            case 3:
+                final int type = entry.getType();
+                result = type == ArchiveDataVO.TYPE_REQUEST ? 
+                        Archivable.TYPE_REQUEST : Archivable.TYPE_RESPONSE;
+                break;
+            case 4:
+                result = entry.getSignerId();
+                break;
+            case 5:
+                result = entry.getRequestCertSerialNumber();
+                break;
+            case 6:
+                result = entry.getRequestIssuerDN();
+                break;
+            case 7:
+                result = entry.getRequestIP();
+                break;
+            default:
+                result = "";
+        }
+        
+        return result;
+    }
+    
+    @Override
+    public String getColumnName(final int columnIndex) {
+        return COLUMNS[columnIndex];
     }
     
 }

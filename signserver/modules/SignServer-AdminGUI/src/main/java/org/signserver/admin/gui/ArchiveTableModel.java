47bf32eee151a3365c2cae849ea8591f18ccd9a2
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-08-18 12:34:24 +0000

    DSS-820: Added stub implementation of table model for archive search results.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4977 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-AdminGUI/src/main/java/org/signserver/admin/gui/ArchiveTableModel.java
@@ -0,0 +1,43 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.admin.gui;
+
+import javax.swing.table.AbstractTableModel;
+
+import org.signserver.admin.gui.adminws.gen.ArchiveEntry;
+
+/**
+ * Table model for archive search result.
+ * 
+ * @author Marcus Lundblad
+ * @version $Id$
+ */
+public class ArchiveTableModel extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getColumnCount() {
+                throw new UnsupportedOperationException("Not supported yet.");
+
+    }
+
+    @Override
+    public Object getValueAt(int arg0, int arg1) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}

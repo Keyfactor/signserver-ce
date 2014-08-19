800f4e4e325a5fef6fe4617dba36b9333594b094
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-08-19 06:54:51 +0000

    DSS-820: Added copyright header and class javadoc.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@4987 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-AdminGUI/src/main/java/org/signserver/admin/gui/ArchiveColumn.java
+++ signserver/modules/SignServer-AdminGUI/src/main/java/org/signserver/admin/gui/ArchiveColumn.java
@@ -1,7 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.signserver.admin.gui;
 
 import java.util.Arrays;
@@ -10,8 +18,10 @@ import org.signserver.common.ArchiveMetadata;
 import org.signserver.server.archive.Archivable;
 
 /**
- *
- * @author marcus
+ * Representation of an archive search result column column with name and description.
+ * 
+ * @author Marcus Lundblad
+ * @version $Id$
  */
 public enum ArchiveColumn implements QueryColumn {
 

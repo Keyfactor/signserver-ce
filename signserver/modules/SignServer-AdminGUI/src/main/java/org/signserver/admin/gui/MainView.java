3d237e897c9a270b6f3df26c749841c9df12301a
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-08-22 10:26:09 +0000

    DSS-820: Remove unused imports.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5017 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-AdminGUI/src/main/java/org/signserver/admin/gui/MainView.java
+++ signserver/modules/SignServer-AdminGUI/src/main/java/org/signserver/admin/gui/MainView.java
@@ -18,8 +18,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;

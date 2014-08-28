cb833be6f8f5e8351214e0553bbff407c41693a9
Markus Kilås <markus.kilas@primekey.com>
2014-08-28 13:52:10 +0000

    DSS-809: Removed unneeded method call as suggested in review
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5065 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/CryptoTokenHelper.java
+++ signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/CryptoTokenHelper.java
@@ -341,7 +341,6 @@ public class CryptoTokenHelper {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA1", "BC");
             final String res = new String(Hex.encode(md.digest(key.getEncoded())));
-            md.reset();
             return res;
         } catch (NoSuchProviderException ex) {
             final String message = "No such provider trying to hash public key";

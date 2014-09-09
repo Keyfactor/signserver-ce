07e123d1e40c740bb6e974f986ce4924dab2bc78
Markus Kilås <markus.kilas@primekey.com>
2014-09-09 13:57:13 +0000

    DSS-855: Adding sample index for ArchiveData table which speeds up querying when there are a larger number of rows
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5120 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/doc/sql-scripts/create-index-signserver.sql
+++ signserver/doc/sql-scripts/create-index-signserver.sql
@@ -2,6 +2,11 @@
 
 -- Selecting log entries when verifying/exporting IntegrityProtectedDevice logs:
 CREATE UNIQUE INDEX auditrecorddata_idx2 ON AuditRecordData (nodeId,sequenceNumber);
+
 -- Selecting log entries from IntegrityProtectedDevice logs in the AdminGUI is usually
--- done using time constraints.
+-- ordered by time stamp.
 CREATE INDEX auditrecorddata_idx3 ON AuditRecordData (timeStamp);
+
+-- Selecting archivables from the archive in the AdminGUI is usually
+-- ordered by time stamp.
+CREATE INDEX archivedata_idx3 ON ArchiveData (time);

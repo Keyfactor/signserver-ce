d73cfba4c2953ee3ed41e1f59a320a42b3471b09
Marcus Lundblad <marcus.lundblad@primekey.com>
2014-09-11 06:43:30 +0000

    DSS-821: Remove unused import.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5150 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/main/java/org/signserver/server/timedservices/hsmkeepalive/TestKeyDebugCryptoToken.java
+++ signserver/modules/SignServer-Server/src/main/java/org/signserver/server/timedservices/hsmkeepalive/TestKeyDebugCryptoToken.java
@@ -21,7 +21,6 @@ import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Properties;
 import org.apache.log4j.Logger;

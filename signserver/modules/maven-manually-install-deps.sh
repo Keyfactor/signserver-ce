9f07ccf9fa569d69b63ac0358c3b0210139c9baa
Markus Kilås <markus.kilas@primekey.com>
2014-11-04 15:33:58 +0000

    DSS-797: Scripts to enable building with Maven
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5367 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/maven-manually-install-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+### Install CESeCore jars etc not in an repository and without POM
+
+mvn install:install-file -Dfile=lib/ext/cesecore-common-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-common -Dversion=6.1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/ext/cesecore-entity-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-entity -Dversion=6.1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/ext/cesecore-interfaces-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-interfaces -Dversion=6.1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/ext/cesecore-ejb-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-ejb -Dversion=6.1.1 -Dpackaging=ejb
+mvn install:install-file -Dfile=lib/ext/cert-cvc-1.2.11.jar -DgroupId=org.ejbca.cvc -DartifactId=cert-cvc -Dversion=1.2.11 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/ext/xades4j/xades4j-1.3.0-signserver.jar -DgroupId=org.signserver.xades4j -DartifactId=xades4j -Dversion=1.3.0-signserver -Dpackaging=jar

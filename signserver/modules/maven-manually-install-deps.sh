b2609ddc6329febb15af1ed90023692f04fbe1bb
Markus Kilås <markus.kilas@primekey.com>
2014-11-05 12:53:42 +0000

    DSS-797:  Fixed path to xades4j jar
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5376 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/maven-manually-install-deps.sh
+++ signserver/modules/maven-manually-install-deps.sh
@@ -6,7 +6,7 @@ mvn install:install-file -Dfile=lib/ext/cesecore-entity-6.1.1.jar -DgroupId=org.
 mvn install:install-file -Dfile=lib/ext/cesecore-interfaces-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-interfaces -Dversion=6.1.1 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/ext/cesecore-ejb-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-ejb -Dversion=6.1.1 -Dpackaging=ejb
 mvn install:install-file -Dfile=lib/ext/cert-cvc-1.2.11.jar -DgroupId=org.ejbca.cvc -DartifactId=cert-cvc -Dversion=1.2.11 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/ext/xades4j/xades4j-1.3.0-signserver.jar -DgroupId=org.signserver.xades4j -DartifactId=xades4j -Dversion=1.3.0-signserver -Dpackaging=jar
+mvn install:install-file -Dfile=lib/ext/xades4j-1.3.0-signserver.jar -DgroupId=org.signserver.xades4j -DartifactId=xades4j -Dversion=1.3.0-signserver -Dpackaging=jar
 
 
 # Install the DeployTools artifacts from JARS

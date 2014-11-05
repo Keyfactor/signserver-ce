8b9ebcd4adf5834a7ac75365bc55e29f9af6cdd3
Markus Kilås <markus.kilas@primekey.com>
2014-11-05 09:35:12 +0000

    DSS-797: Upgraded DeployTools
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5370 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/maven-manually-install-deps.sh
+++ signserver/modules/maven-manually-install-deps.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ### Install CESeCore jars etc not in an repository and without POM
 
-mvn install:install-file -Dfile=lib/ext/DeployTools-Common-1.1.jar -DgroupId=org.signserver.deploytools -DartifactId=DeployTools-Common -Dversion=1.1 -Dpackaging=jar
-mvn install:install-file -Dfile=lib/ext/DeployTools-Maven-1.1.jar -DgroupId=org.signserver.deploytools -DartifactId=DeployTools-Maven -Dversion=1.1 -Dpackaging=jar
+mvn install:install-file -Dfile=lib/ext/DeployTools-Common-1.1.1-SNAPSHOT.jar -DgroupId=org.signserver.deploytools -DartifactId=DeployTools-Common -Dversion=1.1.1-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -Dfile=lib/ext/DeployTools-Maven-1.1.1-SNAPSHOT.jar -DgroupId=org.signserver.deploytools -DartifactId=DeployTools-Maven -Dversion=1.1.1-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=lib/ext/cesecore-common-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-common -Dversion=6.1.1 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/ext/cesecore-entity-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-entity -Dversion=6.1.1 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/ext/cesecore-interfaces-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-interfaces -Dversion=6.1.1 -Dpackaging=jar

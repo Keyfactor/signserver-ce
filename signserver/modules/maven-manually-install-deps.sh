389af6d3fdfce7480fcaff41fe6761e382e95c9e
Markus Kilås <markus.kilas@primekey.com>
2014-11-10 08:01:49 +0000

    DSS-797:  Manually install quartz which is missing pom
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5387 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/maven-manually-install-deps.sh
+++ signserver/modules/maven-manually-install-deps.sh
@@ -7,7 +7,7 @@ mvn install:install-file -Dfile=lib/ext/cesecore-interfaces-6.1.1.jar -DgroupId=
 mvn install:install-file -Dfile=lib/ext/cesecore-ejb-6.1.1.jar -DgroupId=org.cesecore -DartifactId=cesecore-ejb -Dversion=6.1.1 -Dpackaging=ejb
 mvn install:install-file -Dfile=lib/ext/cert-cvc-1.2.11.jar -DgroupId=org.ejbca.cvc -DartifactId=cert-cvc -Dversion=1.2.11 -Dpackaging=jar
 mvn install:install-file -Dfile=lib/ext/xades4j-1.3.0-signserver.jar -DgroupId=org.signserver.xades4j -DartifactId=xades4j -Dversion=1.3.0-signserver -Dpackaging=jar
-
+mvn install:install-file -Dfile=lib/ext/quartz-1.6.0.jar -DgroupId=opensymphony -DartifactId=quartz -Dversion=1.6.0 -Dpackaging=jar
 
 # Install the DeployTools artifacts from JARS
 DPL_VERSION=1.1.1

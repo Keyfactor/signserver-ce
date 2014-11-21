268f95f97f507e4de8c2821d92048f888a008c46
Markus Kilås <markus.kilas@primekey.com>
2014-11-21 10:10:55 +0000

    DSS-885: Script for updating the list of dependencies. Updated list of dependencies.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/branches/Branch_3_6@5452 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/lib/update-jars-list.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Updates jars-list.txt with each jar file, its hash (external jars only),
+# and which project it comes from (filled in manually).
+
+# External dependencies
+EXT=`find ./ext -name "*.jar"`
+
+# Things we build our self
+INT=`find . -name "*-Lib-*.jar"`
+
+# Previous list of dependencies
+PREVFILE="../tmp/jars-list.txt"
+mkdir -p ../tmp/
+cp jars-list.txt ../tmp/
+echo "" > jars-list.txt
+
+for f in $EXT
+do
+    # Checksum (and file name)
+    SUM=`sha256sum $f`
+
+    # Fill in project name from previous file
+    PROJECT=`grep $f ${PREVFILE} | awk -F\; '{ print $2 }' | sed -e 's/^[ \t]*//'`
+
+    printf "%-120s  %s\n" "${SUM};" "${PROJECT}" >> jar-files.txt
+done
+
+for f in $INT
+do
+    # Only file name for JARs we build
+    SUM=`printf "%64s  %s" " " "$f"`
+
+    # Fill in project name from previous file
+    PROJECT=`grep $f ${PREVFILE} | awk -F\; '{ print $2 }' | sed -e 's/^[ \t]*//'`
+
+    printf "%-120s  %s\n" "${SUM}" "${PROJECT}" >> jar-files.txt
+done
---
+++ signserver/lib/update-projects-list.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Updates projects-list.txt with each project used based on the projects
+# referenced in jars-list.txt. Information about license etc are filled in
+# manually.
+
+# Each project name
+PROJECTS=`cat jars-list.txt | awk -F\; '{ print $2}' | sed -e 's/^[ \t]*//'| sort | uniq`
+
+# Previous list of dependencies
+PREVFILE="../tmp/projects-list.txt"
+mkdir -p ../tmp/
+cp projects-list.txt ../tmp/
+echo "" > projects-list.txt
+
+OURIFS=$(echo -en "\n\b")
+OLDIFS=$IFS
+IFS=$OURIFS
+for p in ${PROJECTS}
+do
+    IFS=$OLDIFS
+    INFO=`grep "$p" ${PREVFILE}`
+    LICENSE=`echo ${INFO} | awk -F\; '{ print $2 }'`
+    OTHER1=`echo ${INFO} | awk -F\; '{ print $3 }'`
+    OTHER2=`echo ${INFO} | awk -F\; '{ print $4 }'`
+
+    printf "%-28s %-48s %s %s\n" "$p;" "${LICENSE};" "${OTHER1};" "${OTHER2};" >> projects.txt
+    IFS=$OURIFS
+done
+IFS=$OLDIFS

e65743c427c44cfdebe730403a0900e3b33435c6
Marcus Lundblad <marcus.lundblad@primekey.com>
2015-02-02 08:39:42 +0000

    DSS-911: Fix the authCode argument working. Added copyright header.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5666 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-AdminCLI/src/main/java/org/signserver/admin/cli/defaultimpl/ImportCertificateChainCommand.java
+++ signserver/modules/SignServer-AdminCLI/src/main/java/org/signserver/admin/cli/defaultimpl/ImportCertificateChainCommand.java
@@ -1,7 +1,15 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.signserver.admin.cli.defaultimpl;
 
 import java.security.cert.Certificate;
@@ -17,7 +25,8 @@ import org.signserver.common.OperationUnsupportedException;
 import org.signserver.common.WorkerStatus;
 
 /**
- *
+ * Command to import a certificate chain to a crypto token.
+ * 
  * @author Marcus Lundblad
  * @version $Id$
  */
@@ -49,7 +58,7 @@ public class ImportCertificateChainCommand extends AbstractAdminCommand {
             final String alias = args[2];
             String authCode = null;
 
-            if (authCode != null) {
+            if (args.length > 3) {
                 authCode = args[3];
             }
             

64b7b5549633fe7a8d669b90055b6e2561d2cb0d
Markus Kilås <markus.kilas@primekey.com>
2015-03-13 14:40:27 +0000

    DSS-937: Adding default implementation for providing worker status
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@5869 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/main/java/org/signserver/server/BaseWorker.java
+++ signserver/modules/SignServer-Server/src/main/java/org/signserver/server/BaseWorker.java
@@ -13,12 +13,20 @@
 package org.signserver.server;
 
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Properties;
 import javax.naming.NamingException;
 import javax.persistence.EntityManager;
 import org.apache.log4j.Logger;
+import org.signserver.common.AuthorizedClient;
+import org.signserver.common.ProcessableConfig;
 import org.signserver.common.ServiceLocator;
+import org.signserver.common.SignServerConstants;
+import org.signserver.common.StaticWorkerStatus;
 import org.signserver.common.WorkerConfig;
+import org.signserver.common.WorkerStatus;
+import org.signserver.common.WorkerStatusInfo;
 import org.signserver.ejb.interfaces.IGlobalConfigurationSession;
 
 /**
@@ -121,5 +129,56 @@ public abstract class BaseWorker implements IWorker {
     protected List<String> getFatalErrors() {
         return Collections.emptyList();
     }
-    
+
+    /**
+     * Default status information implementation for Workers.
+     * This method can be overridden to provide a more customized status page.
+     * @param additionalFatalErrors discovered at a different level
+     * @param services available to query status
+     * @return the status information
+     */
+    @Override
+    public WorkerStatus getStatus(List<String> additionalFatalErrors, final IServices services) {
+        final List<String> fatalErrors = new LinkedList<String>(additionalFatalErrors);
+        fatalErrors.addAll(getFatalErrors());
+
+        final List<WorkerStatusInfo.Entry> briefEntries = new LinkedList<WorkerStatusInfo.Entry>();
+        final List<WorkerStatusInfo.Entry> completeEntries = new LinkedList<WorkerStatusInfo.Entry>();
+
+        // Worker status
+        final boolean active = fatalErrors.isEmpty();
+        briefEntries.add(new WorkerStatusInfo.Entry("Worker status", active ? "Active" : "Offline"));
+
+        // Disabled or not
+        if (Boolean.TRUE.toString().equalsIgnoreCase(config.getProperty(SignServerConstants.DISABLED))) {
+            briefEntries.add(new WorkerStatusInfo.Entry("", "Worker is disabled"));
+        }
+
+        // Worker Properties
+        final StringBuilder configValue = new StringBuilder();
+        Properties properties = config.getProperties();
+        for (String key : properties.stringPropertyNames()) {
+            configValue.append(key).append("=").append(properties.getProperty(key)).append("\n\n");
+        }
+        completeEntries.add(new WorkerStatusInfo.Entry("Worker properties", configValue.toString()));
+
+        // Authorized Clients
+        final StringBuilder clientsValue = new StringBuilder();
+        for (AuthorizedClient client : new ProcessableConfig(config).getAuthorizedClients()) {
+            clientsValue.append(client.getCertSN()).append(", ").append(properties.getProperty(client.getIssuerDN())).append("\n");
+        }
+        completeEntries.add(new WorkerStatusInfo.Entry("Authorized clients (serial number, issuer DN)", clientsValue.toString()));
+
+        // Return everything
+        return new StaticWorkerStatus(new WorkerStatusInfo(
+                workerId, 
+                config.getProperty("NAME"), 
+                "Worker", 
+                active ? WorkerStatus.STATUS_ACTIVE : WorkerStatus.STATUS_OFFLINE, 
+                briefEntries, 
+                fatalErrors, 
+                completeEntries, 
+                config));
+    }
+
 }

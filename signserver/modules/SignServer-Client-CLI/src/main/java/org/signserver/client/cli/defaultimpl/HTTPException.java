def733de68c67b91e41d59958937a0e5c44704e1
Markus Kilås <markus.kilas@primekey.com>
2015-05-11 08:28:17 +0000

    DSS-969: Fixed spelling. Added missing source code header.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/branches/Branch_3_6@6018 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Client-CLI/src/main/java/org/signserver/client/cli/defaultimpl/HTTPException.java
+++ signserver/modules/SignServer-Client-CLI/src/main/java/org/signserver/client/cli/defaultimpl/HTTPException.java
@@ -1,16 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.signserver.client.cli.defaultimpl;
 
 import java.io.IOException;
 import java.net.URL;
 
 /**
+ * Exception indicating an error at the HTTP level.
+ * Contains more information about the failure.
  *
- * @author user
+ * @author Markus Kilås
+ * @version $Id$
  */
 public class HTTPException extends IOException {
 
@@ -18,8 +28,7 @@ public class HTTPException extends IOException {
     private final int responseCode;
     private final String responseMessage;
     private final byte[] responseBody;
-    
-    
+
     public HTTPException(URL url, int responseCode, String responseMessage, byte[] responseBody) {
         super("Server returned HTTP response code: " + responseCode + " for URL: " + url + ": " + responseMessage);
         this.url = url;

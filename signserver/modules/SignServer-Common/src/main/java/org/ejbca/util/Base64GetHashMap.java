0a61c3e6243deac37a4e0210aaede05adf37fbf7
Marcus Lundblad <marcus.lundblad@primekey.com>
2015-10-06 07:44:44 +0000

    DSS-873: Add wrapper classes for the old Base64Get/PushHashMap.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@6419 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Common/src/main/java/org/ejbca/util/Base64GetHashMap.java
@@ -0,0 +1,23 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.ejbca.util;
+
+/**
+ * Wrapper to allow reading old configurations referring to serialized
+ * instances of Base64PutHashMap for the old EJBCA-util package.
+ * 
+ * @author Marcus Lundblad
+ * @version $Id$
+ */
+public class Base64GetHashMap extends org.cesecore.util.Base64GetHashMap {
+}

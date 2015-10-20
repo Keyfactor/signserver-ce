1a2bd4bf25fb5e97d871d7372310c7cd6ac4826b
Marcus Lundblad <marcus.lundblad@primekey.com>
2015-10-20 11:22:49 +0000

    DSS-960: Try running from ANT_HOME if set in the Windows command file.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@6523 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/bin/ant.cmd
+++ signserver/bin/ant.cmd
@@ -1,2 +1,6 @@
 @echo off
-ant -f build.xml -q %* | FindStr /v "Trying to override old definition of task http://www.netbeans.org/"
+if "%ANT_HOME%" == "" (
+  ant -f build.xml -q %* | FindStr /v "Trying to override old definition of task http://www.netbeans.org/"
+) else (
+  %ANT_HOME%\bin\ant -f build.xml -q %* | FindStr /v "Trying to override old definition of task http://www.netbeans.org/"
+)

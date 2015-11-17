1fe29ea4d2c51867da0b730de8559a01fc48b165
Marcus Lundblad <marcus.lundblad@primekey.com>
2015-11-17 14:21:31 +0000

    DSS-971: Remove unused imports.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@6733 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/PKCS11CryptoToken.java
+++ signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/PKCS11CryptoToken.java
@@ -18,7 +18,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.math.BigInteger;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;

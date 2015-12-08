0b69e93dd93e01728bae77a23ce4d010d2e10a98
Marcus Lundblad <marcus.lundblad@primekey.com>
2015-12-08 09:27:36 +0000

    DSS-971: Try getting public key from PKCS11CryptoToken by generating a CSR.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@6834 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-P11/src/test/java/org/signserver/server/cryptotokens/P11SignTest.java
+++ signserver/modules/SignServer-Test-P11/src/test/java/org/signserver/server/cryptotokens/P11SignTest.java
@@ -19,7 +19,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.security.InvalidKeyException;
 import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.PublicKey;
 import java.security.cert.Certificate;
 import java.security.interfaces.RSAPublicKey;
 import java.util.Arrays;
@@ -48,6 +51,7 @@ import org.bouncycastle.cms.SignerInformationStore;
 import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.pkcs.PKCS10CertificationRequest;
+import org.bouncycastle.pkcs.jcajce.JcaPKCS10CertificationRequest;
 import org.bouncycastle.tsp.TSPAlgorithms;
 import org.bouncycastle.tsp.TimeStampRequest;
 import org.bouncycastle.tsp.TimeStampRequestGenerator;
@@ -59,6 +63,7 @@ import org.signserver.common.CryptoTokenOfflineException;
 import org.signserver.common.GenericSignRequest;
 import org.signserver.common.GenericSignResponse;
 import org.signserver.common.GlobalConfiguration;
+import org.signserver.common.ICertReqData;
 import org.signserver.common.KeyTestResult;
 import org.signserver.common.PKCS10CertReqInfo;
 import org.signserver.common.RequestContext;
@@ -936,12 +941,16 @@ public class P11SignTest extends ModulesTestCase {
                 assertTrue("Testkey successful", testResult.isSuccess());
             }
             
-            // check the public exponent of the public key
-            final List<Certificate> signerCertificateChain =
-                    workerSession.getSignerCertificateChain(workerId,
-                                                            "keywithexponent");
-            final Certificate issuer = signerCertificateChain.get(0);
-            final RSAPublicKey pubKey = (RSAPublicKey) issuer.getPublicKey();
+            // Generate CSR, and check the public key's public exponent
+            final PKCS10CertReqInfo certReqInfo = new PKCS10CertReqInfo("SHA1WithRSA",
+                "CN=test01GenerateKey,C=SE", null);
+            Base64SignerCertReqData data = (Base64SignerCertReqData) workerSession
+                .getCertificateRequest(workerId, certReqInfo, false, "keywithexponent");
+            final byte[] reqBytes = data.getBase64CertReq();
+            final PKCS10CertificationRequest req
+                = new PKCS10CertificationRequest(Base64.decode(reqBytes));
+
+            final RSAPublicKey pubKey = (RSAPublicKey) getPublicKeyFromRequest(req);
             
             assertEquals("Returned public exponent", 5, pubKey.getPublicExponent());
         } finally {
@@ -953,6 +962,13 @@ public class P11SignTest extends ModulesTestCase {
             } catch (SignServerException ignored) {}
             removeWorker(workerId);
         }
+    }
+    
+    private PublicKey getPublicKeyFromRequest(final PKCS10CertificationRequest req)
+            throws InvalidKeyException, NoSuchAlgorithmException {
+        final JcaPKCS10CertificationRequest jcaPKCS10CertificationRequest =
+                new JcaPKCS10CertificationRequest(req);
+        return jcaPKCS10CertificationRequest.getPublicKey();
     }
     
     /**

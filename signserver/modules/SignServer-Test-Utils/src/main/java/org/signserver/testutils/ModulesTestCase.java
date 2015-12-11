e4f8c4340f34f9e727fa32ae7960d6a8807967d4
Markus Kilås <markus.kilas@primekey.com>
2015-12-11 12:38:45 +0000

    DSS-1097: Making sure the tests sets the required worker property
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@6885 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-Utils/src/main/java/org/signserver/testutils/ModulesTestCase.java
+++ signserver/modules/SignServer-Test-Utils/src/main/java/org/signserver/testutils/ModulesTestCase.java
@@ -457,6 +457,8 @@ public class ModulesTestCase extends TestCase {
     
     public void addTimeStampSigner(final int signerId, final String signerName, final boolean autoActivate) throws CertificateException, FileNotFoundException {
         addP12DummySigner("org.signserver.module.tsa.TimeStampSigner", signerId, signerName, new File(getSignServerHome(), KEYSTORE_TSSIGNER1_FILE), autoActivate ? KEYSTORE_PASSWORD : null, KEYSTORE_TSSIGNER1_ALIAS);
+        getWorkerSession().setWorkerProperty(signerId, "DEFAULTTSAPOLICYOID", "1.2.3");
+        getWorkerSession().reloadConfiguration(signerId);
     }
     
     public void addMSTimeStampSigner(final int signerId, final String signerName, final boolean autoActivate) throws CertificateException, FileNotFoundException {

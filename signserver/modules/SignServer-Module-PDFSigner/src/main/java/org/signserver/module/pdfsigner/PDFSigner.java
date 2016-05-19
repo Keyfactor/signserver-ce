fc64be523f51a5b82000bf5ea8114d45a6005406
Marcus Lundblad <marcus.lundblad@primekey.com>
2016-05-19 14:09:55 +0000

    DSS-1004: Fix refactoring.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@7363 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-PDFSigner/src/main/java/org/signserver/module/pdfsigner/PDFSigner.java
+++ signserver/modules/SignServer-Module-PDFSigner/src/main/java/org/signserver/module/pdfsigner/PDFSigner.java
@@ -338,13 +338,17 @@ public class PDFSigner extends BaseSigner {
             
             // The client can be charged for the request
             requestContext.setRequestFulfilledByWorker(true);
-        } catch (DocumentException | IOException e) {
+        } catch (DocumentException e) {
             throw new IllegalRequestException("Could not sign document: "
                     + e.getMessage(), e);
         } catch (BadPasswordException ex) {
             throw new IllegalRequestException("A valid password is required to sign the document: " + ex.getMessage(), ex);
         } catch (UnsupportedEncodingException ex) {
             throw new IllegalRequestException("The supplied password could not be read: " + ex.getMessage(), ex);
+        } catch (IOException e) {
+            // fallback for IOException
+            throw new IllegalRequestException("Could not sign document: "
+                    + e.getMessage(), e);
         } finally {
             releaseCryptoInstance(crypto, requestContext);
         }

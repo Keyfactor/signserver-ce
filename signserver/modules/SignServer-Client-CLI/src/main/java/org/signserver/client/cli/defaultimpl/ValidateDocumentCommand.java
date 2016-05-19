27beb7235f1ad13fea03e599cbdf6f5efb5f8900
Marcus Lundblad <marcus.lundblad@primekey.com>
2016-05-19 14:00:57 +0000

    DSS-1004: Fix refactoring.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@7362 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Client-CLI/src/main/java/org/signserver/client/cli/defaultimpl/ValidateDocumentCommand.java
+++ signserver/modules/SignServer-Client-CLI/src/main/java/org/signserver/client/cli/defaultimpl/ValidateDocumentCommand.java
@@ -352,7 +352,7 @@ public class ValidateDocumentCommand extends AbstractCommand {
         } catch (FileNotFoundException ex) {
             LOG.error(MessageFormat.format(TEXTS.getString("FILE_NOT_FOUND:"),
                     ex.getLocalizedMessage()));
-        } catch (IllegalRequestException | CryptoTokenOfflineException | SignServerException | IOException ex) {
+        } catch (IllegalRequestException | CryptoTokenOfflineException | SignServerException ex) {
             LOG.error(ex);
         } catch (SOAPFaultException ex) {
             if (ex.getCause() instanceof AuthorizationRequiredException) {
@@ -364,6 +364,9 @@ public class ValidateDocumentCommand extends AbstractCommand {
         } catch (HTTPException ex) {
             LOG.error("Failure: HTTP error: " + ex.getResponseCode() + ": " +
                       ex.getResponseMessage());
+        } catch (IOException ex) {
+            // fallback to IOException...
+            LOG.error(ex);
         } finally {
             if (fin != null) {
                 try {

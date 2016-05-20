c0adb1fab91ee11217c6192038780d68d3605725
Marcus Lundblad <marcus.lundblad@primekey.com>
2016-05-20 06:53:05 +0000

    DSS-1004: Use JDK7 features
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@7366 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Client-CLI/src/main/java/org/signserver/client/cli/performance/PDFPerformanceCommand.java
+++ signserver/modules/SignServer-Client-CLI/src/main/java/org/signserver/client/cli/performance/PDFPerformanceCommand.java
@@ -96,18 +96,18 @@ public class PDFPerformanceCommand extends AbstractCommand {
 				continue;
 			}
 			System.out.println("Processing " + file.getName() + "\n");
-			try {
-				BufferedReader br = new BufferedReader(new FileReader(file));
+			try (BufferedReader br =
+                                new BufferedReader(new FileReader(file))) {
 				String str = br.readLine();
 				if (str == null) {
 					continue;
 				}
 				String[] columns = str.split(";");
-				ArrayList<ArrayList<Long>> roundedData = new ArrayList<ArrayList<Long>>();
-				ArrayList<Long> uniqueTimes = new ArrayList<Long>();
+				ArrayList<ArrayList<Long>> roundedData = new ArrayList<>();
+				ArrayList<Long> uniqueTimes = new ArrayList<>();
 				long startTime = 0;
 				while ((str = br.readLine()) != null) {
-					ArrayList<Long> row = new ArrayList<Long>();
+					ArrayList<Long> row = new ArrayList<>();
 					String[] values = str.split(";");
 					if (startTime == 0) {
 						startTime = Long.parseLong(values[(values.length-1)]);
@@ -124,9 +124,8 @@ public class PDFPerformanceCommand extends AbstractCommand {
 						row.add(currentTime);
 						roundedData.add(row);
 					}
-				}
-				br.close();
-				ArrayList<String> explanationRow = new ArrayList<String>();
+                                }
+				ArrayList<String> explanationRow = new ArrayList<>();
 				explanationRow.add(columns[columns.length-1]);	// Add time first
 				explanationRow.add("Invocations per "  + timeInterval/1000 + " seconds");	// and then the total in the time interval
 				explanationRow.add("Invocations per second");	// and then the total in the time interval
@@ -136,11 +135,11 @@ public class PDFPerformanceCommand extends AbstractCommand {
 					explanationRow.add(columns[i] + " per second");
 				}
 				// Loop trough unique times and sum the other column, calculate average and time-derivate
-				ArrayList<ArrayList<Double>> processedData = new ArrayList<ArrayList<Double>>();
+				ArrayList<ArrayList<Double>> processedData = new ArrayList<>();
 				List<Long> uniqueTimesSubset = uniqueTimes.subList(1, uniqueTimes.size() - 1);	// Ignore first and last interval
 				for (Long currentTime : uniqueTimesSubset) {
 					long currentFound = 0;
-					ArrayList<Double> sums = new ArrayList<Double>();
+					ArrayList<Double> sums = new ArrayList<>();
 					for (ArrayList<Long> point : roundedData) {
 						if (point.get(point.size()-1).longValue() == currentTime.longValue()) {
 							currentFound++;
@@ -183,13 +182,9 @@ public class PDFPerformanceCommand extends AbstractCommand {
 					PerformanceTestTask performanceTestTask = (PerformanceTestTask) Class.forName(moduleName).newInstance();
 					performanceTestTask.createDiagrams(file.getName(), statisticsDirectory, explanationRow, processedData);
 				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			} catch (InstantiationException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			} catch (ClassNotFoundException e) {
+			} catch (IOException | InstantiationException |
+                                 IllegalAccessException |
+                                 ClassNotFoundException e) {
 				e.printStackTrace();
 			}
 		}
@@ -221,10 +216,10 @@ public class PDFPerformanceCommand extends AbstractCommand {
 			"Threads: " + threads + "\n" +
 			"Base URL: " + baseURLString + "\n\n";
 		System.out.println(info);
-		for (int i=0; i<testModules.length; i++) {
-			System.out.println("Starting test " + testModules[i]);
-			runTest(testModules[i], runTime, threads);
-		}
+                for (String testModule : testModules) {
+                    System.out.println("Starting test " + testModule);
+                    runTest(testModule, runTime, threads);
+                }
 	}
 
 	/**
@@ -232,14 +227,15 @@ public class PDFPerformanceCommand extends AbstractCommand {
 	 */
 	private long runTest(String className, long timeToRun, int numberOfThreads) {
 		totalInvocations = 0;
-		ArrayList<Thread> threads = new ArrayList<Thread>();
+		ArrayList<Thread> threads = new ArrayList<>();
 		Object setupData = null;
 		for (int i = 0; i<numberOfThreads; i++) {
 			PerformanceTestTask performanceTestTask = null;
 			try {
 				Class<?> implClass = Class.forName(className);
 				performanceTestTask = (PerformanceTestTask) implClass.newInstance();
-			} catch (Exception e) {
+			} catch (ClassNotFoundException | InstantiationException |
+                                 IllegalAccessException e) {
 				e.printStackTrace();
 				return 0;
 			}
@@ -300,6 +296,7 @@ public class PDFPerformanceCommand extends AbstractCommand {
 
 	/** Allow pattern "*.csv".  */
 	private class CSVFilter implements FilenameFilter {
+                @Override
 		public boolean accept(File dir, String name) {
 			return (name.toLowerCase().endsWith(".csv"));
 		}

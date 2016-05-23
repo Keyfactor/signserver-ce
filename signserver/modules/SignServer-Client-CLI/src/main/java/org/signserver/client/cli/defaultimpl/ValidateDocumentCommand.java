b328ef9b271b07183b817ea339050d6c64cff338
Marcus Lundblad <marcus.lundblad@primekey.com>
2016-05-23 06:58:45 +0000

    DSS-1004: Fix compilation.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@7377 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Client-CLI/src/main/java/org/signserver/client/cli/defaultimpl/ValidateDocumentCommand.java
+++ signserver/modules/SignServer-Client-CLI/src/main/java/org/signserver/client/cli/defaultimpl/ValidateDocumentCommand.java
@@ -352,7 +352,7 @@ public class ValidateDocumentCommand extends AbstractCommand {
         } catch (FileNotFoundException ex) {
             LOG.error(MessageFormat.format(TEXTS.getString("FILE_NOT_FOUND:"),
                     ex.getLocalizedMessage()));
-        } catch (IllegalRequestException | CryptoTokenOfflineException | SignServerException | IOException ex) {
+        } catch (IllegalRequestException | CryptoTokenOfflineException | SignServerException ex) {
             LOG.error(ex);
         } catch (SOAPFaultException ex) {
             if (ex.getCause() instanceof AuthorizationRequiredException) {
@@ -361,12 +361,13 @@ public class ValidateDocumentCommand extends AbstractCommand {
                 LOG.error("Authorization required: " + authEx.getMessage());
             }
             LOG.error(ex);
-        }catch (HTTPException ex) {
+        } catch (HTTPException ex) {
             LOG.error("Failure: HTTP error: " + ex.getResponseCode() + ": " +
                       ex.getResponseMessage());
-        }
-        // fallback to IOException...
-         finally {
+        } catch (IOException ex) {
+            // fallback to IOException...
+            LOG.error(ex);
+        } finally {
             if (fin != null) {
                 try {
                     fin.close();

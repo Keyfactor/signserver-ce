2e83f97e8382ee8bfcb9a71470e14235f7f64fbb
Marcus Lundblad <marcus.lundblad@primekey.com>
2016-05-27 10:15:59 +0000

    DSS-942: Add more unit tests checking overriding getFatalErrors() etc.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@7399 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/test/java/org/signserver/server/log/BaseWorkerLoggerUnitTest.java
+++ signserver/modules/SignServer-Server/src/test/java/org/signserver/server/log/BaseWorkerLoggerUnitTest.java
@@ -12,6 +12,8 @@
  *************************************************************************/
 package org.signserver.server.log;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import junit.framework.TestCase;
 import org.junit.Test;
@@ -31,9 +33,10 @@ public class BaseWorkerLoggerUnitTest extends TestCase {
     /**
      * Test that no fatal error is included by default.
      * 
+     * @throws Exception
      */
     @Test
-    public void testNoErrors() {
+    public void testNoErrors() throws Exception {
         final BaseWorkerLogger workerLogger = new BaseWorkerLogger() {
             @Override
             public void init(int workerId, WorkerConfig config, SignServerContext context) {
@@ -47,5 +50,97 @@ public class BaseWorkerLoggerUnitTest extends TestCase {
         assertEquals("Should contain no errors",
                      0, workerLogger.getFatalErrors().size());
         assertFalse("Should not report errors", workerLogger.hasErrors());
+    }
+    
+    /**
+     * Test that adding an error using the base addFatalError method works.
+     * 
+     * @throws Exception 
+     */
+    @Test
+    public void testOneError() throws Exception {
+        final BaseWorkerLogger workerLogger = new BaseWorkerLogger() {
+            @Override
+            public void init(int workerId, WorkerConfig config, SignServerContext context) {
+                addFatalError("An error");
+            }
+
+            @Override
+            public void log(AdminInfo adminInfo, Map<String, String> fields, RequestContext requestContext) throws WorkerLoggerException {
+            }            
+        };
+        
+        workerLogger.init(0, null, null);
+        
+        assertEquals("Should contain one error",
+                     1, workerLogger.getFatalErrors().size());
+        assertTrue("Should contain the error",
+                   workerLogger.getFatalErrors().contains("An error"));
+    }
+    
+    /**
+     * Test that overriding the getFatalErrors method works.
+     * 
+     * @throws Exception 
+     */
+    @Test
+    public void testOverrideGetFatalErrors() throws Exception {
+        final BaseWorkerLogger workerLogger = new BaseWorkerLogger() {
+            @Override
+            public void init(int workerId, WorkerConfig config, SignServerContext context) {
+            }
+
+            @Override
+            public void log(AdminInfo adminInfo, Map<String, String> fields, RequestContext requestContext) throws WorkerLoggerException {
+            }
+            
+            @Override
+            public List<String> getFatalErrors() {
+                return Arrays.asList("An overridden error");
+            }
+        };
+        
+        assertEquals("Should contain one error",
+                     1, workerLogger.getFatalErrors().size());
+        assertTrue("Should contain the error",
+                   workerLogger.getFatalErrors().contains("An overridden error"));
+    }
+    
+    /**
+     * Test that overriding the getFatalErrors method and chaning up to super's
+     * implementation retains the ability to add errors at the base level using
+     * addFatalError.
+     * 
+     * @throws Exception 
+     */
+    @Test
+    public void testOverrideGetFatalErrorAndUseSuper() throws Exception {
+        final BaseWorkerLogger workerLogger = new BaseWorkerLogger() {
+            @Override
+            public void init(int workerId, WorkerConfig config, SignServerContext context) {
+                addFatalError("Base error");
+            }
+
+            @Override
+            public void log(AdminInfo adminInfo, Map<String, String> fields, RequestContext requestContext) throws WorkerLoggerException {
+            }
+            
+            @Override
+            public List<String> getFatalErrors() {
+                List<String> errors = super.getFatalErrors();
+                
+                errors.add("Overridden error");
+                return errors; 
+            }
+        };
+        
+        workerLogger.init(0, null, null);
+
+        assertEquals("Should contain two errors",
+                     2, workerLogger.getFatalErrors().size());
+        assertTrue("Should contain the base error",
+                   workerLogger.getFatalErrors().contains("Base error"));
+        assertTrue("Should contain the overridden error",
+                   workerLogger.getFatalErrors().contains("Overridden error"));
     }
 }

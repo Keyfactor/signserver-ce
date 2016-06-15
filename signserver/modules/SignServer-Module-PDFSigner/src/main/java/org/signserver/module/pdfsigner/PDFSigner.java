87fff744240bcc57ebc5275a71dc7d04aa955bab
Marcus Lundblad <marcus.lundblad@primekey.com>
2016-06-15 08:23:07 +0000

    DSS-1166: Fix typos.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@7488 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-PDFSigner/src/main/java/org/signserver/module/pdfsigner/PDFSigner.java
+++ signserver/modules/SignServer-Module-PDFSigner/src/main/java/org/signserver/module/pdfsigner/PDFSigner.java
@@ -267,14 +267,14 @@ public class PDFSigner extends BaseSigner {
         // with a byte[].
         if (!(signRequest instanceof GenericSignRequest)) {
             throw new IllegalRequestException(
-                    "Received request wasn't a expected GenericSignRequest.");
+                    "Received request wasn't an expected GenericSignRequest.");
         }
         
         final ISignRequest sReq = (ISignRequest) signRequest;
         
         if (!(sReq.getRequestData() instanceof byte[])) {
             throw new IllegalRequestException(
-                    "Received request data wasn't a expected byte[].");
+                    "Received request data wasn't an expected byte[].");
         }
 
         // Log values

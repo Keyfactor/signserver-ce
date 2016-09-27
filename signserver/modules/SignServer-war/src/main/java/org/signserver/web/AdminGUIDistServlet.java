c2b0026a270c480d13021a26c0f3d0e1c7834fe4
Marcus Lundblad <marcus.lundblad@primekey.com>
2016-09-27 07:05:15 +0000

    DSS-1228: Refactor out common ZIP download functionallity to an abstract base class.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@7738 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-war/src/main/java/org/signserver/web/AdminGUIDistServlet.java
+++ signserver/modules/SignServer-war/src/main/java/org/signserver/web/AdminGUIDistServlet.java
@@ -31,80 +31,12 @@ import org.apache.commons.io.IOUtils;
  * @author Markus Kilås
  * @version $Id$
  */
-public class AdminGUIDistServlet extends HttpServlet {
+public class AdminGUIDistServlet extends AbstractDistServlet {
 
     private final SettingsBean settings = new SettingsBean();
-    
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
 
-        final File file = settings.getAdminGUIDistFile();
-        if (file == null || !file.exists()) {
-            throw new FileNotFoundException("AdminGUI binary not available");
-        }
-        
-        response.setContentType("application/zip");
-        OutputStream out = response.getOutputStream();
-        InputStream in = null;
-        try {
-            in = new BufferedInputStream(new FileInputStream(file));
-            IOUtils.copy(in, out);
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException ignored) {}
-            }
-            out.close();
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    protected File getFile() {
+        return settings.getAdminGUIDistFile();
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
-
 }

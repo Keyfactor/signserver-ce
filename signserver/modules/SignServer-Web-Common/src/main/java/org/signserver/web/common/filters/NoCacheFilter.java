0762ff108a8680bf7d4036e3470090d1e2cff565
Markus Kilås <markus.kilas@primekey.com>
2017-01-02 14:51:35 +0000

    DSS-1289: Adding no-cache headers to the admin web
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@7918 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Web-Common/src/main/java/org/signserver/web/common/filters/NoCacheFilter.java
@@ -0,0 +1,48 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.web.common.filters;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Filter adding no-cache headers to the response.
+ *
+ * @author Markus Kilås
+ * @version $Id$
+ */
+public class NoCacheFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig fc) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse res = (HttpServletResponse)response;
+        res.addHeader("Cache-Control", "no-cache, no-store, must-revalidate, private");
+        res.addHeader("Pragma", "no-cache");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+    
+}

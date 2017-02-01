6d62ab0ea8f5e0de0f5938a4a26a703709d51cda
Marcus Lundblad <marcus.lundblad@primekey.com>
2017-02-01 08:23:33 +0000

    DSS-1318: Remove unused import.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk/signserver@8013 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- modules/mod-enterprise/SignServer-Module-MSAuthCode/src/main/java/org/signserver/module/msauthcode/signer/MSAuthCodeSigner.java
+++ modules/mod-enterprise/SignServer-Module-MSAuthCode/src/main/java/org/signserver/module/msauthcode/signer/MSAuthCodeSigner.java
@@ -21,7 +21,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.RandomAccessFile;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.security.MessageDigest;

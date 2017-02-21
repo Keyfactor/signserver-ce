417b6ef1366b5a75cce92aba6da090939d62d376
Markus Kilås <markus.kilas@primekey.com>
2017-02-21 16:32:22 +0000

    DSS-1309: Refactored duplicated code for roles handling
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk/signserver@8073 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- modules/mod-enterprise/SignServer-Admin-web/src/main/java/org/signserver/admin/web/AdministratorsBean.java
+++ modules/mod-enterprise/SignServer-Admin-web/src/main/java/org/signserver/admin/web/AdministratorsBean.java
@@ -28,6 +28,8 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
 import org.apache.log4j.Logger;
 import org.cesecore.util.CertTools;
+import org.signserver.admin.common.roles.AdminEntry;
+import org.signserver.admin.common.roles.AdminsUtil;
 import org.signserver.common.ClientEntry;
 import org.signserver.common.GlobalConfiguration;
 import org.signserver.common.SignServerUtil;
@@ -56,8 +58,8 @@ public class AdministratorsBean {
     @ManagedProperty(value = "#{authenticationBean}")
     private AuthenticationBean authBean;
 
-    private LinkedHashMap<ClientEntry, Entry> admins;
-    private List<Entry> config;
+    private LinkedHashMap<ClientEntry, AdminEntry> admins;
+    private List<AdminEntry> config;
     private GlobalConfiguration globalConfig;
 
     private Boolean allowAny;
@@ -99,14 +101,14 @@ public class AdministratorsBean {
         return globalConfig;
     }
 
-    public List<Entry> getConfig() throws AdminNotAuthorizedException {
+    public List<AdminEntry> getConfig() throws AdminNotAuthorizedException {
         if (config == null) {
             config = new ArrayList<>(getAdmins().values());
         }
         return config;
     }
 
-    private LinkedHashMap<ClientEntry, Entry> getAdmins() throws AdminNotAuthorizedException {
+    private LinkedHashMap<ClientEntry, AdminEntry> getAdmins() throws AdminNotAuthorizedException {
         if (admins == null) {
             admins = parseAdmins();
         }
@@ -214,7 +216,7 @@ public class AdministratorsBean {
         return "administrators?faces-redirect=true";
     }
 
-    public void editAction(Entry entry) throws AdminNotAuthorizedException {
+    public void editAction(AdminEntry entry) throws AdminNotAuthorizedException {
         this.edit = true;
         this.certSN = entry.getHexSerialNumber();
         this.issuerDN = entry.getClient().getIssuerDN();
@@ -223,7 +225,7 @@ public class AdministratorsBean {
         this.roleArchiveAuditor = entry.isArchiveAuditor();
     }
 
-    public void removeAction(Entry entry) {
+    public void removeAction(AdminEntry entry) {
         this.remove = true;
         this.certSN = entry.getHexSerialNumber();
         this.issuerDN = entry.getClient().getIssuerDN();
@@ -240,15 +242,15 @@ public class AdministratorsBean {
         workerSessionBean.setGlobalProperty(authBean.getAdminCertificate(),
                 GlobalConfiguration.SCOPE_GLOBAL,
                 "WSADMINS",
-                serializeAdmins(admins));
+                AdminsUtil.serializeAdmins(admins));
         workerSessionBean.setGlobalProperty(authBean.getAdminCertificate(),
                 GlobalConfiguration.SCOPE_GLOBAL,
                 "WSAUDITORS",
-                serializeAuditors(admins));
+                AdminsUtil.serializeAuditors(admins));
         workerSessionBean.setGlobalProperty(authBean.getAdminCertificate(),
                 GlobalConfiguration.SCOPE_GLOBAL,
                 "WSARCHIVEAUDITORS",
-                serializeArchiveAuditors(admins));
+                AdminsUtil.serializeArchiveAuditors(admins));
         return "administrators?faces-redirect=true";
     }
 
@@ -259,8 +261,8 @@ public class AdministratorsBean {
 
         getAdmins().remove(oldEntry);
 
-        final Entry newEntry
-                = new Entry(newCred,
+        final AdminEntry newEntry
+                = new AdminEntry(newCred,
                         roleAdmin,
                         roleAuditor,
                         roleArchiveAuditor);
@@ -270,15 +272,15 @@ public class AdministratorsBean {
         workerSessionBean.setGlobalProperty(authBean.getAdminCertificate(),
                 GlobalConfiguration.SCOPE_GLOBAL,
                 "WSADMINS",
-                serializeAdmins(admins));
+                AdminsUtil.serializeAdmins(admins));
         workerSessionBean.setGlobalProperty(authBean.getAdminCertificate(),
                 GlobalConfiguration.SCOPE_GLOBAL,
                 "WSAUDITORS",
-                serializeAuditors(admins));
+                AdminsUtil.serializeAuditors(admins));
         workerSessionBean.setGlobalProperty(authBean.getAdminCertificate(),
                 GlobalConfiguration.SCOPE_GLOBAL,
                 "WSARCHIVEAUDITORS",
-                serializeArchiveAuditors(admins));
+                AdminsUtil.serializeArchiveAuditors(admins));
         return "administrators?faces-redirect=true";
     }
 
@@ -286,8 +288,8 @@ public class AdministratorsBean {
 
         final ClientEntry newCred = new ClientEntry(new BigInteger(certSN, 16), issuerDN);
 
-        final Entry newEntry
-                = new Entry(newCred,
+        final AdminEntry newEntry
+                = new AdminEntry(newCred,
                         roleAdmin,
                         roleAuditor,
                         roleArchiveAuditor);
@@ -298,19 +300,19 @@ public class AdministratorsBean {
             workerSessionBean.setGlobalProperty(authBean.getAdminCertificate(),
                     GlobalConfiguration.SCOPE_GLOBAL,
                     "WSADMINS",
-                    serializeAdmins(admins));
+                    AdminsUtil.serializeAdmins(admins));
         }
         if (roleAuditor) {
             workerSessionBean.setGlobalProperty(authBean.getAdminCertificate(),
                     GlobalConfiguration.SCOPE_GLOBAL,
                     "WSAUDITORS",
-                    serializeAuditors(admins));
+                    AdminsUtil.serializeAuditors(admins));
         }
         if (roleArchiveAuditor) {
             workerSessionBean.setGlobalProperty(authBean.getAdminCertificate(),
                     GlobalConfiguration.SCOPE_GLOBAL,
                     "WSARCHIVEAUDITORS",
-                    serializeArchiveAuditors(admins));
+                    AdminsUtil.serializeArchiveAuditors(admins));
         }
         return "administrators?faces-redirect=true";
     }
@@ -322,7 +324,7 @@ public class AdministratorsBean {
     public void loadCurrentAction() throws NotLoggedInException {
         X509Certificate current = getAuthBean().getAdminCertificate();
         certSN = current.getSerialNumber().toString(16);
-        issuerDN = getIssuerDN(current);
+        issuerDN = AdminsUtil.getIssuerDN(current);
     }
 
     public void cancelBrowseAction() {
@@ -342,260 +344,12 @@ public class AdministratorsBean {
 
     }
 
-    /////////////////////////////////////////////////////
-    // TODO: Duplicated from AdminGUI AdministratorsFrame.java this MUST be refactored!
-    // some adaptions done here
-    private LinkedHashMap<ClientEntry, Entry> parseAdmins()
+    private LinkedHashMap<ClientEntry, AdminEntry> parseAdmins()
             throws AdminNotAuthorizedException {
         GlobalConfiguration globalConfig1 = getGlobalConfig();
         String admins = globalConfig1.getProperty(GlobalConfiguration.SCOPE_GLOBAL, "WSADMINS");
         String auditors = globalConfig1.getProperty(GlobalConfiguration.SCOPE_GLOBAL, "WSAUDITORS");
         String archiveAuditors = globalConfig1.getProperty(GlobalConfiguration.SCOPE_GLOBAL, "WSARCHIVEAUDITORS");
-
-        final LinkedHashMap<ClientEntry, Entry> entryMap
-                = new LinkedHashMap<>();
-
-        // Admins
-        if (admins != null && admins.contains(";")) {
-            for (String entryString : admins.split(";")) {
-                final String[] parts = entryString.split(",", 2);
-                try {
-                    final ClientEntry client
-                            = new ClientEntry(new BigInteger(parts[0], 16), parts[1]);
-                    Entry entry = entryMap.get(client);
-                    if (entry == null) {
-                        entry = new Entry(client);
-                        entryMap.put(client, entry);
-                    }
-                    entry.setAdmin(true);
-                } catch (NumberFormatException e) {
-                    LOG.error("Invalid serialnumber for administrator: "
-                            + parts[0]);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    LOG.error("Invalid administrator definition: " + entryString);
-                }
-            }
-        }
-
-        // Auditors
-        if (auditors != null && auditors.contains(";")) {
-            for (String entryString : auditors.split(";")) {
-                final String[] parts = entryString.split(",", 2);
-
-                try {
-                    final ClientEntry client
-                            = new ClientEntry(new BigInteger(parts[0], 16), parts[1]);
-                    Entry entry = entryMap.get(client);
-                    if (entry == null) {
-                        entry = new Entry(client);
-                        entryMap.put(client, entry);
-                    }
-                    entry.setAuditor(true);
-                } catch (NumberFormatException e) {
-                    LOG.error("Invalid serialnumber for administrator: "
-                            + parts[0]);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    LOG.error("Invalid administrator definition: " + entryString);
-                }
-            }
-        }
-
-        // Archive auditors
-        if (archiveAuditors != null && archiveAuditors.contains(";")) {
-            for (final String entryString : archiveAuditors.split(";")) {
-                final String[] parts = entryString.split(",", 2);
-
-                try {
-                    final ClientEntry client
-                            = new ClientEntry(new BigInteger(parts[0], 16), parts[1]);
-                    Entry entry = entryMap.get(client);
-                    if (entry == null) {
-                        entry = new Entry(client);
-                        entryMap.put(client, entry);
-                    }
-                    entry.setArchiveAuditor(true);
-                } catch (NumberFormatException e) {
-                    LOG.error("Invalid serialnumber for administrator: "
-                            + parts[0]);
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    LOG.error("Invalid administrator definition: " + entryString);
-                }
-            }
-        }
-
-        return entryMap;
+        return AdminsUtil.parseAdmins(admins, auditors, archiveAuditors);
     }
-
-    private static String serializeAdmins(final Map<ClientEntry, Entry> entries) {
-        final StringBuilder buff = new StringBuilder();
-        for (Entry entry : entries.values()) {
-            if (entry.isAdmin()) {
-                buff.append(entry.getClient().getSerialNumber().toString(16));
-                buff.append(",");
-                buff.append(entry.getClient().getIssuerDN());
-                buff.append(";");
-            }
-        }
-        return buff.toString();
-    }
-
-    private static String serializeAuditors(final Map<ClientEntry, Entry> entries) {
-        final StringBuilder buff = new StringBuilder();
-        for (Entry entry : entries.values()) {
-            if (entry.isAuditor()) {
-                buff.append(entry.getClient().getSerialNumber().toString(16));
-                buff.append(",");
-                buff.append(entry.getClient().getIssuerDN());
-                buff.append(";");
-            }
-        }
-        return buff.toString();
-    }
-
-    private static String serializeArchiveAuditors(final Map<ClientEntry, Entry> entries) {
-        final StringBuilder buff = new StringBuilder();
-        for (Entry entry : entries.values()) {
-            if (entry.isArchiveAuditor()) {
-                buff.append(entry.getClient().getSerialNumber().toString(16));
-                buff.append(",");
-                buff.append(entry.getClient().getIssuerDN());
-                buff.append(";");
-            }
-        }
-        return buff.toString();
-    }
-
-    /**
-     * @return The issuer DN formatted as expected by the AdminWS
-     */
-    private String getIssuerDN(X509Certificate certificate) {
-        String dn = certificate.getIssuerX500Principal().getName();
-        SignServerUtil.BasicX509NameTokenizer tok
-                = new SignServerUtil.BasicX509NameTokenizer(dn);
-        StringBuilder buf = new StringBuilder();
-        while (tok.hasMoreTokens()) {
-            final String token = tok.nextToken();
-            buf.append(token);
-            if (tok.hasMoreTokens()) {
-                buf.append(", ");
-            }
-        }
-        return buf.toString();
-    }
-
-    public static class Entry {
-
-        private final ClientEntry client;
-        private boolean admin;
-        private boolean auditor;
-        private boolean archiveAuditor;
-        private final String hexSerialNumber;
-
-        public Entry(final ClientEntry client, final boolean admin,
-                final boolean auditor, final boolean archiveAuditor) {
-            this.client = client;
-            this.admin = admin;
-            this.auditor = auditor;
-            this.archiveAuditor = archiveAuditor;
-            this.hexSerialNumber = client.getSerialNumber().toString(16);
-        }
-
-        private Entry(final ClientEntry client) {
-            this.client = client;
-            this.hexSerialNumber = client.getSerialNumber().toString(16);
-        }
-
-        public ClientEntry getClient() {
-            return client;
-        }
-
-        public boolean isAdmin() {
-            return admin;
-        }
-
-        public boolean isAuditor() {
-            return auditor;
-        }
-
-        public boolean isArchiveAuditor() {
-            return archiveAuditor;
-        }
-
-        public void setAdmin(boolean admin) {
-            this.admin = admin;
-        }
-
-        public void setAuditor(boolean auditor) {
-            this.auditor = auditor;
-        }
-
-        public void setArchiveAuditor(boolean archiveAuditor) {
-            this.archiveAuditor = archiveAuditor;
-        }
-
-        public String getHexSerialNumber() {
-            return hexSerialNumber;
-        }
-
-        @Override
-        public int hashCode() {
-            int hash = 5;
-            hash = 29 * hash + Objects.hashCode(this.client);
-            hash = 29 * hash + (this.admin ? 1 : 0);
-            hash = 29 * hash + (this.auditor ? 1 : 0);
-            hash = 29 * hash + (this.archiveAuditor ? 1 : 0);
-            hash = 29 * hash + Objects.hashCode(this.hexSerialNumber);
-            return hash;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj) {
-                return true;
-            }
-            if (obj == null) {
-                return false;
-            }
-            if (getClass() != obj.getClass()) {
-                return false;
-            }
-            final Entry other = (Entry) obj;
-            if (this.admin != other.admin) {
-                return false;
-            }
-            if (this.auditor != other.auditor) {
-                return false;
-            }
-            if (this.archiveAuditor != other.archiveAuditor) {
-                return false;
-            }
-            if (!Objects.equals(this.hexSerialNumber, other.hexSerialNumber)) {
-                return false;
-            }
-            return Objects.equals(this.client, other.client);
-        }
-
-        public String getRoles() {
-            final StringBuilder sb = new StringBuilder();
-            if (admin) {
-                sb.append("Admin");
-            }
-            if (auditor) {
-                if (sb.length() != 0) {
-                    sb.append(", ");
-                }
-                sb.append("Auditor");
-            }
-            if (archiveAuditor) {
-                if (sb.length() != 0) {
-                    sb.append(", ");
-                }
-                sb.append("Archive Auditor");
-            }
-            return sb.toString();
-        }
-
-    }
-
-    ////////////////////////////////////////////////////
 }

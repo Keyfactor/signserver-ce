4c5306c210b27faef1725e13909be490f593b511
Marcus Lundblad <marcus.lundblad@primekey.com>
2017-05-02 08:56:41 +0000

    DSS-1318: Add license header and Javadoc.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk/signserver@8373 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- modules/mod-enterprise/SignServer-Module-MSAuthCode/src/main/java/org/signserver/module/msauthcode/signer/SpcSipInfo.java
+++ modules/mod-enterprise/SignServer-Module-MSAuthCode/src/main/java/org/signserver/module/msauthcode/signer/SpcSipInfo.java
@@ -1,18 +1,15 @@
-/**
- * Copyright 2012 Emmanuel Bourg
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 
 package org.signserver.module.msauthcode.signer;
 
@@ -26,8 +23,11 @@ import org.bouncycastle.asn1.ASN1Sequence;
 import org.bouncycastle.asn1.BERSequence;
 
 /**
- *
- * @author Emmanual Bourgh
+ * Implements the SpcSipInfo ASN.1 structure.
+ * 
+ * @author Markus Kilås
+ * @author Marcus Lundblad
+ * @version $Id$
  */
 public class SpcSipInfo extends ASN1Object {
     

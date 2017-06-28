220ae08eaec5ddad17deccdba873a27832d22949
Markus Kilås <markus.kilas@primekey.com>
2017-06-28 09:42:58 +0000

    DSS-1409: Adapting to change in CertTools.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@8435 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-XAdES/src/main/java/org/signserver/module/xades/validator/AbstractCustomCertPathChecker.java
+++ signserver/modules/SignServer-Module-XAdES/src/main/java/org/signserver/module/xades/validator/AbstractCustomCertPathChecker.java
@@ -120,7 +120,7 @@ public abstract class AbstractCustomCertPathChecker extends PKIXCertPathChecker 
         String ocspURLString = null;
         try {
             ocspURLString = CertTools.getAuthorityInformationAccessOcspUrl(certificate);
-        } catch (CertificateParsingException ex) {
+        } catch (Exception ex) {
             if (LOG.isDebugEnabled()) {
                LOG.debug("Could not read AIA OCSP URL: " + ex.getMessage());
             }

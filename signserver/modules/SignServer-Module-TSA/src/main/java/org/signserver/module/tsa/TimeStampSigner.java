2eb0b258b99f10f91e0745ce17a034f36d5f5588
Markus Kilås <markus.kilas@primekey.com>
2017-07-10 12:07:29 +0000

    DSS-1415: Reworked legacy encoding support using the new change in BC 4afdba1
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/branches/Branch_4_1@8461 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-TSA/src/main/java/org/signserver/module/tsa/TimeStampSigner.java
+++ signserver/modules/SignServer-Module-TSA/src/main/java/org/signserver/module/tsa/TimeStampSigner.java
@@ -37,6 +37,7 @@ import java.util.List;
 import java.util.Set;
 import javax.persistence.EntityManager;
 import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.ASN1Encoding;
 import org.bouncycastle.asn1.ASN1ObjectIdentifier;
 import org.bouncycastle.asn1.cmp.PKIStatus;
 import org.bouncycastle.asn1.x500.X500Name;
@@ -657,7 +658,7 @@ public class TimeStampSigner extends BaseSigner {
                         timeStampResponseGen.generateGrantedResponse(timeStampRequest,
                                                       serialNumber, date,
                                                       includeStatusString ? "Operation Okay" : null,
-                                                      additionalExtensions, legacyEncoding);
+                                                      additionalExtensions);
             } catch (TSPException e) {
                 if (LOG.isDebugEnabled()) {
                     LOG.debug("Got exception generating response: ", e);
@@ -667,7 +668,7 @@ public class TimeStampSigner extends BaseSigner {
             }
 
             final TimeStampToken token = timeStampResponse.getTimeStampToken();
-            final byte[] signedbytes = timeStampResponse.getEncoded();
+            final byte[] signedbytes = legacyEncoding ? timeStampResponse.getEncoded(ASN1Encoding.DL) : timeStampResponse.getEncoded();
             out.write(signedbytes);
             cert = crypto.getCertificate();
             

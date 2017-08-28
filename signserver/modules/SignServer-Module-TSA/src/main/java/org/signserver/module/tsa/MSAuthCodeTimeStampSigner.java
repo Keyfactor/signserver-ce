d493d10b893152b4d37e98b42d58b7afe9c574b7
Marcus Lundblad <marcus.lundblad@primekey.com>
2017-08-28 13:29:01 +0000

    DSS-1436: Add debug logging when skipping trailing NULL byte.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@8568 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Module-TSA/src/main/java/org/signserver/module/tsa/MSAuthCodeTimeStampSigner.java
+++ signserver/modules/SignServer-Module-TSA/src/main/java/org/signserver/module/tsa/MSAuthCodeTimeStampSigner.java
@@ -273,6 +273,9 @@ public class MSAuthCodeTimeStampSigner extends BaseSigner {
              * data
              */
             if (requestbytes[requestbytes.length - 1] == 0x00) {
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Skipping trailing NULL byte in request");
+                }
                 inputBytes = Arrays.copyOf(requestbytes, requestbytes.length - 1);
             } else {
                 inputBytes = requestbytes;

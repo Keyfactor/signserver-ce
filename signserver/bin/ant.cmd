ff0f17ced1d1652e720ff020f834dcc5dd308fa3
Vinay Singh <vinay.singh@primekey.se>
2017-11-05 09:15:08 +0000

    DSS-1458: removed findstr command from ant.cmd
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@8784 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/bin/ant.cmd
+++ signserver/bin/ant.cmd
@@ -1,6 +1,6 @@
 @echo off
 if "%ANT_HOME%" == "" (
-  ant -f build.xml -q %* | FindStr /v "Trying to override old definition of task http://www.netbeans.org/"
+  ant -f build.xml -q %*
 ) else (
-  %ANT_HOME%\bin\ant -f build.xml -q %* | FindStr /v "Trying to override old definition of task http://www.netbeans.org/"
+  %ANT_HOME%\bin\ant -f build.xml -q %*
 )

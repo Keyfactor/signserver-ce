99c2b95e31554c2d1fbfeba823649d89af8d0321
Vinay Singh <vinay.singh@primekey.se>
2018-01-10 08:56:42 +0000

    DSS-1490: added support for PSS algorithms
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@8969 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/MechanismNames.java
+++ signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/MechanismNames.java
@@ -15,7 +15,9 @@ package org.signserver.server.cryptotokens;
 import java.util.HashMap;
 import java.util.Map;
 import org.pkcs11.jacknji11.C;
+import org.pkcs11.jacknji11.CKG;
 import org.pkcs11.jacknji11.CKM;
+import org.pkcs11.jacknji11.ULong;
 
 /**
  * Handles mapping between PKCS#11 mechanism constant names and values.
@@ -28,6 +30,7 @@ public class MechanismNames {
     private static final Map<Long, String> L2S = C.createL2SMap(CKM.class);
     private static final Map<String, Long> S2L;
     private static final Map<String, Long> SIGALGOS2L;
+    public static final Map<Long, byte[]> CKM_PARAMS;
 
     static {
         S2L = new HashMap<>(L2S.size());
@@ -45,7 +48,16 @@ public class MechanismNames {
         SIGALGOS2L.put("SHA512withRSA", CKM.SHA512_RSA_PKCS);
         SIGALGOS2L.put("NONEwithDSA", CKM.DSA);
         SIGALGOS2L.put("SHA1withDSA", CKM.DSA_SHA1);
-        //SIGALGOS2L.put("SHA256withRSAandMGF1", CKM.SHA256_RSA_PKCS_PSS);
+        SIGALGOS2L.put("SHA1withRSAandMGF1", CKM.SHA1_RSA_PKCS_PSS);
+        SIGALGOS2L.put("SHA256withRSAandMGF1", CKM.SHA256_RSA_PKCS_PSS);
+        SIGALGOS2L.put("SHA384withRSAandMGF1", CKM.SHA384_RSA_PKCS_PSS);
+        SIGALGOS2L.put("SHA512withRSAandMGF1", CKM.SHA512_RSA_PKCS_PSS);
+        
+        CKM_PARAMS = new HashMap<>();
+        CKM_PARAMS.put(CKM.SHA1_RSA_PKCS_PSS, ULong.ulong2b(new long[]{CKM.SHA_1, CKG.MGF1_SHA1, 20}));
+        CKM_PARAMS.put(CKM.SHA256_RSA_PKCS_PSS, ULong.ulong2b(new long[]{CKM.SHA256, CKG.MGF1_SHA256, 32}));
+        CKM_PARAMS.put(CKM.SHA384_RSA_PKCS_PSS, ULong.ulong2b(new long[]{CKM.SHA384, CKG.MGF1_SHA384, 48}));
+        CKM_PARAMS.put(CKM.SHA512_RSA_PKCS_PSS, ULong.ulong2b(new long[]{CKM.SHA512, CKG.MGF1_SHA512, 64}));  
     }
 
     /**

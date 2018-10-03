6279ea343281d82c1369250264a6e323dd6e6bde
George Matveev <george.matveev@primekey.se>
2018-10-03 09:01:40 +0000

    DSS-1477: using String.Utils.trim() in ALL places of Admin Web UI where trim() is needed
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@9543 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-ejb/src/main/java/org/signserver/ejb/WorkerSessionBean.java
+++ signserver/modules/SignServer-ejb/src/main/java/org/signserver/ejb/WorkerSessionBean.java
@@ -477,6 +477,7 @@ public class WorkerSessionBean implements WorkerSessionLocal, WorkerSessionRemot
         
             signer.generateKey(keyAlgorithm, keySpec, alias, authCode, params,
                     servicesImpl);
+
             final HashMap<String, Object> auditMap = new HashMap<>();
             auditMap.put(AdditionalDetailsTypes.KEYALG.name(), keyAlgorithm);
             auditMap.put(AdditionalDetailsTypes.KEYSPEC.name(), keySpec);

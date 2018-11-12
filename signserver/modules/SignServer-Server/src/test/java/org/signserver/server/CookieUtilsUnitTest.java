39f48a27813a810096122e47ec2168b792e468cb
Markus Kilås <markus.kilas@primekey.com>
2018-11-12 15:22:44 +0000

    DSS-1793: Source code headers. SVN keyword.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@9687 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/test/java/org/signserver/server/CookieUtilsUnitTest.java
+++ signserver/modules/SignServer-Server/src/test/java/org/signserver/server/CookieUtilsUnitTest.java
@@ -1,8 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.signserver.server;
 
 import org.apache.log4j.Logger;
@@ -64,8 +71,6 @@ public class CookieUtilsUnitTest {
         
         // Check some of the cookie-octets handling (more tests are in testToCookieValue()) to see that this method uses that one
         assertEquals("key4=comma%2C", CookieUtils.toCookiePair("key4", "comma,"));
-        
-        // TODO: Check cookie name also
     }
 
     /**

82c942cf0337da0ed4242bc4265cc1d016040791
Markus Kilås <markus.kilas@primekey.com>
2019-02-08 16:40:24 +0000

    DSS-1903: Fix syntax issue in script
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@10320 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/maven-manually-install-deps.sh
+++ signserver/modules/maven-manually-install-deps.sh
@@ -6,4 +6,4 @@ MAVEN_REPO_DIR=${MAVEN_REPO_DIR:-"${HOME}/.m2/repository"}
 mvn -Dmaven.repo.local=${MAVEN_REPO_DIR} install:install-file -Dfile=lib/ext/cesecore-common-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-common -Dversion=7.0.0.1 -Dpackaging=jar &&
 mvn -Dmaven.repo.local=${MAVEN_REPO_DIR} install:install-file -Dfile=lib/ext/cesecore-entity-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-entity -Dversion=7.0.0.1 -Dpackaging=jar &&
 mvn -Dmaven.repo.local=${MAVEN_REPO_DIR} install:install-file -Dfile=lib/ext/cesecore-interfaces-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-interfaces -Dversion=7.0.0.1 -Dpackaging=jar &&
-mvn -Dmaven.repo.local=${MAVEN_REPO_DIR}install:install-file -Dfile=lib/ext/cesecore-ejb-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-ejb -Dversion=7.0.0.1 -Dpackaging=ejb
+mvn -Dmaven.repo.local=${MAVEN_REPO_DIR} install:install-file -Dfile=lib/ext/cesecore-ejb-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-ejb -Dversion=7.0.0.1 -Dpackaging=ejb

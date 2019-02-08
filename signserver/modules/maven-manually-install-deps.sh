3e58d07d8caf240db8ffc2772ab04c9373d2b29c
Vinay Singh <vinay.singh@primekey.se>
2019-02-08 15:00:11 +0000

    DSS-1903: fix jar & artifact name in script
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@10314 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/maven-manually-install-deps.sh
+++ signserver/modules/maven-manually-install-deps.sh
@@ -3,5 +3,5 @@
 
 mvn install:install-file -Dfile=lib/ext/cesecore-common-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-common -Dversion=7.0.0.1 -Dpackaging=jar &&
 mvn install:install-file -Dfile=lib/ext/cesecore-entity-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-entity -Dversion=7.0.0.1 -Dpackaging=jar &&
-mvn install:install-file -Dfile=lib/ext/cesecore-ejb-interfaces-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-ejb-interfaces -Dversion=7.0.0.1 -Dpackaging=jar &&
+mvn install:install-file -Dfile=lib/ext/cesecore-interfaces-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-interfaces -Dversion=7.0.0.1 -Dpackaging=jar &&
 mvn install:install-file -Dfile=lib/ext/cesecore-ejb-7.0.0.1.jar -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-ejb -Dversion=7.0.0.1 -Dpackaging=ejb

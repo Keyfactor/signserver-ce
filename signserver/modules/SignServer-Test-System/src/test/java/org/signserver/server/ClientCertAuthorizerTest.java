a2888824e6e39f993632b9cec987c20f8de2ca70
Marcus Lundblad <marcus.lundblad@primekey.com>
2019-03-28 07:45:58 +0000

    DSS-1722: Add test with an additional matching rule.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@10579 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-System/src/test/java/org/signserver/server/ClientCertAuthorizerTest.java
+++ signserver/modules/SignServer-Test-System/src/test/java/org/signserver/server/ClientCertAuthorizerTest.java
@@ -28,9 +28,15 @@ public class ClientCertAuthorizerTest {
     private final String SUBJECT_SERIALNUMBER = "723507815f93333";
     private final String SUBJECT_SERIALNUMBER_WITH_LEADING_ZERO = "0723507815f93333";
     private final String SUBJECT_SERIALNUMBER_UPPERCASE = "723507815F93333";
+    private final String SUBJECT_SERIALNUMBER_OTHER = "123456789ab";
     private final String ISSUER_DN = "CN=DSS Root CA 10,OU=Testing,O=SignServer,C=SE";
     private final String DESCRIPTION = "Test auth client";
     
+    /**
+     * Test authorization with a subject serial number rule.
+     * 
+     * @throws Exception 
+     */
     @Test
     public void testSerialNumber() throws Exception {
         try {
@@ -72,6 +78,12 @@ public class ClientCertAuthorizerTest {
         }
     }
     
+    /**
+     * Test authorization with a subject serial number rule specifying SN with
+     * a leading zero.
+     * 
+     * @throws Exception 
+     */
     @Test
     public void testSerialNumberWithLeadingZero() throws Exception {
         try {
@@ -113,6 +125,12 @@ public class ClientCertAuthorizerTest {
         }
     }
     
+    /**
+     * Test authorization with a subject serial number rule. Specifying SN
+     * with hex upper-case digits.
+     * 
+     * @throws Exception 
+     */
     @Test
     public void testSerialNumberUppercase() throws Exception {
         try {
@@ -133,6 +151,60 @@ public class ClientCertAuthorizerTest {
                     "-add", 
                     "-matchSubjectWithType", "CERTIFICATE_SERIALNO",
                     "-matchSubjectWithValue", SUBJECT_SERIALNUMBER_UPPERCASE,
+                    "-matchIssuerWithValue", ISSUER_DN,
+                    "-description", DESCRIPTION));
+            test.getWorkerSession().reloadConfiguration(signerId);
+            
+            assertEquals("execute signdocument", 0,
+                    client.execute("signdocument", "-workerid", String.valueOf(signerId),
+                                   "-data", "foo", "-protocol", "CLIENTWS",
+                                   "-host", "localhost",
+                                   "-port", "8443",
+                                   "-keystore",
+                                   dss10Path + File.separator + "dss10_admin1.p12",
+                                   "-keystorepwd", "foo123",
+                                   "-truststore",
+                                   dss10Path + File.separator + "dss10_truststore.jks",
+                                   "-truststorepwd", "changeit"));
+                                           
+        } finally {
+            test.removeWorker(test.getSignerIdCMSSigner1());
+        }
+    }
+    
+    /**
+     * Test authorization with a subject serial number rule. With an additional
+     * matching rule.
+     * 
+     * @throws Exception 
+     */
+    @Test
+    public void testSerialNumberWithAdditionalRule() throws Exception {
+        try {
+            final int signerId = test.getSignerIdCMSSigner1();
+            final String dss10Path = test.getSignServerHome().getAbsolutePath() +
+                                           File.separator + "res" +
+                                           File.separator + "test" +
+                                           File.separator + "dss10";
+            
+            test.addCMSSigner1();
+            test.getWorkerSession().setWorkerProperty(signerId, "AUTHTYPE",
+                                                      "org.signserver.server.ClientCertAuthorizer");
+            test.getWorkerSession().reloadConfiguration(signerId);
+            
+            // Add
+            assertEquals("execute add", 0,
+                    cli.execute("clients", "-worker", String.valueOf(signerId),
+                    "-add", 
+                    "-matchSubjectWithType", "CERTIFICATE_SERIALNO",
+                    "-matchSubjectWithValue", SUBJECT_SERIALNUMBER,
+                    "-matchIssuerWithValue", ISSUER_DN,
+                    "-description", DESCRIPTION));
+            assertEquals("execute add", 0,
+                    cli.execute("clients", "-worker", String.valueOf(signerId),
+                    "-add", 
+                    "-matchSubjectWithType", "CERTIFICATE_SERIALNO",
+                    "-matchSubjectWithValue", SUBJECT_SERIALNUMBER_OTHER,
                     "-matchIssuerWithValue", ISSUER_DN,
                     "-description", DESCRIPTION));
             test.getWorkerSession().reloadConfiguration(signerId);

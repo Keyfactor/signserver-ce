5777e231d2ad03c161863923e01398ef3988d587
Markus Kilås <markus.kilas@primekey.com>
2019-05-14 09:35:04 +0000

    DSS-1980: WIP: Adding initial compliance tests.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@10814 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Test-System/src/test/java/org/signserver/module/debiandpkgsig/signer/DebianDpkgSigSignerGpgComplianceTest.java
@@ -0,0 +1,387 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.module.debiandpkgsig.signer;
+
+import org.signserver.module.openpgp.signer.*;
+import java.io.File;
+import java.io.IOException;
+import static junit.framework.TestCase.assertTrue;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+import org.bouncycastle.openpgp.PGPPublicKey;
+import static org.junit.Assert.assertEquals;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.signserver.client.cli.ClientCLI;
+import org.signserver.common.AbstractCertReqData;
+import org.signserver.common.PKCS10CertReqInfo;
+import org.signserver.common.WorkerIdentifier;
+import org.signserver.common.util.PropertiesConstants;
+import org.signserver.testutils.CLITestHelper;
+import org.signserver.testutils.ComplianceTestUtils;
+import org.signserver.testutils.ModulesTestCase;
+
+/**
+ * Compliance test running dpkg-sig to verify signatures created by the
+ * DebianDpkgSigSigner.
+ *
+ * These tests can be disabled by setting the test.dpkgsig.enabled test
+ * configuration parameter to false for test environments where the dpkg-sig
+ * CLI tool is not available.
+ *
+ * @author Markus Kilås
+ * @author Marcus Lundblad
+ * @version $Id: TimeStampSignerOpenSslComplianceTest.java 9042 2018-01-18 10:16:57Z malu9369 $
+ */
+public class DebianDpkgSigSignerGpgComplianceTest {
+    /** Logger for this class */
+    private static final Logger LOG = Logger.getLogger(DebianDpkgSigSignerGpgComplianceTest.class);
+    private static final String DPKGSIG_ENABLED = "test.dpkgsig.enabled";
+    private static final String HELLO_DEB = "res/test/HelloDeb.deb";
+
+    private final ModulesTestCase helper = new ModulesTestCase();
+    private static final CLITestHelper CLI = new CLITestHelper(ClientCLI.class);
+
+    private static boolean enabled;
+    private static boolean ecdsaSupported;
+
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        enabled = Boolean.valueOf(new ModulesTestCase().getConfig().getProperty(DPKGSIG_ENABLED));
+        if (enabled) {
+            final ComplianceTestUtils.ProcResult res =
+                ComplianceTestUtils.execute("gpg2", "--version");
+            final String output = ComplianceTestUtils.toString(res.getOutput());
+            LOG.info("GPG version output: " + output);
+            ecdsaSupported = output.contains("ECDSA");
+        }
+    }
+
+    @Before
+    public void setUpTest() {
+        Assume.assumeTrue("dpkg-sig enabled", enabled);
+    }
+
+    /**
+     * Test that the command "gpg2 --version" prints a reasonable message.
+     * And log the version string, so that it's visible in the stdout from the
+     * test.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGpgVersion() throws Exception {
+        final ComplianceTestUtils.ProcResult res =
+                ComplianceTestUtils.execute("gpg2", "--version");
+        final String output = ComplianceTestUtils.toString(res.getOutput());
+
+        assertTrue("Contains GPG message", output.startsWith("gpg (GnuPG)"));
+        LOG.info("GPG version output: " + output);
+    }
+
+    @Test
+    public void testSigning_RSA_SHA256() throws Exception {
+        signAndVerify("rsa2048", "SHA-256", HELLO_DEB);
+    }
+
+    /*@Test
+    public void testSigning_RSA_SHA1() throws Exception {
+        signAndVerify("rsa2048", "SHA-1", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA_SHA224() throws Exception {
+        signAndVerify("rsa2048", "SHA-224", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA_SHA384() throws Exception {
+        signAndVerify("rsa2048", "SHA-384", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA_SHA512() throws Exception {
+        signAndVerify("rsa2048", "SHA-512", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_ECDSA_SHA256() throws Exception {
+        Assume.assumeTrue("ECDSA supported by GPG version", ecdsaSupported);
+        signAndVerify("nistp256", "SHA-256", HELLO_DEB);
+    }
+
+    // Note: GPG won't accept SHA-1 with a 256-bit curve
+    //@Test
+    //public void testSigning_ECDSA_SHA1() throws Exception {
+    //    Assume.assumeTrue("ECDSA supported by GPG version", ecdsaSupported);
+    //    signAndVerify("nistp256", "SHA-1");
+    //}
+
+    // Note: GPG won't accept SHA-224 with a 256-bit curve
+    //@Test
+    //public void testSigning_ECDSA_SHA224() throws Exception {
+    //    Assume.assumeTrue("ECDSA supported by GPG version", ecdsaSupported);
+    //    signAndVerify("nistp256", "SHA-224");
+    //}
+
+    @Test
+    public void testSigning_ECDSA_SHA384() throws Exception {
+        Assume.assumeTrue("ECDSA supported by GPG version", ecdsaSupported);
+        signAndVerify("nistp256", "SHA-384", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_ECDSA_SHA512() throws Exception {
+        Assume.assumeTrue("ECDSA supported by GPG version", ecdsaSupported);
+        signAndVerify("nistp256", "SHA-512", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA4096_SHA256() throws Exception {
+        signAndVerify("rsa4096", "SHA-256", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA4096_SHA1() throws Exception {
+        signAndVerify("rsa4096", "SHA-1", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA4096_SHA224() throws Exception {
+        signAndVerify("rsa4096", "SHA-224", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA4096_SHA384() throws Exception {
+        signAndVerify("rsa4096", "SHA-384", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA4096_SHA512() throws Exception {
+        signAndVerify("rsa4096", "SHA-512", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_DSA1024_SHA256() throws Exception {
+        signAndVerify("dsa1024", "SHA-256", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_DSA1024_SHA1() throws Exception {
+        signAndVerify("dsa1024", "SHA-1", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_DSA1024_SHA224() throws Exception {
+        signAndVerify("dsa1024", "SHA-224", HELLO_DEB);
+    }
+
+    // Note: Not supported with SUN/JKS:
+    //@Test
+    //public void testSigning_DSA1024_SHA384() throws Exception {
+    //    signAndVerify("dsa1024", "SHA-384");
+    //}
+
+    // Note: Not supported with SUN/JKS:
+    //@Test
+    //public void testSigning_DSA1024_SHA512() throws Exception {
+    //    signAndVerify("dsa1024", "SHA-512");
+    //}
+
+    @Test
+    public void testSigning_RSA_SHA256_clearText() throws Exception {
+        signAndVerify("rsa2048", "SHA-256", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA_SHA1_clearText() throws Exception {
+        signAndVerify("rsa2048", "SHA-1", HELLO_DEB);
+    }
+
+    // Note: Not supported by BC:
+    //@Test
+    //public void testSigning_RSA_SHA224_clearText() throws Exception {
+    //    signAndVerify("rsa2048", "SHA-224", false, false);
+    //}
+
+    @Test
+    public void testSigning_RSA_SHA384_clearText() throws Exception {
+        signAndVerify("rsa2048", "SHA-384", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA_SHA512_clearText() throws Exception {
+        signAndVerify("rsa2048", "SHA-512", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_ECDSA_SHA256_clearText() throws Exception {
+        Assume.assumeTrue("ECDSA supported by GPG version", ecdsaSupported);
+        signAndVerify("nistp256", "SHA-256", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_ECDSA_SHA384_clearText() throws Exception {
+        Assume.assumeTrue("ECDSA supported by GPG version", ecdsaSupported);
+        signAndVerify("nistp256", "SHA-384", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_ECDSA_SHA512_clearText() throws Exception {
+        Assume.assumeTrue("ECDSA supported by GPG version", ecdsaSupported);
+        signAndVerify("nistp256", "SHA-512", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA4096_SHA256_clearText() throws Exception {
+        signAndVerify("rsa4096", "SHA-256", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA4096_SHA1_clearText() throws Exception {
+        signAndVerify("rsa4096", "SHA-1", HELLO_DEB);
+    }
+
+    // Note: Not supported by BC:
+    //@Test
+    //public void testSigning_RSA4096_SHA224_clearText() throws Exception {
+    //    signAndVerify("rsa4096", "SHA-224", false, false);
+    //}
+
+    @Test
+    public void testSigning_RSA4096_SHA384_clearText() throws Exception {
+        signAndVerify("rsa4096", "SHA-384", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_RSA4096_SHA512_clearText() throws Exception {
+        signAndVerify("rsa4096", "SHA-512", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_DSA1024_SHA256_clearText() throws Exception {
+        signAndVerify("dsa1024", "SHA-256", HELLO_DEB);
+    }
+
+    @Test
+    public void testSigning_DSA1024_SHA1_clearText() throws Exception {
+        signAndVerify("dsa1024", "SHA-1", HELLO_DEB);
+    }
+
+    // Note: Not supported by BC:
+    //@Test
+    //public void testSigning_DSA1024_SHA224_clearText() throws Exception {
+    //    signAndVerify("dsa1024", "SHA-224", false, false);
+    //}
+*/
+
+    /**
+     * Sets up a signer using a key with the chosen algorithm,
+     * then adds a user ID to the public key,
+     * then imports the public key in a new local key ring and trusts it,
+     * then performs a signing,
+     * then verifies the signature using GPG2 and checks that it succeeds
+     * and has the expected algorithms.
+     *
+     * @param expectedKeyAlgorithm in gpg format
+     * @param digestAlgorithm to use
+     * @param revokeAfter if true, issue and import a revocation certificate afterwards
+     * @throws Exception
+     */
+    private void signAndVerify(final String expectedKeyAlgorithm, final String digestAlgorithm, String inputFile) throws Exception {
+        final int workerId = 42;
+        final String workerName = "OpenPGPSigner-" + expectedKeyAlgorithm + "-" + digestAlgorithm ;
+        final File inFile = new File(helper.getSignServerHome(), inputFile);
+        final File outFile = File.createTempFile("HelloDeb", "-signed.deb");
+        final File ringFile = File.createTempFile("pubring", ".gpg");
+        final File trustFile = File.createTempFile("trustdb", ".gpg");
+        final File publicKeyFile = File.createTempFile("pubkey", ".gpg");
+        final String userId = "User 1 (Code Signing) <user1@example.com>";
+
+        try {
+
+            helper.addSigner("org.signserver.module.debiandpkgsig.signer.DebianDpkgSigSigner", workerId, workerName, true);
+            switch (expectedKeyAlgorithm) {
+                case "rsa2048": {
+                    helper.getWorkerSession().setWorkerProperty(workerId, "DEFAULTKEY", "signer00001");
+                    break;
+                }
+                case "rsa4096": {
+                    helper.getWorkerSession().setWorkerProperty(workerId, "DEFAULTKEY", "ts40003");
+                    break;
+                }
+                case "nistp256": {
+                    helper.getWorkerSession().setWorkerProperty(workerId, "DEFAULTKEY", "signer00002");
+                    break;
+                }
+                case "dsa1024": {
+                    final File keystore = new File(helper.getSignServerHome(), "res/test/dss10/dss10_tssigner6dsa.jks");
+                    helper.getWorkerSession().setWorkerProperty(workerId, PropertiesConstants.CRYPTOTOKEN_IMPLEMENTATION_CLASS, "org.signserver.server.cryptotokens.JKSCryptoToken");
+                    helper.getWorkerSession().setWorkerProperty(workerId, "KEYSTOREPATH", keystore.getAbsolutePath());
+                    helper.getWorkerSession().setWorkerProperty(workerId, "DEFAULTKEY", "mykey");
+                    break;
+                }
+                default: {
+                    throw new UnsupportedOperationException("Test does not support key algorithm: " + expectedKeyAlgorithm);
+                }
+            }
+
+            helper.getWorkerSession().setWorkerProperty(workerId, "DIGEST_ALGORITHM", digestAlgorithm);
+            helper.getWorkerSession().reloadConfiguration(workerId);
+
+            // Add User ID and get public key
+            final AbstractCertReqData requestData = (AbstractCertReqData) helper.getWorkerSession().getCertificateRequest(new WorkerIdentifier(workerId), new PKCS10CertReqInfo(digestAlgorithm + "withRSA", userId, null), false);
+            final byte[] publicKeyBytes = requestData.toBinaryForm();
+            FileUtils.writeByteArrayToFile(publicKeyFile, publicKeyBytes);
+            final PGPPublicKey pgpPublicKey = OpenPGPUtils.parsePublicKeys(requestData.toArmoredForm()).get(0);
+
+            // Import public key
+            trustFile.delete(); // Seems to be a bug in older versions of gpg not liking that the file is empty but non-existing is fine: https://dev.gnupg.org/T2417
+            ComplianceTestUtils.ProcResult res = ComplianceTestUtils.execute("gpg2", "--trustdb-name", trustFile.getAbsolutePath(), "--no-default-keyring", "--keyring", ringFile.getAbsolutePath(),
+                    "--import", publicKeyFile.getAbsolutePath());
+            assertEquals("gpg2 --import: " + res.getErrorMessage(), 0, res.getExitValue());
+
+            // Trust public key
+            // Equaivalent of Bash: echo -e "trust\n5\ny\nsave\n" | gpg --command-fd 0 --edit-key F7B50A4D55F6E703
+            res = ComplianceTestUtils.executeWriting("trust\n5\ny\nsave\n".getBytes(), "gpg2", "--trustdb-name", trustFile.getAbsolutePath(), "--no-default-keyring", "--keyring", ringFile.getAbsolutePath(),
+                    "--command-fd", "0", "--no-tty", "--edit-key", OpenPGPUtils.formatKeyID(pgpPublicKey.getKeyID()));
+            assertEquals("gpg2 --edit-key: " + res.getErrorMessage(), 0, res.getExitValue());
+
+            // Sign
+            assertEquals("Status code", ClientCLI.RETURN_SUCCESS,
+                         CLI.execute("signdocument", "-workername",
+                                     workerName,
+                                     "-infile", inFile.getAbsolutePath(),
+                                     "-outfile", outFile.getAbsolutePath()));
+
+            // Verify
+            res = ComplianceTestUtils.execute("dpkg-sig", "--gpgoptions", "--trustdb-name " + trustFile.getAbsolutePath() + " --no-default-keyring --keyring " + ringFile.getAbsolutePath(),
+                        "--verify", outFile.getAbsolutePath());
+
+            final String output = res.getErrorMessage();
+
+            assertTrue("Expecting Good signature: " + output, output.contains("gpg: Good signature from \"User 1 (Code Signing) <user1@example.com>\""));
+            assertEquals("return code", 0, res.getExitValue());
+
+        } finally {
+            helper.removeWorker(workerId);
+            FileUtils.deleteQuietly(outFile);
+            FileUtils.deleteQuietly(ringFile);
+            FileUtils.deleteQuietly(trustFile);
+            FileUtils.deleteQuietly(publicKeyFile);
+        }
+    }
+
+}

ef3ae20f7b69b88880578fc1046d412520aaab3b
Markus Kilås <markus.kilas@primekey.com>
2019-05-22 09:02:55 +0000

    DSS-1823: DSS-1822: System test for both server-side PGP signer and client-side plain signer
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@10891 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Test-System/src/test/java/org/signserver/module/openpgp/signer/OpenPGPSignerTest.java
+++ signserver/modules/SignServer-Test-System/src/test/java/org/signserver/module/openpgp/signer/OpenPGPSignerTest.java
@@ -12,26 +12,70 @@
  *************************************************************************/
 package org.signserver.module.openpgp.signer;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
+import org.bouncycastle.bcpg.ArmoredInputStream;
+import org.bouncycastle.bcpg.BCPGInputStream;
+import org.bouncycastle.openpgp.PGPPublicKey;
+import org.bouncycastle.openpgp.PGPSignature;
+import org.bouncycastle.openpgp.PGPSignatureList;
+import org.bouncycastle.openpgp.jcajce.JcaPGPObjectFactory;
+import org.bouncycastle.openpgp.operator.jcajce.JcaPGPContentVerifierBuilderProvider;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.signserver.client.cli.ClientCLI;
 import org.signserver.common.AbstractCertReqData;
 import org.signserver.common.PKCS10CertReqInfo;
+import org.signserver.common.SignServerUtil;
+import org.signserver.common.WorkerConfig;
 import org.signserver.common.WorkerIdentifier;
 import org.signserver.common.WorkerStatus;
+import org.signserver.common.WorkerType;
+import org.signserver.common.util.PathUtil;
+import org.signserver.ejb.interfaces.WorkerSession;
+import org.signserver.testutils.CLITestHelper;
 import org.signserver.testutils.ModulesTestCase;
 
 /**
  * System tests for the OpenPGPSigner.
- * 
+ *
  * @author Marcus Lundblad
  * @version $Id$
  */
 public class OpenPGPSignerTest {
     /** Logger for this class */
-    private static final Logger LOG = Logger.getLogger(OpenPGPSignerGpgComplianceTest.class);
+    private static final Logger LOG = Logger.getLogger(OpenPGPSignerTest.class);
 
-    private final ModulesTestCase helper = new ModulesTestCase();
+    private static final int WORKER_OPENPGPSIGNER = 40000;
+    private static final int WORKER_OPENPGPPLAINSIGNER = 41000;
+
+    private final File sampleBinaryFile;
+
+    private final ModulesTestCase testCase = new ModulesTestCase();
+    private final WorkerSession workerSession = testCase.getWorkerSession();
+    private static final CLITestHelper CLI = new CLITestHelper(ClientCLI.class);
+
+    public OpenPGPSignerTest() throws FileNotFoundException {
+        final File home = PathUtil.getAppHome();
+        sampleBinaryFile = new File(home, "res/test/pdf/sample.pdf"); // Let's use any binary file as input
+    }
+
+    
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        SignServerUtil.installBCProvider();
+    }
     
     /**
      * Test that generating a certification works both when generating
@@ -39,8 +83,8 @@ public class OpenPGPSignerTest {
      * set in PGPPUBLICKEY, installing the new public certificate and and
      * setting PGPPUBLICKEY to the new certificate and updating DEFAULTKEY.
      * The worker should be active in both cases.
-     * 
-     * @throws Exception 
+     *
+     * @throws Exception
      */
     @Test
     public void testGeneratePublicKeyForNoDefaultKey() throws Exception {
@@ -48,50 +92,185 @@ public class OpenPGPSignerTest {
         final String workerName = "OpenPGPSigner-gen-non-default";
         try {
             final WorkerIdentifier wi = new WorkerIdentifier(workerId);
-            helper.addSigner("org.signserver.module.openpgp.signer.OpenPGPSigner",
+            testCase.addSigner("org.signserver.module.openpgp.signer.OpenPGPSigner",
                              workerId, workerName, true);
-            helper.getWorkerSession().setWorkerProperty(workerId, "DEFAULTKEY",
+            testCase.getWorkerSession().setWorkerProperty(workerId, "DEFAULTKEY",
                                                         "signer00001");
-            helper.getWorkerSession().setWorkerProperty(workerId,
+            testCase.getWorkerSession().setWorkerProperty(workerId,
                                                         "DETACHEDSIGNATURE",
                                                         "true");
-            helper.getWorkerSession().reloadConfiguration(workerId);
+            testCase.getWorkerSession().reloadConfiguration(workerId);
 
             PKCS10CertReqInfo certReqInfo =
                     new PKCS10CertReqInfo("SHA256withRSA", "User1", null);
             AbstractCertReqData requestData = (AbstractCertReqData)
-                    helper.getWorkerSession().getCertificateRequest(wi,
+                    testCase.getWorkerSession().getCertificateRequest(wi,
                                                                     certReqInfo,
                                                                     false);
-            
+
             String pgpPublicKey = requestData.toArmoredForm();
-            helper.getWorkerSession().setWorkerProperty(workerId, "PGPPUBLICKEY",
+            testCase.getWorkerSession().setWorkerProperty(workerId, "PGPPUBLICKEY",
                                                         pgpPublicKey);
-            helper.getWorkerSession().reloadConfiguration(workerId);
-            
-            WorkerStatus status = helper.getWorkerSession().getStatus(wi);
+            testCase.getWorkerSession().reloadConfiguration(workerId);
+
+            WorkerStatus status = testCase.getWorkerSession().getStatus(wi);
             assertTrue("Worker active: " + status.getFatalErrors().toString(),
                        status.getFatalErrors().isEmpty());
-            
+
             // generate certification for another key
             certReqInfo = new PKCS10CertReqInfo("SHA256withRSA", "User2", null);
             requestData = (AbstractCertReqData)
-                    helper.getWorkerSession().getCertificateRequest(wi,
+                    testCase.getWorkerSession().getCertificateRequest(wi,
                                                                     certReqInfo,
                                                                     false,
                                                                     "signer00003");
             pgpPublicKey = requestData.toArmoredForm();
-            helper.getWorkerSession().setWorkerProperty(workerId, "PGPPUBLICKEY",
+            testCase.getWorkerSession().setWorkerProperty(workerId, "PGPPUBLICKEY",
                                                         pgpPublicKey);
-            helper.getWorkerSession().setWorkerProperty(workerId, "DEFAULTKEY",
+            testCase.getWorkerSession().setWorkerProperty(workerId, "DEFAULTKEY",
                                                         "signer00003");
-            helper.getWorkerSession().reloadConfiguration(workerId);
+            testCase.getWorkerSession().reloadConfiguration(workerId);
 
-            status = helper.getWorkerSession().getStatus(wi);
+            status = testCase.getWorkerSession().getStatus(wi);
             assertTrue("Worker active: " + status.getFatalErrors().toString(),
                        status.getFatalErrors().isEmpty());
         } finally {
-            helper.removeWorker(workerId);
+            testCase.removeWorker(workerId);
         }
     }
+
+    /**
+     * Tests adding a User Id to the public key, sign something producing 
+     * detached signature and verifying it with client-side option.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddUserIdDetachedSignAndVerify_clientSide() throws Exception {
+        addUserIdDetachedSignAndVerify(true);
+    }
+
+    /**
+     * Tests adding a User Id to the public key, sign something producing 
+     * detached signature and verifying it.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testAddUserIdDetachedSignAndVerify_serverSide() throws Exception {
+        addUserIdDetachedSignAndVerify(false);
+    }
+
+    private void setupOpenPGPSignerOnlyProperties(final int workerId, boolean detachedSignature) throws Exception {
+        // Setup worker
+        workerSession.setWorkerProperty(workerId, WorkerConfig.TYPE, WorkerType.PROCESSABLE.name());
+        workerSession.setWorkerProperty(workerId, WorkerConfig.IMPLEMENTATION_CLASS, "org.signserver.module.openpgp.signer.OpenPGPSigner");
+        workerSession.setWorkerProperty(workerId, "NAME", "OpenPGPSigner" + workerId);
+        workerSession.setWorkerProperty(workerId, "AUTHTYPE", "NOAUTH");
+        workerSession.setWorkerProperty(workerId, "CRYPTOTOKEN", testCase.getSignerNameDummy1());
+        workerSession.setWorkerProperty(workerId, "DEFAULTKEY", "signer00003");
+        workerSession.setWorkerProperty(workerId, "DETACHEDSIGNATURE", String.valueOf(detachedSignature));
+    }
+
+    private void setupOpenPGPPlainSignerOnlyProperties(final int workerId) throws Exception {
+        // Setup worker
+        workerSession.setWorkerProperty(workerId, WorkerConfig.TYPE, WorkerType.PROCESSABLE.name());
+        workerSession.setWorkerProperty(workerId, WorkerConfig.IMPLEMENTATION_CLASS, "org.signserver.module.openpgp.enterprise.signer.PGPPlainSigner");
+        workerSession.setWorkerProperty(workerId, "NAME", "OpenPGPPlainSigner" + workerId);
+        workerSession.setWorkerProperty(workerId, "AUTHTYPE", "NOAUTH");
+        workerSession.setWorkerProperty(workerId, "CRYPTOTOKEN", testCase.getSignerNameDummy1());
+        workerSession.setWorkerProperty(workerId, "DEFAULTKEY", "signer00003");
+        workerSession.setWorkerProperty(workerId, "SIGNATUREALGORITHM", "NONEwithRSA");
+    }
+
+    /**
+     * Tests adding a User Id to the public key, sign something using CLI
+     * producing detached signature and verifying it.
+     *
+     * @param clientSide if client-side option should be used
+     * @throws Exception
+     */
+    private void addUserIdDetachedSignAndVerify(boolean clientSide) throws Exception {
+        LOG.info("addUserIdDetachedSignAndVerify-" + (clientSide ? "clientSide" : "serverSide"));
+        int workerId = 0;
+        try {
+            final File outFile = File.createTempFile("outfile-", ".asc");
+            testCase.addDummySigner1(true);
+            if (clientSide) {
+                workerId = WORKER_OPENPGPPLAINSIGNER;
+                setupOpenPGPPlainSignerOnlyProperties(WORKER_OPENPGPPLAINSIGNER);
+            } else {
+                workerId = WORKER_OPENPGPSIGNER;
+                setupOpenPGPSignerOnlyProperties(WORKER_OPENPGPSIGNER, true);
+            }
+            workerSession.reloadConfiguration(testCase.getSignerIdDummy1());
+            workerSession.reloadConfiguration(WORKER_OPENPGPSIGNER);
+
+            // Generate the public key
+            final String userId = "Worker (" + workerId + ") <worker@example.com>";
+            final PKCS10CertReqInfo certReqInfo = new PKCS10CertReqInfo("SHA256WithRSA", userId, null);
+            AbstractCertReqData csr = (AbstractCertReqData) workerSession.getCertificateRequest(new WorkerIdentifier(workerId), certReqInfo, false);
+            assertNotNull(csr);
+            String publicKeyArmored = csr.toArmoredForm();
+            assertTrue("public key header: " + publicKeyArmored, publicKeyArmored.contains("-----BEGIN PGP PUBLIC KEY BLOCK-----"));
+            assertTrue("public key footer: " + publicKeyArmored, publicKeyArmored.contains("-----END PGP PUBLIC KEY BLOCK-----"));
+
+            // Store the updated public key
+            workerSession.setWorkerProperty(workerId, "PGPPUBLICKEY", publicKeyArmored);
+            workerSession.reloadConfiguration(workerId);
+
+            // Check the status has no errors and that the user id is printed
+            WorkerStatus status = workerSession.getStatus(new WorkerIdentifier(workerId));
+            assertEquals("fatal errors", "[]", status.getFatalErrors().toString());
+            ByteArrayOutputStream bout = new ByteArrayOutputStream();
+            status.displayStatus(new PrintStream(bout), true);
+            String statusOutput = bout.toString(StandardCharsets.UTF_8.toString());
+            assertTrue("key contains user id: " + statusOutput, statusOutput.contains(userId));
+
+            // Test signing
+            final byte[] originalData = FileUtils.readFileToByteArray(sampleBinaryFile);
+            if (clientSide) {
+                assertEquals("Status code", ClientCLI.RETURN_SUCCESS,
+                         CLI.execute("signdocument", "-workerid",
+                                     String.valueOf(workerId),
+                                     "-infile", sampleBinaryFile.getAbsolutePath(),
+                                     "-outfile", outFile.getAbsolutePath(),
+                                     "-clientside",
+                                     "-filetype", "PGP",
+                                     "-digestalgorithm", "SHA256"));
+            } else {
+                assertEquals("Status code", ClientCLI.RETURN_SUCCESS,
+                         CLI.execute("signdocument", "-workerid",
+                                     String.valueOf(workerId),
+                                     "-infile", sampleBinaryFile.getAbsolutePath(),
+                                     "-outfile", outFile.getAbsolutePath()));
+            }
+            final byte[] signedBytes = FileUtils.readFileToByteArray(outFile);
+
+
+            // Verify signature
+            PGPSignature sig;
+            try (InputStream in = createInputStream(new ByteArrayInputStream(signedBytes), true)) {
+                JcaPGPObjectFactory objectFactory = new JcaPGPObjectFactory(in);
+                PGPSignatureList p3 = (PGPSignatureList) objectFactory.nextObject();
+                sig = p3.get(0);
+            }
+            final PGPPublicKey pgpPublicKey = OpenPGPUtils.parsePublicKeys(publicKeyArmored).get(0);
+            sig.init(new JcaPGPContentVerifierBuilderProvider().setProvider("BC"), pgpPublicKey);
+            sig.update(originalData);
+            assertTrue("verified", sig.verify());
+
+        } finally {
+            testCase.removeWorker(testCase.getSignerIdDummy1());
+            if (workerId != 0) {
+                testCase.removeWorker(workerId);
+            }
+        }
+    }
+    // TODO: See OpenPGPP11SignTest.java for a clear-text version of above method
+
+    private BCPGInputStream createInputStream(InputStream in, boolean armored) throws IOException {
+        return new BCPGInputStream(armored ? new ArmoredInputStream(in) : in);
+    }
+
 }

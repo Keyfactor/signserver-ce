e3dbd837cb69c90392398d8890197f95aa7066d4
Marcus Lundblad <marcus.lundblad@primekey.com>
2019-08-19 13:51:43 +0000

    DSS-2016: Add license header and interface Javadoc
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@11170 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Common/src/main/java/org/signserver/serviceprovider/PeersProvider.java
+++ signserver/modules/SignServer-Common/src/main/java/org/signserver/serviceprovider/PeersProvider.java
@@ -1,15 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.signserver.serviceprovider;
 
 import java.util.List;
 import org.cesecore.authentication.tokens.AuthenticationToken;
 
 /**
- *
+ * Interface to implement for exposing peer systems functionallity to the
+ * admin web GUI.
+ * 
  * @author Marcus Lundblad
  * @version $Id$
  */

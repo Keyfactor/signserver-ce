2167d701a1649a6ee0b921284e39611ee5f644c2
Marcus Lundblad <marcus.lundblad@primekey.com>
2020-01-30 12:42:19 +0000

    DSS-2064: Add license header and author tags
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@11802 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/AzureKeyVaultKeyStoreDelegator.java
+++ signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/AzureKeyVaultKeyStoreDelegator.java
@@ -1,8 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package org.signserver.server.cryptotokens;
 
 import java.io.IOException;
@@ -22,8 +29,11 @@ import org.signserver.common.CryptoTokenOfflineException;
 import org.signserver.server.IServices;
 
 /**
+ * Implementation of KeyStoreDelegator using an Azure Key Vault cloud
+ * key vault implementation from CESeCore.
  *
- * @author user
+ * @author Markus Kilås
+ * @version $Id$
  */
 class AzureKeyVaultKeyStoreDelegator implements KeyStoreDelegator {
     

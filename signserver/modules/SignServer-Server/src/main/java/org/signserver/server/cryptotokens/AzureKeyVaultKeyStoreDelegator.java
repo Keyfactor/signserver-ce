eb0b84cdb635532cfaca091ae34280df77cea08b
Marcus Lundblad <marcus.lundblad@primekey.com>
2020-02-06 15:25:13 +0000

    DSS-2064: Add additional token entry info
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@11814 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/AzureKeyVaultKeyStoreDelegator.java
+++ signserver/modules/SignServer-Server/src/main/java/org/signserver/server/cryptotokens/AzureKeyVaultKeyStoreDelegator.java
@@ -21,12 +21,21 @@ import java.security.UnrecoverableKeyException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateEncodingException;
 import java.security.cert.CertificateException;
+import java.security.interfaces.RSAPublicKey;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.logging.Level;
+import java.util.Map;
+import org.apache.log4j.Logger;
+import org.cesecore.certificates.util.AlgorithmConstants;
+import org.cesecore.certificates.util.AlgorithmTools;
 import org.cesecore.keys.token.AzureCryptoToken;
 import org.signserver.common.CryptoTokenOfflineException;
 import org.signserver.server.IServices;
+import static org.signserver.server.cryptotokens.CryptoTokenHelper.INFO_KEY_ALGORITHM;
+import static org.signserver.server.cryptotokens.CryptoTokenHelper.INFO_KEY_PUBLIC_EXPONENT;
+import static org.signserver.server.cryptotokens.CryptoTokenHelper.INFO_KEY_SIGNINGS;
+import static org.signserver.server.cryptotokens.CryptoTokenHelper.INFO_KEY_SPECIFICATION;
 
 /**
  * Implementation of KeyStoreDelegator using an Azure Key Vault cloud
@@ -36,7 +45,8 @@ import org.signserver.server.IServices;
  * @version $Id$
  */
 class AzureKeyVaultKeyStoreDelegator implements KeyStoreDelegator {
-    
+    // Logger for this class
+    private static final Logger LOG = Logger.getLogger(AzureKeyVaultKeyStoreDelegator.class);
     private final AzureCryptoToken delegate;
 
     public AzureKeyVaultKeyStoreDelegator(AzureCryptoToken delegate) {
@@ -95,14 +105,47 @@ class AzureKeyVaultKeyStoreDelegator implements KeyStoreDelegator {
     }
 
     @Override
-    public void addAdditionalDataToEntry(TokenEntry entry, char[] authCode, IServices services) throws KeyStoreException, CryptoTokenOfflineException {
-        System.err.println("TODO: Add aditional data to entry");
+    public void addAdditionalDataToEntry(final TokenEntry entry,
+                                         final char[] authCode,
+                                         final IServices services)
+            throws KeyStoreException, CryptoTokenOfflineException {
+        final Map<String, String> info = new HashMap<>();
+        final String keyAlias = entry.getAlias();
+        
         try {
+            if (TokenEntry.TYPE_PRIVATEKEY_ENTRY.equals(entry.getType())) {
+                try {
+                    final PublicKey pubKey = delegate.getPublicKey(keyAlias);
+                    final String keyAlgorithm =
+                                AlgorithmTools.getKeyAlgorithm(pubKey);
+
+                    info.put(INFO_KEY_ALGORITHM, keyAlgorithm);
+                    info.put(INFO_KEY_SPECIFICATION,
+                             AlgorithmTools.getKeySpecification(pubKey));
+
+                    if (AlgorithmConstants.KEYALGORITHM_RSA.equals(keyAlgorithm)) {
+                        final RSAPublicKey rsaKey = (RSAPublicKey) pubKey;
+
+                        info.put(INFO_KEY_PUBLIC_EXPONENT,
+                                rsaKey.getPublicExponent().toString(10));
+                    }
+                    info.put(INFO_KEY_SIGNINGS,
+                             String.valueOf(CryptoTokenHelper.getNoOfSignings(pubKey,
+                                            services)));
+
+
+                } catch (org.cesecore.keys.token.CryptoTokenOfflineException ex) {
+                    info.put("Error", ex.getMessage());
+                    LOG.error("Crypto token offline");
+                }
+            }
             entry.setParsedChain(new Certificate[0]);
-            //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         } catch (CertificateEncodingException ex) {
-            java.util.logging.Logger.getLogger(AzureKeyVaultCryptoToken.class.getName()).log(Level.SEVERE, null, ex);
+            info.put("Error", ex.getMessage());
+            LOG.error("Certificate could not be encoded for alias: " + keyAlias, ex);
         }
+
+        entry.setInfo(info);
     }
 
     @Override

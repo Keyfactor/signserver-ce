b0c2986767bed4c72fb1eca35c9e6549292dc486
Marcus Lundblad <marcus.lundblad@primekey.com>
2020-03-05 13:49:33 +0000

    DSS-2105: Add unit tests for valid token and no token
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@11851 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-ejb/src/test/java/org/signserver/server/JWTAuthorizerUnitTest.java
+++ signserver/modules/SignServer-ejb/src/test/java/org/signserver/server/JWTAuthorizerUnitTest.java
@@ -12,10 +12,25 @@
  *************************************************************************/
 package org.signserver.server;
 
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import java.security.KeyPair;
+import java.security.Security;
+import java.util.Base64;
+import java.util.Date;
 import java.util.List;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.signserver.common.AuthorizationRequiredException;
+import org.signserver.common.RequestContext;
 import org.signserver.common.WorkerConfig;
+import org.signserver.test.utils.builders.CryptoUtils;
 
 /**
  * Unit tests for JWTAuthorizer.
@@ -25,6 +40,20 @@ import org.signserver.common.WorkerConfig;
  */
 public class JWTAuthorizerUnitTest {
 
+    private static final String TEST_ISSUER1 = "issuer1";
+    private static final String TEST_SUBJECT1 = "subject1";
+    private static KeyPair keyPair;
+    
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        Security.addProvider(new BouncyCastleProvider());
+    }
+
+    @Before
+    public void beforeTests() throws Exception {
+        keyPair = CryptoUtils.generateRSA(2048);
+    }
+    
     /**
      * Test that setting an illegal value for MAX_ALLOWED_CLOCK_SCEW
      * results in an error message.
@@ -82,5 +111,71 @@ public class JWTAuthorizerUnitTest {
 
         assertTrue("Contains no error: " + fatalErrors.toString(),
                    fatalErrors.isEmpty());
+    }
+
+    /**
+     * Test authorizing with a valid token.
+     * 
+     * @throws Exception 
+     */
+    @Test
+    public void testValidToken() throws Exception {
+        final JWTAuthorizer instance = new JWTAuthorizer();
+        final WorkerConfig config = new WorkerConfig();
+
+        config.setProperty("AUTH_SERVER_1.ISSUER", TEST_ISSUER1);
+        config.setProperty("AUTH_SERVER_1.PUBLICKEY",
+                           new String(Base64.getEncoder().encode(keyPair.getPublic().getEncoded())));
+        instance.init(42, config, null);
+        
+        try {
+            final RequestContext context = new RequestContext();
+        
+            context.put(RequestContext.CLIENT_CREDENTIAL_BEARER, generateToken());
+            instance.isAuthorized(null, context);
+        } catch (AuthorizationRequiredException e) {
+            fail("Should be authorized");
+        }
+    }
+
+    /**
+     * Test that we get "Authorization required" when there's no token in the
+     * request.
+     *
+     * @throws Exception 
+     */
+    @Test
+    public void testNoToken() throws Exception {
+        final JWTAuthorizer instance = new JWTAuthorizer();
+        final WorkerConfig config = new WorkerConfig();
+
+        config.setProperty("AUTH_SERVER_1.ISSUER", TEST_ISSUER1);
+        config.setProperty("AUTH_SERVER_1.PUBLICKEY",
+                           new String(Base64.getEncoder().encode(keyPair.getPublic().getEncoded())));
+        instance.init(42, config, null);
+        
+        try {
+            final RequestContext context = new RequestContext();
+
+            instance.isAuthorized(null, context);
+        } catch (AuthorizationRequiredException e) {
+            assertEquals("Exception message", "Authorization required",
+                         e.getMessage());
+        }
+    }
+
+    private String generateToken() {
+        final SignatureAlgorithm sigAlg = SignatureAlgorithm.RS256;
+        final long nowMs = System.currentTimeMillis();
+        final Date now = new Date(nowMs);
+        final Date exp = new Date(nowMs + 10000); // 10 s
+        final JwtBuilder builder = Jwts.builder().setId("id")
+                .setIssuedAt(now)
+                .setSubject(TEST_SUBJECT1)
+                .setIssuer(TEST_ISSUER1)
+                .setExpiration(exp)
+                .signWith(keyPair.getPrivate(), sigAlg);
+
+        return builder.compact();
     }
 }

5fce0219bec7da7aa662f2e99857088397942cb4
Markus Kilås <markus.kilas@primekey.com>
2020-03-09 13:06:17 +0000

    DSS-2105: Moved parser construction to init. More documentation and cleanups.
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@11866 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-ejb/src/main/java/org/signserver/server/JWTAuthorizer.java
+++ signserver/modules/SignServer-ejb/src/main/java/org/signserver/server/JWTAuthorizer.java
@@ -16,6 +16,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwsHeader;
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SigningKeyResolverAdapter;
 import java.security.Key;
@@ -86,17 +87,18 @@ public class JWTAuthorizer implements IAuthorizer {
     private final Map<String, PublicKey> authServers = new HashMap<>();
     private final List<JwtMatchingRule> matchRules = new ArrayList<>(2);
     private int maxAllowedClockScew;
+    private JwtParser jwtParser;
     
     @Override
     public void init(int workerId, WorkerConfig config, EntityManager em)
             throws SignServerException {
         // Read properties
-        for (final String property : config.getProperties().stringPropertyNames()) {
+        config.getProperties().stringPropertyNames().forEach((property) -> {
             if (property.startsWith(AUTH_SERVER_PREFIX) &&
-                property.endsWith(ISSUER_SUFFIX)) {
+                    property.endsWith(ISSUER_SUFFIX)) {
                 final String publicKeyProperty =
                         AUTH_SERVER_PREFIX + property.substring(AUTH_SERVER_PREFIX.length(),
-                                                                property.indexOf(ISSUER_SUFFIX)) +
+                                property.indexOf(ISSUER_SUFFIX)) +
                         PUBLICKEY_SUFFIX;
 
                 try {
@@ -105,21 +107,21 @@ public class JWTAuthorizer implements IAuthorizer {
                     authServers.put(issuer,  KeyFactory.getInstance("RSA").generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(publicKey))));
                 } catch (InvalidKeySpecException | NoSuchAlgorithmException e) {
                     configErrors.add("Could not parse public key " +
-                                     publicKeyProperty + ": " + e.getMessage());
+                            publicKeyProperty + ": " + e.getMessage());
                 }
             } else if (property.startsWith(AUTHJWT_PREFIX) &&
-                       property.endsWith(ISSUER_SUFFIX)) {
+                    property.endsWith(ISSUER_SUFFIX)) {
                 final String claimNameProperty =
                         AUTHJWT_PREFIX + property.substring(AUTHJWT_PREFIX.length(),
-                                                            property.indexOf(ISSUER_SUFFIX)) +
+                                property.indexOf(ISSUER_SUFFIX)) +
                         CLAIM_NAME_SUFFIX;
                 final String claimValueProperty =
                         AUTHJWT_PREFIX + property.substring(AUTHJWT_PREFIX.length(),
-                                                            property.indexOf(ISSUER_SUFFIX)) +
+                                property.indexOf(ISSUER_SUFFIX)) +
                         CLAIM_VALUE_SUFFIX;
                 final String descriptionProperty =
                         AUTHJWT_PREFIX + property.substring(AUTHJWT_PREFIX.length(),
-                                                            property.indexOf(ISSUER_SUFFIX)) +
+                                property.indexOf(ISSUER_SUFFIX)) +
                         DESCRIPTION_SUFFIX;
                 final String issuer = config.getProperty(property);
                 final String claimName = config.getProperty(claimNameProperty);
@@ -130,10 +132,10 @@ public class JWTAuthorizer implements IAuthorizer {
                     configErrors.add("CLAIM.NAME and CLAIM.VALUE needs to be specified for AUTHJWT rules");
                 } else {
                     matchRules.add(new JwtMatchingRule(claimName, claimValue,
-                                                       issuer, description));
+                            issuer, description));
                 }
             }
-        }
+        });
         
         if (LOG.isDebugEnabled()) {
             LOG.debug("Authorization rules: " + matchRules);
@@ -155,6 +157,23 @@ public class JWTAuthorizer implements IAuthorizer {
             configErrors.add("Illegal value for " + MAX_ALLOWED_CLOCK_SCEW +
                              ": " + maxAllowedClockScewString);
         }
+        
+        // Create JWT parser
+        jwtParser = Jwts.parserBuilder().setSigningKeyResolver(new SigningKeyResolverAdapter() {
+            
+            @Override
+            public Key resolveSigningKey(JwsHeader header, Claims claims) {
+                final PublicKey result = authServers.get(claims.getIssuer());
+                if (LOG.isDebugEnabled()) {
+                    LOG.debug("Unknown issuer: " + claims.getIssuer());
+                }
+                if (result == null) {
+                    throw new JwtException("Unknown issuer");
+                }
+                return result;
+            }
+
+        }).setAllowedClockSkewSeconds(maxAllowedClockScew).build();
     }
 
     @Override
@@ -163,15 +182,22 @@ public class JWTAuthorizer implements IAuthorizer {
             SignServerException {
         final String bearerToken =
                 (String) requestContext.get(RequestContext.CLIENT_CREDENTIAL_BEARER);
+        // Check configuration
+        if (!configErrors.isEmpty()) {
+            throw new SignServerException("Component is misconfigured");
+        }
         
+        // Token available?
         if (bearerToken == null) {
             throw new AuthorizationRequiredException("Authorization required");
         }
         
         try {
+            // Validate token
             final Jws<Claims> jws = validateToken(bearerToken);
-            final JwtMatchingRule matchedRule = findMatchingRule(jws.getBody());
             
+            // Find matching rule
+            final JwtMatchingRule matchedRule = findMatchingRule(jws.getBody());
             if (matchedRule == null) {
                 throw new AuthorizationRequiredException("Not authorized");
             }
@@ -183,34 +209,34 @@ public class JWTAuthorizer implements IAuthorizer {
         }
     }
 
+    /**
+     * Parses and returns the validated token.
+     *
+     * @param token in textual (i.e. JSON) representation to parse
+     * @return the parsed and validated token
+     * @throws JwtException in case of failure to validate the token
+     */
     protected final Jws<Claims> validateToken(String token) throws JwtException {
         // Parse JWS
-        final Jws<Claims> jws = Jwts.parserBuilder().setSigningKeyResolver(new SigningKeyResolverAdapter() {
-
-            @Override
-            public Key resolveSigningKey(JwsHeader header, Claims claims) {
-                final PublicKey result = authServers.get(claims.getIssuer());
-                if (result == null) {
-                    throw new JwtException("Unknown issuer");
-                }
-                return result;
-            }
-
-        }).setAllowedClockSkewSeconds(maxAllowedClockScew).build().parseClaimsJws(token);
+        final Jws<Claims> jws = jwtParser.parseClaimsJws(token);
 
         if (LOG.isDebugEnabled()) {
             LOG.debug("Header: " + jws.getHeader() 
                     + "\nBody: " + jws.getBody());
         }
 
+        // Extra check for none as algorithm
         if ("none".equalsIgnoreCase(jws.getHeader().getAlgorithm())) {
             throw new JwtException("Algorithm is none");
         }
 
+        // Extra check for type
         if (!"JWT".equals(jws.getHeader().getType())) {
             throw new JwtException("Token type is not JWT");
         }
 
+        // Extra check for audiance.
+        // TODO: Later: Should be configurable?
         if (jws.getBody().getAudience() != null) {
             throw new JwtException("Specific audience specified");
         }
@@ -218,6 +244,13 @@ public class JWTAuthorizer implements IAuthorizer {
         return jws;
     }
 
+    /**
+     * Get the first rule that matches the given claims or null if no match
+     * could be found.
+     *
+     * @param claims (i.e. token body) to match against
+     * @return The matched rule or null if no match was found
+     */
     protected final JwtMatchingRule findMatchingRule(final Claims claims) {
         JwtMatchingRule result = null;
         for (JwtMatchingRule rule : matchRules) {

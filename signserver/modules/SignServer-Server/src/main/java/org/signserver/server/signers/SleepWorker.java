c56f67580dbfe0d908023a477ce076070fc28364
Marcus Lundblad <marcus.lundblad@primekey.com>
2020-09-18 08:54:59 +0000

    DSS-2211: Move to org.signserver.server.signers and also fix LOG domain
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@12286 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Server/src/main/java/org/signserver/server/signers/SleepWorker.java
@@ -0,0 +1,179 @@
+/*************************************************************************
+ *                                                                       *
+ *  SignServer: The OpenSource Automated Signing Server                  *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public           *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 2.1 of the License, or any later version.                    *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
+package org.signserver.server.signers;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import org.apache.log4j.Logger;
+import org.signserver.common.CryptoTokenOfflineException;
+import org.signserver.common.IllegalRequestException;
+import org.signserver.common.RequestContext;
+import org.signserver.common.SignServerException;
+import org.signserver.common.WorkerConfig;
+import org.signserver.server.BaseProcessable;
+import org.signserver.server.IServices;
+import org.signserver.server.WorkerContext;
+import org.signserver.server.archive.Archivable;
+import org.signserver.server.archive.DefaultArchivable;
+import org.signserver.common.data.ReadableData;
+import org.signserver.common.data.Request;
+import org.signserver.common.data.Response;
+import org.signserver.common.data.SignatureRequest;
+import org.signserver.common.data.SignatureResponse;
+import org.signserver.common.data.WritableData;
+import org.signserver.server.log.LogMap;
+
+/**
+ * Debug worker inposing a sleep pause (using Thread.sleep()).
+ * <p>
+ * The worker has two worker properties:
+ * </p>
+ * <ul>
+ *    <li>
+ *        <b>SLEEP_TIME</b> = Time to sleep in milliseconds (Required).
+ *    </li>
+ * </ul>
+ * <p>
+ * The worker will pause the request's thread for the specified number of
+ * milliseconds and reply with a message containing the input data and the
+ * time it spent in sleep.
+ * </p>
+ * @author Macus Lundblad
+ * @version $Id$
+ */
+public class SleepWorker extends BaseProcessable {
+
+    /** Logger for this class. */
+    private static final Logger LOG = Logger.getLogger(SleepWorker.class);
+
+    // Worker properties
+    public static final String PROPERTY_SLEEP_TIME = "SLEEP_TIME";
+
+    // Log fields
+    public static final String LOG_NAME = "HELLO_NAME";
+
+    // Default values
+    private static final String DEFAULT_SUFFIX = "!";
+
+    // Content types
+    private static final String REQUEST_CONTENT_TYPE = "text/plain";
+    private static final String RESPONSE_CONTENT_TYPE = "text/plain";
+
+    // Configuration errors
+    private final LinkedList<String> configErrors = new LinkedList<>();
+
+    // Configuration values
+    private int sleepTime;
+
+    @Override
+    public void init(int workerId, WorkerConfig config,
+            WorkerContext workerContext, EntityManager workerEM) {
+        super.init(workerId, config, workerContext, workerEM);
+
+        // Sleep time
+        final String sleepTimeString = config.getProperty(PROPERTY_SLEEP_TIME);
+
+        if (sleepTimeString != null) {
+            sleepTime = Integer.parseInt(sleepTimeString);
+        } else {
+            configErrors.add("Missing required property: " + PROPERTY_SLEEP_TIME);
+        }
+    }
+
+    @Override
+    public Response processData(Request signRequest,
+            RequestContext requestContext) throws IllegalRequestException,
+                CryptoTokenOfflineException, SignServerException {
+        if (!configErrors.isEmpty()) {
+            throw new SignServerException("Worker is misconfigured");
+        }
+        if (!(signRequest instanceof SignatureRequest)) {
+            throw new IllegalRequestException("Unexpected request type");
+        }
+        final SignatureRequest request = (SignatureRequest) signRequest;
+
+        // Get the data from request
+        final ReadableData requestData = request.getRequestData();
+        final WritableData responseData = request.getResponseData();
+
+        try (OutputStream out = responseData.getAsOutputStream()) {
+            // Get the name from request (We don't expect it to be large)
+            final byte[] data = requestData.getAsByteArray();
+            final String name = new String(data, StandardCharsets.UTF_8);
+            if (name.isEmpty()) {
+                // This is a client error
+                throw new IllegalRequestException(
+                        "Supplying a name as input is required.");
+            }
+
+            // Log anything interesting from the request to the worker logger
+            LogMap.getInstance(requestContext).put(LOG_NAME, name);
+
+            // Additional debug logging using Log4j
+            if (LOG.isDebugEnabled()) {
+                LOG.debug("Will produce a greeting for: " + name);
+            }
+
+            Thread.sleep(sleepTime);
+            
+            // Produce the result, ie doing the work
+            final String result = "Hi " + name + ", I've slept for " + sleepTime + " milliseconds!";
+            out.write(result.getBytes(StandardCharsets.UTF_8));
+        } catch (IOException ex) {
+            throw new SignServerException("Unable to read/write data", ex);
+        } catch (InterruptedException ex) {
+            throw new SignServerException("Unable to sleep", ex);
+        }
+
+        // Create the archivables (request and response)
+        final String archiveId = createArchiveId(new byte[0], 
+                (String) requestContext.get(RequestContext.TRANSACTION_ID));
+        final Collection<? extends Archivable> archivables = Arrays.asList(
+                new DefaultArchivable(Archivable.TYPE_REQUEST, 
+                        REQUEST_CONTENT_TYPE, requestData, archiveId), 
+                new DefaultArchivable(Archivable.TYPE_RESPONSE, 
+                        RESPONSE_CONTENT_TYPE, responseData.toReadableData(), archiveId));
+
+        // Suggest new file name
+        final Object fileNameOriginal
+                = requestContext.get(RequestContext.FILENAME);
+        if (fileNameOriginal instanceof String) {
+            requestContext.put(RequestContext.RESPONSE_FILENAME,
+                    fileNameOriginal + "");
+        }
+
+        // As everyting went well, the client can be charged for the request
+        requestContext.setRequestFulfilledByWorker(true);
+
+        // Return the response
+        return new SignatureResponse(
+                request.getRequestID(), responseData, null, archiveId,
+                archivables, RESPONSE_CONTENT_TYPE);
+    }
+
+    @Override
+    protected List<String> getFatalErrors(final IServices services) {
+        // Add our errors to the list of errors
+        final LinkedList<String> errors = new LinkedList<>(
+                super.getFatalErrors(services));
+        errors.addAll(configErrors);
+        return errors;
+    }
+
+}

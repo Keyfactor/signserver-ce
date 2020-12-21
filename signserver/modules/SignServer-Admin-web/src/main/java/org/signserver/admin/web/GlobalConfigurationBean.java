ee638bc8ad7d63a0b9eda3551bd586e3e108eb65
Nima Saboonchi <nima.saboonchi@primekey.com>
2020-12-21 10:06:46 +0000

    DSS-2118: Add "reload from database" button for global configuration
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@12405 91c8134f-2a44-0410-ba45-dce2f532dbc6

--- signserver/modules/SignServer-Admin-web/src/main/java/org/signserver/admin/web/GlobalConfigurationBean.java
+++ signserver/modules/SignServer-Admin-web/src/main/java/org/signserver/admin/web/GlobalConfigurationBean.java
@@ -30,14 +30,15 @@ import org.signserver.admin.common.auth.AdminNotAuthorizedException;
 import org.signserver.admin.web.ejb.AdminWebSessionBean;
 
 /**
- *
  * @author Markus Kilås
  * @version $Id$
  */
 @ManagedBean
 @ViewScoped
 public class GlobalConfigurationBean {
-    /** Logger for this class. */
+    /**
+     * Logger for this class.
+     */
     private static final Logger LOG = Logger.getLogger(GlobalConfigurationBean.class);
 
     @EJB
@@ -202,5 +203,14 @@ public class GlobalConfigurationBean {
             }
         }
         return sb.toString();
+    }
+
+    /**
+     * Reload global configuration from database.
+     */
+    public String reloadFromDatabase() throws AdminNotAuthorizedException {
+        config = null;
+        config = getConfig();
+        return "global-configuration?faces-redirect=true;";
     }
 }

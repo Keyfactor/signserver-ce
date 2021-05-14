dce91801ec9b03645aa14085cdbdcac7ee2304e8
Andrey Sergeev <andrey.sergeev.ext@primekey.com>
2021-05-14 07:22:09 +0000

    DSS-2288: ClientSideHashingHelper unit test
    git-svn-id: https://svn.cesecore.eu/svn/signserver-restricted/trunk@12619 91c8134f-2a44-0410-ba45-dce2f532dbc6

---
+++ signserver/modules/SignServer-Module-CMSSigner/src/test/java/org/signserver/module/cmssigner/ClientSideHashingHelperUnitTest.java
@@ -0,0 +1,274 @@
+package org.signserver.module.cmssigner;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.bouncycastle.asn1.nist.NISTObjectIdentifiers;
+import org.bouncycastle.asn1.x509.AlgorithmIdentifier;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.signserver.common.IllegalRequestException;
+import org.signserver.common.RequestContext;
+import org.signserver.common.RequestMetadata;
+import org.signserver.common.WorkerConfig;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.signserver.module.cmssigner.ClientSideHashingHelper.ACCEPTED_HASHDIGEST_ALGORITHMS;
+import static org.signserver.module.cmssigner.ClientSideHashingHelper.ALLOW_CLIENTSIDEHASHING_OVERRIDE;
+import static org.signserver.module.cmssigner.ClientSideHashingHelper.CLIENTSIDEHASHING;
+import static org.signserver.module.cmssigner.ClientSideHashingHelper.CLIENTSIDE_HASHDIGESTALGORITHM_PROPERTY;
+import static org.signserver.module.cmssigner.ClientSideHashingHelper.USING_CLIENTSUPPLIED_HASH_PROPERTY;
+
+/**
+ * A unit test for ClientSideHashingHelper.
+ *
+ * @author Andrey Sergeev
+ * @version $Id$
+ */
+public class ClientSideHashingHelperUnitTest {
+
+    private static final Logger LOG = Logger.getLogger(ClientSideHashingHelperUnitTest.class);
+
+    private static final String TRUE = Boolean.TRUE.toString();
+    private static final String FALSE = Boolean.FALSE.toString();
+
+    /**
+     * Important WorkerConfig properties are:
+     * - CLIENTSIDEHASHING
+     * - ALLOW_CLIENTSIDEHASHING_OVERRIDE
+     * - ACCEPTED_HASHDIGEST_ALGORITHMS
+     * - CLIENTSIDE_HASHDIGESTALGORITHM_PROPERTY
+     */
+    private WorkerConfig workerConfig;
+    private LinkedList<String> workerConfigErrors;
+    private final RequestContext requestContext = new RequestContext();
+    private Map<String, String> requestMetadataMap;
+    // Class under test
+    private ClientSideHashingHelper helper;
+
+    @Rule
+    public ExpectedException exceptionRule = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        helper = new ClientSideHashingHelper();
+        workerConfig = new WorkerConfig();
+        workerConfigErrors = new LinkedList<>();
+        requestMetadataMap = new HashMap<>();
+    }
+
+    @Test
+    public void usingClientSuppliedHashRequestUnset() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, FALSE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, FALSE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "");
+        helper.init(workerConfig, workerConfigErrors);
+        // when
+        final boolean result = helper.shouldUseClientSideHashing(requestContext);
+        LOG.info("usingClientSuppliedHashUnset:" + result);
+        // then
+        assertFalse("If 'USING_CLIENTSUPPLIED_HASH' unset, should return false.", result);
+        assertEquals("Should not have errors", Collections.EMPTY_LIST, workerConfigErrors);
+    }
+
+    @Test
+    public void usingClientSuppliedHashRequestSetToFalse() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, FALSE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, FALSE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, FALSE);
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // when
+        final boolean result = helper.shouldUseClientSideHashing(requestContext);
+        // then
+        assertFalse("If 'USING_CLIENTSUPPLIED_HASH' set to false, should return false.", result);
+        assertEquals("Should not have errors", Collections.EMPTY_LIST, workerConfigErrors);
+    }
+
+    @Test
+    public void failureClientSuppliedHashRequestSetToTrue() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, FALSE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, FALSE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, TRUE);
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // then
+        exceptionRule.expect(IllegalRequestException.class);
+        exceptionRule.expectMessage("Client-side hashing requested but not allowed");
+        // when
+        helper.shouldUseClientSideHashing(requestContext);
+    }
+
+    @Test
+    public void failureClientSuppliedHashSetToFalse() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, TRUE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, FALSE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, FALSE);
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // then
+        exceptionRule.expect(IllegalRequestException.class);
+        exceptionRule.expectMessage("Server-side hashing requested but not allowed");
+        // when
+        helper.shouldUseClientSideHashing(requestContext);
+    }
+
+    @Test
+    public void usingClientSuppliedHashSetToTrueAcceptedButErrors() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, TRUE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, TRUE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, TRUE);
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // when
+        final boolean result = helper.shouldUseClientSideHashing(requestContext);
+        // then
+        assertTrue("If 'USING_CLIENTSUPPLIED_HASH' set to true, should return true.", result);
+        assertNotEquals("Should have errors'", Collections.EMPTY_LIST, workerConfigErrors);
+        final String errorMessage = workerConfigErrors.getFirst();
+        assertThat(
+                "Should have an error about missing 'ACCEPTED_HASH_DIGEST_ALGORITHMS'",
+                errorMessage,
+                containsString("Must specify ACCEPTED_HASH_DIGEST_ALGORITHMS when")
+        );
+    }
+
+    @Test
+    public void usingClientSuppliedHashSetToTrueAccepted() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, TRUE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, TRUE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "SHA-256,SHA-512");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, TRUE);
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // when
+        final boolean result = helper.shouldUseClientSideHashing(requestContext);
+        // then
+        assertTrue("If 'USING_CLIENTSUPPLIED_HASH' set to true, should return true.", result);
+        assertEquals("Should not have errors", Collections.EMPTY_LIST, workerConfigErrors);
+    }
+
+    @Test
+    public void failureOnAlgorithmIdentifierWithUnknownAlgorithm() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, TRUE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, TRUE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, TRUE);
+        requestMetadataMap.put(CLIENTSIDE_HASHDIGESTALGORITHM_PROPERTY, "XYZ");
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // then
+        exceptionRule.expect(IllegalRequestException.class);
+        exceptionRule.expectMessage("Client specified an unknown digest algorithm");
+        // when
+        helper.getClientSideHashAlgorithm(requestContext);
+    }
+
+    @Test
+    public void failureOnAlgorithmIdentifierWithOtherAcceptedHashDigestAlgorithms() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, TRUE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, TRUE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "SHA-512");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, TRUE);
+        requestMetadataMap.put(CLIENTSIDE_HASHDIGESTALGORITHM_PROPERTY, "SHA-256");
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // then
+        exceptionRule.expect(IllegalRequestException.class);
+        exceptionRule.expectMessage("Client specified a non-accepted digest hash algorithm");
+        // when
+        helper.getClientSideHashAlgorithm(requestContext);
+    }
+
+    @Test
+    public void successOnAlgorithmIdentifier() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, TRUE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, TRUE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "SHA-256,SHA-512");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, TRUE);
+        requestMetadataMap.put(CLIENTSIDE_HASHDIGESTALGORITHM_PROPERTY, "SHA-256");
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // when
+        final AlgorithmIdentifier algorithmIdentifier = helper.getClientSideHashAlgorithm(requestContext);
+        // then
+        assertEquals("Should not have errors", Collections.EMPTY_LIST, workerConfigErrors);
+        assertNotNull("AlgorithmIdentifier not null", algorithmIdentifier);
+        assertEquals("Algorithm should match", NISTObjectIdentifiers.id_sha256, algorithmIdentifier.getAlgorithm());
+    }
+
+    @Test
+    public void failureOnAlgorithmNameWithUnknownAlgorithm() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, TRUE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, TRUE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, TRUE);
+        requestMetadataMap.put(CLIENTSIDE_HASHDIGESTALGORITHM_PROPERTY, "XYZ");
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // then
+        exceptionRule.expect(IllegalRequestException.class);
+        exceptionRule.expectMessage("Client specified an unknown digest algorithm");
+        // when
+        helper.getClientSideHashAlgorithmName(requestContext);
+    }
+
+    @Test
+    public void failureOnAlgorithmNameWithOtherAcceptedHashDigestAlgorithms() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, TRUE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, TRUE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "SHA-512");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, TRUE);
+        requestMetadataMap.put(CLIENTSIDE_HASHDIGESTALGORITHM_PROPERTY, "SHA-256");
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // then
+        exceptionRule.expect(IllegalRequestException.class);
+        exceptionRule.expectMessage("Client specified a non-accepted digest hash algorithm");
+        // when
+        helper.getClientSideHashAlgorithmName(requestContext);
+    }
+
+    @Test
+    public void successOnAlgorithmName() throws Exception {
+        // given
+        workerConfig.setProperty(CLIENTSIDEHASHING, TRUE);
+        workerConfig.setProperty(ALLOW_CLIENTSIDEHASHING_OVERRIDE, TRUE);
+        workerConfig.setProperty(ACCEPTED_HASHDIGEST_ALGORITHMS, "SHA-256,SHA-512");
+        helper.init(workerConfig, workerConfigErrors);
+        requestMetadataMap.put(USING_CLIENTSUPPLIED_HASH_PROPERTY, TRUE);
+        requestMetadataMap.put(CLIENTSIDE_HASHDIGESTALGORITHM_PROPERTY, "SHA-256");
+        RequestMetadata.getInstance(requestContext).putAll(requestMetadataMap);
+        // when
+        final String algorithmName = helper.getClientSideHashAlgorithmName(requestContext);
+        // then
+        assertEquals("Should not have errors", Collections.EMPTY_LIST, workerConfigErrors);
+        assertEquals("Algorithm name should match", "SHA-256", algorithmName);
+    }
+}

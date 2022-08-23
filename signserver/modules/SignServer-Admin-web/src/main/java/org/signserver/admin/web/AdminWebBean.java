436a4bd7adf123c7926a06a4ee05c6cf8fe088bb
Oscar Norman <Oscar.Norman@primekey.com>
2022-08-23 15:43:09 +0200

    DSSINTER-834: Removed hardcoded value for the new logo

--- signserver/modules/SignServer-Admin-web/src/main/java/org/signserver/admin/web/AdminWebBean.java
+++ signserver/modules/SignServer-Admin-web/src/main/java/org/signserver/admin/web/AdminWebBean.java
@@ -57,7 +57,11 @@ public class AdminWebBean {
     }
 
     public String getProductVersion() {
-        return "v" + CompileTimeSettings.getInstance().getProperty(CompileTimeSettings.SIGNSERVER_VERSION_NUMBER);
+        if (isDefaultThemeEnabled()) {
+            return CompileTimeSettings.getInstance().getProperty(CompileTimeSettings.SIGNSERVER_VERSION_NUMBER);
+        } else {
+            return "v" + CompileTimeSettings.getInstance().getProperty(CompileTimeSettings.SIGNSERVER_VERSION_NUMBER);
+        }
     }
 
     public String getVersion() {

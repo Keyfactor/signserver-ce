48d701e1def88cf24cd585428d71c64d5da9533f
Nima Saboonchi <nima.saboonchi@keyfactor.com>
2023-04-05 15:57:16 +0200

    DSSINTER-936: Maven-install-files modified to work with container build

--- signserver/lib/maven-install-files.sh
+++ signserver/lib/maven-install-files.sh
@@ -9,21 +9,21 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 # Below add commands for manually installing dependencies to Maven (for CE see separate file for EE).
 # Note that this is intended as a temporarily solution until artifacts are in the repo.
 
-echo "Will call mvn install:install-file with certain dependencies from ${DIR}/lib/ext/."
+echo "Will call mvn install:install-file with certain dependencies from ${DIR}/ext/."
 
 # Install BC: As we currently use a beta version. This should be removed after upgrading to official release.
-mvn ${MVN_OPTS} install:install-file -Dfile=${DIR}/ext/bcutil-jdk18on-1.73b.12.jar -DgroupId=org.bouncycastle -DartifactId=bcutil-jdk18on -Dversion=1.73b.12 -Dpackaging=jar
-mvn ${MVN_OPTS} install:install-file -Dfile=${DIR}/ext/bcprov-jdk18on-1.73b.12.jar -DgroupId=org.bouncycastle -DartifactId=bcprov-jdk18on -Dversion=1.73b.12 -Dpackaging=jar
-mvn ${MVN_OPTS} install:install-file -Dfile=${DIR}/ext/bcpkix-jdk18on-1.73b.12.jar -DgroupId=org.bouncycastle -DartifactId=bcpkix-jdk18on -Dversion=1.73b.12 -Dpackaging=jar
-mvn ${MVN_OPTS} install:install-file -Dfile=${DIR}/ext/bcpg-jdk18on-1.73b.12.jar -DgroupId=org.bouncycastle -DartifactId=bcpg-jdk18on -Dversion=1.73b.12 -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/bcutil-jdk18on-1.73b.12.jar" -DgroupId=org.bouncycastle -DartifactId=bcutil-jdk18on -Dversion=1.73b.12 -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/bcprov-jdk18on-1.73b.12.jar" -DgroupId=org.bouncycastle -DartifactId=bcprov-jdk18on -Dversion=1.73b.12 -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/bcpkix-jdk18on-1.73b.12.jar" -DgroupId=org.bouncycastle -DartifactId=bcpkix-jdk18on -Dversion=1.73b.12 -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/bcpg-jdk18on-1.73b.12.jar" -DgroupId=org.bouncycastle -DartifactId=bcpg-jdk18on -Dversion=1.73b.12 -Dpackaging=jar
 
 # Install KFC dependencies not yet in Central
-mvn ${MVN_OPTS} install:install-file -Dfile=${DIR}/ext/x509-common-util-8.0.Alpha0-a7f13143.jar -DgroupId=org.ejbca.cesecore -DartifactId=x509-common-util -Dversion=8.0.Alpha0-a7f13143 -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/x509-common-util-8.0.Alpha0-a7f13143.jar" -DgroupId=org.ejbca.cesecore -DartifactId=x509-common-util -Dversion=8.0.Alpha0-a7f13143 -Dpackaging=jar
 
 # Add next group of dependencies here
 
 
 # Install EE dependencies as well if available
 if [ -f "${DIR}/maven-install-files-ee.sh" ]; then
-    source ${DIR}/maven-install-files-ee.sh
+    source "${DIR}/maven-install-files-ee.sh"
 fi

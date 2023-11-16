27ece6f2b5e6f6a79ae01c2c933ccce9e4e1dde6
Markus Kilås <markus.kilas@primekey.com>
2023-11-16 13:40:27 +0100

    DSS-2700: Include version number in OpenAPI

--- signserver/modules/SignServer-REST-web/src/main/java/org/signserver/rest/api/ApplicationConfig.java
+++ signserver/modules/SignServer-REST-web/src/main/java/org/signserver/rest/api/ApplicationConfig.java
@@ -14,7 +14,30 @@ package org.signserver.rest.api;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
 
+/**
+ * Configuration for the "SignServer REST Interface" JAX-RS application.
+ *
+ * Note: Remember to increase the version number in the info object appropriately
+ * using semantic versioning when updating the API and/or the OpenAPI spec.
+ *
+ * Versioning scheme:
+ * - Using semantic versioning https://semver.org/spec/v2.0.0.html
+ * - Syntax: MAJOR.MINOR.PATCH
+ * - Increase MAJOR version when you make incompatible API changes
+ * - Increase MINOR version when you add functionality in a backward compatible manner
+ * - Increase PATCH version when you make backward compatible bug fixes to the
+ *   API or improvements/clarifications to the OpenAPI documentation
+ *
+ */
 @ApplicationPath("")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "SignServer REST Interface",
+                version = "1.1.0"
+        )
+)
 public class ApplicationConfig extends Application {
 }

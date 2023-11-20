83737133a05892f14de23dc2ec355807b5b37860
oscar.norman <Oscar.Norman@keyfactor.com>
2023-11-20 09:40:06 +0100

    DSS-2696: Removed duplicated assert from test method

--- signserver/modules/SignServer-Test-System/src/test/java/org/signserver/rest/RestAuthorizationTest.java
+++ signserver/modules/SignServer-Test-System/src/test/java/org/signserver/rest/RestAuthorizationTest.java
@@ -297,7 +297,6 @@ public class RestAuthorizationTest extends ModulesTestCase {
             JSONObject responseJsonObject = new JSONObject(response.jsonPath().getJsonObject("$"));
             assertTrue("Response contains the correct message", responseJsonObject.toString().contains("Worker removed successfully!"));
             assertEquals("Check response status code 200", 200, response.statusCode());
-            assertEquals("Check response status code is 200.", 200, response.statusCode());
         } finally {
             removeWorker(HELLO_WORKER_ID);
         }

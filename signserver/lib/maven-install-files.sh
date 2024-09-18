517eeef0b72aeb36e831860ce74252412d90cb0b
christofer.vikstrom <christofer.vikstrom@keyfactor.com>
2024-09-18 17:03:09 +0200

    DSS-2812: Upgraded x509-common-utils to 4.0.0, P11NG to 0.24.0, jacknji11 to 1.3.0. Added cryptotoken-api 2.2.0. Updated the FortanixCryptoToken to the latest implementation.

--- signserver/lib/maven-install-files.sh
+++ signserver/lib/maven-install-files.sh
@@ -11,9 +11,13 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 echo "Will call mvn install:install-file with certain dependencies from ${DIR}/ext/."
 
 # Install KFC dependencies not yet in Central
-XCU_VERSION=0.10.5
+XCU_VERSION=4.0.0
 mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/x509-common-util-${XCU_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=x509-common-util -Dversion=${XCU_VERSION} -Dpackaging=jar
 mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/x509-common-util-${XCU_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=x509-common-util -Dversion=${XCU_VERSION} -Dpackaging=jar -Dclassifier=sources
+
+CRYPTOTOKENS_VERSION=2.2.0
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cryptotokens-api-${CRYPTOTOKENS_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=cryptotokens-api -Dversion=${CRYPTOTOKENS_VERSION} -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cryptotokens-api-${CRYPTOTOKENS_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=cryptotokens-api -Dversion=${CRYPTOTOKENS_VERSION} -Dpackaging=jar -Dclassifier=sources
 
 # Install DeployTools dependencies
 DEPLOYTOOLS_VERSION=2.3
@@ -55,7 +59,7 @@ mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-ejb-${CESECORE_
 # cesecore-entity jar
 mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-entity-${CESECORE_VERSION}.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-entity -Dversion=${CESECORE_VERSION} -Dpackaging=jar
 
-JACKNJI11_VERSION=1.2.7
+JACKNJI11_VERSION=1.3.0
 mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/jacknji11-${JACKNJI11_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=jacknji11 -Dversion=${JACKNJI11_VERSION} -Dpackaging=jar
 
 FILEUPLOAD_VERSION=1.5-signserver7.0.0

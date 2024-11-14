01a1329e0a87f0d8e05608e8c4571eedde6b9a98
Markus Kilås <markus.kilas@primekey.com>
2024-11-14 11:40:11 +0100

    DSS-2961: Merge most changes from KFC in AzureSignature. Fixes issue with data larger than 4096 bytes. Changes that require changes in AzureProvider are not merged as those also requires change of JSON library.

--- signserver/modules/SignServer-Server/src/main/java/org/cesecore/keys/token/AzureSignature.java
+++ signserver/modules/SignServer-Server/src/main/java/org/cesecore/keys/token/AzureSignature.java
@@ -12,11 +12,13 @@
  *************************************************************************/
 package org.cesecore.keys.token;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.InvalidParameterException;
 import java.security.MessageDigest;
@@ -26,10 +28,10 @@ import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.SignatureException;
 import java.security.SignatureSpi;
-import java.util.Arrays;
+import java.security.spec.AlgorithmParameterSpec;
 import java.util.HashMap;
-
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -38,27 +40,27 @@ import org.bouncycastle.crypto.signers.PlainDSAEncoding;
 import org.bouncycastle.crypto.signers.StandardDSAEncoding;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.BigIntegers;
-import org.cesecore.util.Base64;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
  * A Java signature provider for creating signatures with Azure Key Vault. Only does "engineInitSign, engineUpdate and engineSign"
- * 
- * @version $Id$
  */
 public class AzureSignature extends SignatureSpi {
 
     private static final Logger log = Logger.getLogger(AzureSignature.class);
-
     private AzureCryptoToken.KeyVaultPrivateKey privateKey;
+    /** the hash algorithm to use to hash the toBeSigned data, hashing is done in SW before signing */
     protected String hashAlg;
+    /** the signature algorithm as named by the Azure Key Vault REST API, to be used for signing the hashed toBeSigned data */
     protected String azureSignAlg;
-    private byte[] tbs;
+    /** data to be signed */
+    private ByteArrayOutputStream tbs;
 
     @Override
     protected void engineInitVerify(PublicKey publicKey) throws InvalidKeyException {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
@@ -72,7 +74,10 @@ public class AzureSignature extends SignatureSpi {
 
     @Override
     protected void engineUpdate(byte[] b, int off, int len) throws SignatureException {
-        this.tbs = Arrays.copyOfRange(b, off, len);
+        if (this.tbs == null) {
+            this.tbs = new ByteArrayOutputStream();
+        }
+        this.tbs.write(b, off, len);
     }
 
     @Override
@@ -84,29 +89,40 @@ public class AzureSignature extends SignatureSpi {
             // Key Vault REST API: https://docs.microsoft.com/en-us/rest/api/keyvault/            
             final HttpPost request = new HttpPost(privateKey.getKeyURI() + "/sign?api-version=2016-10-01");
             request.setHeader("Content-Type", "application/json");
-
-            final HashMap<String, String> map = new HashMap<>();
-            // RSA SHA-256 Signature algorithm, https://docs.microsoft.com/en-us/dotnet/api/microsoft.azure.keyvault.cryptography.algorithms.rs256
-            // ES256 is ECDSA with curve P-256 from NIST
-            // ES384 is ECDSA with curve P-384 from NIST
-            // ES512 is ECDSA with curve P-521 from NIST
-            map.put("alg", azureSignAlg);
-            MessageDigest hash = null;
+            // Create hash value of the data to be signed
+            final byte[] signInput;
             try {
-                hash = MessageDigest.getInstance(hashAlg, BouncyCastleProvider.PROVIDER_NAME);
+                final MessageDigest digest = MessageDigest.getInstance(hashAlg, BouncyCastleProvider.PROVIDER_NAME);
+                signInput = digest.digest(tbs.toByteArray());
             } catch (NoSuchAlgorithmException e) {
                 throw new SignatureException("Hash algorithm " + hashAlg + " can not be found in the BC provider: ", e);
             } catch (NoSuchProviderException e) {
                 throw new SignatureException("BC provider not installed, fatal error: ", e);
             }
-            final byte[] digest = hash.digest(tbs);
-            map.put("value", java.util.Base64.getEncoder().encodeToString(digest));
+            final HashMap<String, String> map = new HashMap<>();
+            // Signature algorithms, https://docs.microsoft.com/en-us/rest/api/keyvault/keys/sign/sign#jsonwebkeysignaturealgorithm
+            // Supported/tested
+            // RS256 is SHA256WithRSA (PKCS#1 v1.5)
+            // RS384 is SHA384WithRSA (PKCS#1 v1.5)
+            // RS512 is SHA512WithRSA (PKCS#1 v1.5)
+            // ES256 is SHA256WithECDSA with curve P-256 from NIST
+            // ES384 is SHA384WithECDSA with curve P-384 from NIST
+            // ES512 is SHA512WithECDSA with curve P-521 from NIST
+            // PS256 is SHA256WithRSAAndMGF1 (RSA-PSS)
+            // PS384 is SHA384WithRSAAndMGF1 (RSA-PSS)
+            // PS512 is SHA512WithRSAAndMGF1 (RSA-PSS)
+            // Not supported/tested yet
+            // ES256K is SHA256WithECDSA with curve P-256K from NIST
+            map.put("alg", azureSignAlg);
+            map.put("value", Base64.encodeBase64URLSafeString(signInput));
             final JSONObject jsonObject = new JSONObject(map);
             final StringWriter out = new StringWriter();
             jsonObject.writeJSONString(out);
-            request.setEntity(new StringEntity(out.toString()));
+            final String reqJson = out.toString();
+            request.setEntity(new StringEntity(reqJson));
             if (log.isDebugEnabled()) {
-                log.debug("engineSign Request: " + request.toString()+", "+privateKey.toString());
+                log.debug("engineSign Request: " + request.toString() + ", " + privateKey.toString());
+                log.debug("engineSign Request JSON: " + reqJson + ", " + privateKey.toString());
             }
             try (final CloseableHttpResponse response = privateKey.getCryptoToken().performRequest(request)) {
                 final InputStream content = response.getEntity().getContent();
@@ -120,13 +136,13 @@ public class AzureSignature extends SignatureSpi {
                 if (statusCode != 200) {
                     throw new SignatureException("Signing failed with status code " + statusCode + ", and response JSON: " + s);
                 }
-                final JSONParser jsonParser = new JSONParser();
-                final JSONObject parse = (JSONObject) jsonParser.parse(s);
+                final JSONParser parser = new JSONParser();
+                final JSONObject parse = (JSONObject) parser.parse(s);
                 final String value = (String) parse.get("value");
                 if (log.isDebugEnabled()) {
-                    log.debug("Response Signature Base64 value: " + value);
+                    log.debug("Signature response base64 value: " + value);
                 }
-                byte[] bytes = Base64.decodeURLSafe(value);
+                byte[] bytes = Base64.decodeBase64(value);
                 final int valueLength = bytes.length;
                 if (log.isDebugEnabled()) {
                     log.debug("Response bytes length: " + valueLength);
@@ -148,7 +164,7 @@ public class AzureSignature extends SignatureSpi {
                     }
                     final BigInteger n = BigInteger.ONE.shiftLeft(nLen).subtract(BigInteger.ONE); // "order", just to know how long the signature integers should be
                     if (log.isDebugEnabled()) {
-                        log.debug("(EC) n is: "+BigIntegers.getUnsignedByteLength(n));
+                        log.debug("(EC) n is: " + BigIntegers.getUnsignedByteLength(n));
                     }
                     final BigInteger[] plain = PlainDSAEncoding.INSTANCE.decode(n, bytes);
                     bytes = StandardDSAEncoding.INSTANCE.encode(n, plain[0], plain[1]);
@@ -162,19 +178,28 @@ public class AzureSignature extends SignatureSpi {
 
     @Override
     protected boolean engineVerify(byte[] sigBytes) throws SignatureException {
-        return false;
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
     protected void engineSetParameter(String param, Object value) throws InvalidParameterException {
+        // Super method is deprecated. Use engineSetParameter(AlgorithmParameterSpec params)
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    protected void engineSetParameter(AlgorithmParameterSpec params) throws InvalidAlgorithmParameterException {
+        // This method is called when signing with RSA-PSS (MGF1) algorithms
+        // but we can ignore the params here because Azure Key Vault handles/creates them itself
     }
 
     @Override
     protected Object engineGetParameter(String param) throws InvalidParameterException {
-        return null;
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
     public static final class SHA256WithRSA extends AzureSignature {
+
         public SHA256WithRSA() {
             hashAlg = "SHA256";
             azureSignAlg = "RS256";
@@ -182,6 +207,7 @@ public class AzureSignature extends SignatureSpi {
     }
 
     public static final class SHA384WithRSA extends AzureSignature {
+
         public SHA384WithRSA() {
             hashAlg = "SHA384";
             azureSignAlg = "RS384";
@@ -189,13 +215,39 @@ public class AzureSignature extends SignatureSpi {
     }
 
     public static final class SHA512WithRSA extends AzureSignature {
+
         public SHA512WithRSA() {
             hashAlg = "SHA512";
             azureSignAlg = "RS512";
         }
     }
 
+    public static final class SHA256withRSAandMGF1 extends AzureSignature {
+
+        public SHA256withRSAandMGF1() {
+            hashAlg = "SHA256";
+            azureSignAlg = "PS256";
+        }
+    }
+
+    public static final class SHA384withRSAandMGF1 extends AzureSignature {
+
+        public SHA384withRSAandMGF1() {
+            hashAlg = "SHA384";
+            azureSignAlg = "PS384";
+        }
+    }
+
+    public static final class SHA512withRSAandMGF1 extends AzureSignature {
+
+        public SHA512withRSAandMGF1() {
+            hashAlg = "SHA512";
+            azureSignAlg = "PS512";
+        }
+    }
+
     public static final class SHA256WithECDSA extends AzureSignature {
+
         public SHA256WithECDSA() {
             hashAlg = "SHA256";
             azureSignAlg = "ES256";
@@ -203,13 +255,15 @@ public class AzureSignature extends SignatureSpi {
     }
 
     public static final class SHA384WithECDSA extends AzureSignature {
+
         public SHA384WithECDSA() {
-            hashAlg = "SHA384";
-            azureSignAlg = "ES384";
+            this.hashAlg = "SHA384";
+            this.azureSignAlg = "ES384";
         }
     }
 
     public static final class SHA512WithECDSA extends AzureSignature {
+
         public SHA512WithECDSA() {
             hashAlg = "SHA512";
             azureSignAlg = "ES512";

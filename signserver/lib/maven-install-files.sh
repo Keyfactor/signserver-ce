3e3bb74ab4201a2313d4f159431af62a6d0ae75f
Robert Markovski <5818108+Roshan931@users.noreply.github.com>
2026-08-21 14:14:58 +0200

    fix: [BUG] Cannot build version 7.3.2 - com.keyfactor:cryptotoken...

--- signserver/lib/maven-install-files.sh
+++ signserver/lib/maven-install-files.sh
@@ -12,12 +12,12 @@ echo "Will call mvn install:install-file with certain dependencies from ${DIR}/e
 
 # Install KFC dependencies not yet in Central
 XCU_VERSION=5.2.3
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/x509-common-util-${XCU_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=x509-common-util -Dversion=${XCU_VERSION} -Dpackaging=jar
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/x509-common-util-${XCU_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=x509-common-util -Dversion=${XCU_VERSION} -Dpackaging=jar -Dclassifier=sources
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/x509-common-util-${XCU_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=x509-common-util -Dversion=${XCU_VERSION} -Dpackaging=jar -DgeneratePom=true
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/x509-common-util-${XCU_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=x509-common-util -Dversion=${XCU_VERSION} -Dpackaging=jar -Dclassifier=sources -DgeneratePom=true
 
 CRYPTOTOKENS_VERSION=2.4.4
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cryptotokens-api-${CRYPTOTOKENS_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=cryptotokens-api -Dversion=${CRYPTOTOKENS_VERSION} -Dpackaging=jar
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cryptotokens-api-${CRYPTOTOKENS_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=cryptotokens-api -Dversion=${CRYPTOTOKENS_VERSION} -Dpackaging=jar -Dclassifier=sources
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cryptotokens-api-${CRYPTOTOKENS_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=cryptotokens-api -Dversion=${CRYPTOTOKENS_VERSION} -Dpackaging=jar -DgeneratePom=true
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cryptotokens-api-${CRYPTOTOKENS_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=cryptotokens-api -Dversion=${CRYPTOTOKENS_VERSION} -Dpackaging=jar -Dclassifier=sources -DgeneratePom=true
 
 # Install DeployTools dependencies
 DEPLOYTOOLS_VERSION=2.3
@@ -43,30 +43,30 @@ mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/DeployTools-Common-${DEP
 
 # Add next group of dependencies here
 XADES4J_VERSION=2.2.1-signserver7.0.0
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/xades4j-${XADES4J_VERSION}.jar" -DgroupId=org.signserver.xades4j -DartifactId=xades4j -Dversion=${XADES4J_VERSION} -Dpackaging=jar
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/xades4j-${XADES4J_VERSION}-sources.jar" -DgroupId=org.signserver.xades4j -DartifactId=xades4j -Dversion=${XADES4J_VERSION} -Dpackaging=jar -Dclassifier=sources
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/xades4j-${XADES4J_VERSION}.jar" -DgroupId=org.signserver.xades4j -DartifactId=xades4j -Dversion=${XADES4J_VERSION} -Dpackaging=jar -DgeneratePom=true
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/xades4j-${XADES4J_VERSION}-sources.jar" -DgroupId=org.signserver.xades4j -DartifactId=xades4j -Dversion=${XADES4J_VERSION} -Dpackaging=jar -Dclassifier=sources -DgeneratePom=true
 
 CESECORE_VERSION=7.0.0.1signserver7.0.0
 # cesecore-common jar
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-common-${CESECORE_VERSION}.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-common -Dversion=${CESECORE_VERSION} -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-common-${CESECORE_VERSION}.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-common -Dversion=${CESECORE_VERSION} -Dpackaging=jar -DgeneratePom=true
 
 # cesecore-ejb-interfaces jar
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-interfaces-${CESECORE_VERSION}.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-interfaces -Dversion=${CESECORE_VERSION} -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-interfaces-${CESECORE_VERSION}.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-interfaces -Dversion=${CESECORE_VERSION} -Dpackaging=jar -DgeneratePom=true
 
 # cesecore-ebj jar
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-ejb-${CESECORE_VERSION}.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-ejb -Dversion=${CESECORE_VERSION} -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-ejb-${CESECORE_VERSION}.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-ejb -Dversion=${CESECORE_VERSION} -Dpackaging=jar -DgeneratePom=true
 
 # cesecore-entity jar
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-entity-${CESECORE_VERSION}.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-entity -Dversion=${CESECORE_VERSION} -Dpackaging=jar
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cesecore-entity-${CESECORE_VERSION}.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-entity -Dversion=${CESECORE_VERSION} -Dpackaging=jar -DgeneratePom=true
 
 JACKNJI11_VERSION=1.3.0
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/jacknji11-${JACKNJI11_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=jacknji11 -Dversion=${JACKNJI11_VERSION} -Dpackaging=jar
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/jacknji11-${JACKNJI11_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=jacknji11 -Dversion=${JACKNJI11_VERSION} -Dpackaging=jar -Dclassifier=sources
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/jacknji11-${JACKNJI11_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=jacknji11 -Dversion=${JACKNJI11_VERSION} -Dpackaging=jar -DgeneratePom=true
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/jacknji11-${JACKNJI11_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=jacknji11 -Dversion=${JACKNJI11_VERSION} -Dpackaging=jar -Dclassifier=sources -DgeneratePom=true
 
 
 FILEUPLOAD_VERSION=1.5-signserver7.0.0
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/commons-fileupload-${FILEUPLOAD_VERSION}.jar" -DgroupId=org.signserver.commons-fileupload -DartifactId=commons-fileupload -Dversion=${FILEUPLOAD_VERSION} -Dpackaging=jar
-mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/commons-fileupload-${FILEUPLOAD_VERSION}-sources.jar" -DgroupId=org.signserver.commons-fileupload -DartifactId=commons-fileupload -Dversion=${FILEUPLOAD_VERSION} -Dpackaging=jar -Dclassifier=sources
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/commons-fileupload-${FILEUPLOAD_VERSION}.jar" -DgroupId=org.signserver.commons-fileupload -DartifactId=commons-fileupload -Dversion=${FILEUPLOAD_VERSION} -Dpackaging=jar -DgeneratePom=true
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/commons-fileupload-${FILEUPLOAD_VERSION}-sources.jar" -DgroupId=org.signserver.commons-fileupload -DartifactId=commons-fileupload -Dversion=${FILEUPLOAD_VERSION} -Dpackaging=jar -Dclassifier=sources -DgeneratePom=true
 
 # Install EE dependencies as well if available
 if [ -f "${DIR}/maven-install-files-ee.sh" ]; then
@@ -74,14 +74,14 @@ if [ -f "${DIR}/maven-install-files-ee.sh" ]; then
 else
     # Install CE version of cesecore source jars
     # cesecore-common source-jar
-    mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cesecore-common-${CESECORE_VERSION}-sources.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-common -Dversion=${CESECORE_VERSION} -Dpackaging=jar -Dclassifier=sources
+    mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cesecore-common-${CESECORE_VERSION}-sources.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-common -Dversion=${CESECORE_VERSION} -Dpackaging=jar -Dclassifier=sources -DgeneratePom=true
 
     # cesecore-ejb-interfaces source-jar
-    mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cesecore-interfaces-${CESECORE_VERSION}-sources.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-interfaces -Dversion=${CESECORE_VERSION} -Dpackaging=jar -Dclassifier=sources
+    mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cesecore-interfaces-${CESECORE_VERSION}-sources.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-interfaces -Dversion=${CESECORE_VERSION} -Dpackaging=jar -Dclassifier=sources -DgeneratePom=true
 
     # cesecore-ejb source-jar
-    mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cesecore-ejb-${CESECORE_VERSION}-sources.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-ejb -Dversion=${CESECORE_VERSION} -Dpackaging=jar -Dclassifier=sources
+    mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cesecore-ejb-${CESECORE_VERSION}-sources.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-ejb -Dversion=${CESECORE_VERSION} -Dpackaging=jar -Dclassifier=sources -DgeneratePom=true
 
     # cesecore-entity source-jar
-    mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cesecore-entity-${CESECORE_VERSION}-sources.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-entity -Dversion=${CESECORE_VERSION} -Dpackaging=jar -Dclassifier=sources
+    mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cesecore-entity-${CESECORE_VERSION}-sources.jar" -DgroupId=org.ejbca.cesecore -DartifactId=cesecore-entity -Dversion=${CESECORE_VERSION} -Dpackaging=jar -Dclassifier=sources -DgeneratePom=true
 fi

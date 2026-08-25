ea333b9d0789fb38dfd7b924b41c62096458c0bf
Markus Kilås <markus.kilas@keyfactor.com>
2026-08-25 09:03:53 +0200

    DSS-3920: Add cryptotokens pom (without servicemanifestbuilder).

--- signserver/lib/maven-install-files.sh
+++ signserver/lib/maven-install-files.sh
@@ -16,6 +16,7 @@ mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/x509-common-util-${XCU_V
 mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/x509-common-util-${XCU_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=x509-common-util -Dversion=${XCU_VERSION} -Dpackaging=jar -Dclassifier=sources
 
 CRYPTOTOKENS_VERSION=3.10.1
+mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cryptotokens-${CRYPTOTOKENS_VERSION}.pom" -DgroupId=com.keyfactor -DartifactId=cryptotokens -Dversion=${CRYPTOTOKENS_VERSION} -Dpackaging=pom
 mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/ext/cryptotokens-api-${CRYPTOTOKENS_VERSION}.jar" -DgroupId=com.keyfactor -DartifactId=cryptotokens-api -Dversion=${CRYPTOTOKENS_VERSION} -Dpackaging=jar
 mvn ${MVN_OPTS} install:install-file -Dfile="${DIR}/../modules/source-jars/cryptotokens-api-${CRYPTOTOKENS_VERSION}-sources.jar" -DgroupId=com.keyfactor -DartifactId=cryptotokens-api -Dversion=${CRYPTOTOKENS_VERSION} -Dpackaging=jar -Dclassifier=sources
 
